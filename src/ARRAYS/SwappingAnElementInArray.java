323ec33f54fcb2ed655e0b6452cf1b0a2cd927e4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-18 11:06:18 +0530

    modified

--- src/ARRAYS/SwappingAnElementInArray.java
+++ src/ARRAYS/SwappingAnElementInArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SwappingAnElementInArray {
     public static void main(String[] args) {
       int[] arr = {1,2,3,4,5,6,7,8,9,10};
-      swap(arr,0,9);
+      swap(arr,1,5);
         System.out.println(Arrays.toString(arr));
     }
     static void swap(int[] arr, int startIndex, int endIndex){
@@ -14,7 +14,6 @@ public class SwappingAnElementInArray {
             arr[startIndex] = arr[endIndex];
             arr[endIndex]= temp;
     }
-
 }
 // output :
 // [10, 2, 3, 4, 5, 6, 7, 8, 9, 1]
