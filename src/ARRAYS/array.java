712a022c07cfc120df8145b249f0add74824869f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-18 10:48:15 +0530

    modified

--- src/ARRAYS/array.java
+++ src/ARRAYS/array.java
@@ -2,6 +2,8 @@ package ARRAYS;
 
 public class array {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+//     int nums[] = {1,2,3,4,5,6,7,8,9,10};
+//        String[] arr = new String[4];
+//        System.out.println(arr[0]);
     }
 }
