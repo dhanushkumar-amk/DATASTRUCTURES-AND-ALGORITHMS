c3dc9ca70132c264544510e1f688d83e31e1393e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-19 21:33:20 +0530

    Modified on 19-05-2024

--- src/ARRAYS/JumbGame2.java
+++ src/ARRAYS/JumbGame2.java
@@ -4,5 +4,24 @@ public class JumbGame2 {
     public static void main(String[] args) {
 
     }
-    
+
+    static int jumpGame(int[] nums) {
+        int destination = nums.length - 1;
+        int totalJump = 0;
+        int lastIndexJump = 0;
+        int coverage = 0;
+
+        // base condition
+        for (int i = 0; i < nums.length; i++) {
+            coverage = Math.max(coverage, i + nums[i]);
+
+            if (i == lastIndexJump) {
+                lastIndexJump = coverage;
+                totalJump++;
+                if (coverage >= destination)
+                    return totalJump;
+            }
+        }
+        return totalJump;
+    }
 }
