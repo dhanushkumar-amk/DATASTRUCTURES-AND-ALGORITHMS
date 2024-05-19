d71d232dfd947cd440e6d828dbd9c99253e1c421
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-19 19:20:30 +0530

    Modified on 19-05-2024

--- src/ARRAYS/tmep.java
+++ src/ARRAYS/tmep.java
@@ -2,11 +2,6 @@ package ARRAYS;
 
 public class tmep {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println("Arunkumar");
-        System.out.println("hello");
-        System.out.println("xyz");
-        System.out.println("Hello world");
-        System.out.println("tmep.main");
+
     }
 }
