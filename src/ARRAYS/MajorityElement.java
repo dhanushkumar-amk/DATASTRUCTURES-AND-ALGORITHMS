d3a787b2cd49b1a32538b7226adeb1340955e922
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-19 19:28:43 +0530

    Modified on 19-05-2024

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -2,5 +2,24 @@ package ARRAYS;
 
 
 public class MajorityElement {
+    public static void main(String[] args) {
 
+    }
+    static int Majority(int[] nums){
+        int count = 0;
+        int element = 0;
+        int n = nums.length;
+
+        for (int i = 0; i <n; i++) {
+            if(count == 0){
+                count++;
+            element = nums[i];
+            }
+
+            if(element == nums[i]) count++;
+            else  count--;
+        }
+
+        return 0;
+    }
 }
