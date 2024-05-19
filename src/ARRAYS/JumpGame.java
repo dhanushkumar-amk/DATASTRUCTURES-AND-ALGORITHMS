e632f8b0a3d6e48ab1ed606973e13ef61bc1ff61
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-19 19:58:40 +0530

    Modified on 19-05-2024

--- src/ARRAYS/JumpGame.java
+++ src/ARRAYS/JumpGame.java
@@ -2,7 +2,8 @@ package ARRAYS;
 
 public class JumpGame {
     public static void main(String[] args) {
-
+        int nums[] = { 1, 1, 2, 5, 2, 1, 0, 0, 1, 3 };
+        System.out.println(JumpGame1(nums));
     }
     static boolean JumpGame1(int[] nums){
         int finalPosition = nums.length-1;
