c2b390cb03724407c8499c14099bceaf7bdea9c2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-19 19:58:28 +0530

    Modified on 19-05-2024

--- src/ARRAYS/JumpGame.java
+++ src/ARRAYS/JumpGame.java
@@ -5,6 +5,12 @@ public class JumpGame {
 
     }
     static boolean JumpGame1(int[] nums){
+        int finalPosition = nums.length-1;
+        for (int index = nums.length-2; index >=0 ; index--) {
+            if(nums[index] + index >= finalPosition)
+            finalPosition = nums[index];
+        }
+
         return finalPosition ==0;
     }
 }
