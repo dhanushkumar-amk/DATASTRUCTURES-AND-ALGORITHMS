8885e561d9d498dc062bfbcd502e129c88a1bd55
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-19 19:33:37 +0530

    Modified on 19-05-2024

--- src/ARRAYS/MajorityElement.java
+++ src/ARRAYS/MajorityElement.java
@@ -12,22 +12,7 @@ public class MajorityElement {
         int element = 0;
         int n = nums.length;
 
-        for (int i = 0; i <n; i++) {
-            if(count == 0){
-                count++;
-            element = nums[i];
-            }
-
-           else if(element == nums[i]) count++;
-            else  count--;
-        }
-        int finalCount = 0;
-        for (int i = 0; i <n ; i++) {
-
-
-            if(element == nums[i]) finalCount++;
-            if(finalCount >= (n/2)) return  element;
-        }
+       
         return -1;
     }
 }
