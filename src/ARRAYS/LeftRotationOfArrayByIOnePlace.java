8eddff0effbdce61fa8c2f08244e3506fc173f99
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-19 19:45:00 +0530

    Modified on 19-05-2024

--- src/ARRAYS/LeftRotationOfArrayByIOnePlace.java
+++ src/ARRAYS/LeftRotationOfArrayByIOnePlace.java
@@ -12,7 +12,7 @@ public class LeftRotationOfArrayByIOnePlace {
 static void LeftRotation(int[] nums, int n){
         int temp = nums[0];
         int i;
-    for ( i = 0; i <n; i++) {
+    for ( i = 0; i <n-1; i++) {
         nums[i] = nums[i+1];
     }
     nums[i-1] = temp;
