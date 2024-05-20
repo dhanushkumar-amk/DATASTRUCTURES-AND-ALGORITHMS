f58dd2150365c88a6c69c621fabe1d12afcddc37
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-20 14:02:10 +0530

    Modified on 20-05-2024

--- src/STRINGS/ReverseAString.java
+++ src/STRINGS/ReverseAString.java
@@ -7,13 +7,13 @@ public class ReverseAString {
         System.out.println(reverseWords(s));
     }
     static String reverseWords(String s){
-        String[] words = s.split(" +");
-        StringBuilder sb = new StringBuilder();
+     String[] words = s.split(" +");
+     StringBuilder sb = new StringBuilder();
 
-        for (int i = words.length-1; i >=0 ; i--) {
+        for (int i = words.length-1; i <=0 ; i--) {
             sb.append(words[i]);
             sb.append(" ");
         }
-        return sb.toString().trim();
+        return  sb.toString().trim();
     }
 }
