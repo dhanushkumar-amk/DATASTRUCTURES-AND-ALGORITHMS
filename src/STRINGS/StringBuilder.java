c960730ab20091b78da710a53cd49b3c2c24e936
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-20 10:17:06 +0530

    Modified on 20-05-2024

--- src/STRINGS/StringBuilder.java
+++ src/STRINGS/StringBuilder.java
@@ -29,9 +29,13 @@ public class StringBuilder {
         String s1 = "hello";
         String s2 = "world";
         String s3 = "!";
-        System.out.println(s1 + s2);
+   /*     System.out.println(s1 + s2);
         System.out.println(s1.concat(s2));
         System.out.println(s1+s2+s3);
+    */
         System.out.println(s1.concat(s2.concat(s3))); // to join two or more string
+
+
+
     }
 }
