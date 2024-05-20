389cfee34121fd60688ee1bb05c0f45fe2a725d3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-20 07:05:25 +0530

    Modified on 20-05-2024

--- src/ARRAYS/MaximumSubArraySum.java
+++ src/ARRAYS/MaximumSubArraySum.java
@@ -3,7 +3,7 @@ package ARRAYS;
 public class MaximumSubArraySum {
     public static void main(String[] args) {
         int[] nums= {-2,1,-3,4,-1,2,1,-5,-4};
-        System.out.println();
+        System.out.println(MaximumSubArray(nums));
     }
 
     public static int MaximumSubArray(int[] nums){
