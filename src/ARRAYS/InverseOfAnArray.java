41ca24843585fd79fb2ae958b567734e71bdb6a5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-20 07:25:49 +0530

    Modified on 20-05-2024

--- src/ARRAYS/InverseOfAnArray.java
+++ src/ARRAYS/InverseOfAnArray.java
@@ -2,7 +2,12 @@ package ARRAYS;
 
 public class InverseOfAnArray {
     public static void main(String[] args) {
+        int[] nums = { 2, 3, 1, 0, 4 };
+        int[] SecondArrau = Inverse(nums);
 
+        for (int i = 0; i < nums.length ; i++) {
+            System.out.println(SecondArrau[i] + " ");
+        }
     }
     static int[] Inverse(int[] nums){
        int[] SecondArray = new int[nums.length];
