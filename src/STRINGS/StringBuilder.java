499f22fcd75ff7537652248212d8b46819c2cd92
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-20 11:05:38 +0530

    Modified on 20-05-2024

--- src/STRINGS/StringBuilder.java
+++ src/STRINGS/StringBuilder.java
@@ -104,6 +104,13 @@ public class StringBuilder {
         // Start index = 0;
         // end index + 1
         String SubString1 = "Dhanush";
+        System.out.println(SubString1.substring(0,1));
+        System.out.println(SubString1.substring(0,2));
+        System.out.println(SubString1.substring(0,3));
+        System.out.println(SubString1.substring(0,4));
         System.out.println(SubString1.substring(0,5));
+        System.out.println(SubString1.substring(0,5));
+        System.out.println(SubString1.substring(0,6));
+        System.out.println(SubString1.substring(0,7));
     }
 }
