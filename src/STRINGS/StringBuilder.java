fbe5f0be2dae24339313f5cec1fd0867c7be2120
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-20 10:03:54 +0530

    Modified on 20-05-2024

--- src/STRINGS/StringBuilder.java
+++ src/STRINGS/StringBuilder.java
@@ -3,8 +3,13 @@ import java.lang.String;
 
 public class StringBuilder {
     public static void main(String[] args) {
+        // compareing the two strings using ==
         String name  = "Dhanushkumar";
         String name1 = "Dhanushkumar";
         System.out.println(name == name1);
+
+        // two methods
+//        build in method
+
     }
 }
