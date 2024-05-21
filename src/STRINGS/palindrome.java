0ec59ce1234d737f3458e02f7517865211ccd0b5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-21 07:37:10 +0530

    Modified on 21-05-2024

--- src/STRINGS/palindrome.java
+++ src/STRINGS/palindrome.java
@@ -6,7 +6,19 @@ import  java.lang.*;
 public class palindrome {
     public static void main(String[] args) {
        String s = "racecr" ;
-        System.out.println();
+        System.out.println(Palindrome(s));
     }
+    static boolean Palindrome(String s){
+        int left = 0;
+        int right = s.length()-1;
+        while(left < right){
+            if(s.charAt(left) == s.charAt(right)){
+                right--;
+                left++;
+            }
+            else return false;
+        }
 
+        return true;
+    }
 }
