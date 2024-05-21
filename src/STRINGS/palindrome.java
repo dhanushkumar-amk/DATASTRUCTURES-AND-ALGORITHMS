196c32daf53b8d7ecb76947c41bfd5f25f557d0b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-21 08:10:54 +0530

    Modified on 21-05-2024

--- src/STRINGS/palindrome.java
+++ src/STRINGS/palindrome.java
@@ -13,7 +13,12 @@ public class palindrome {
       int right = s.length()-1;
 
       while(left < right){
-          if(s.charAt(left) == s.char)
+          if(s.charAt(left) == s.charAt(right)){
+              left++;
+              right--;
+          }
+          else return  false;
       }
+      return true;
     }
 }
