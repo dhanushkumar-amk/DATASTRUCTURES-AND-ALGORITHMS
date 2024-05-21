e5653b71ebae98fc2e5d585dac83d71b482e9145
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-21 10:19:20 +0530

    Modified on 21-05-2024

--- src/STRINGS/LengthOfTheLastWord.java
+++ src/STRINGS/LengthOfTheLastWord.java
@@ -1,2 +1,15 @@
-package STRINGS;public class LengthOfTheLastWord {
+package STRINGS;
+import java.lang.*;
+
+public class LengthOfTheLastWord {
+    public static void main(String[] args) {
+        String word = "Hello World ";
+        System.out.println(lastWordCount(word));
+    }
+
+    static int lastWordCount(String word){
+
+        
+        return count;
+    }
 }
