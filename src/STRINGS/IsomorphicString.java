67dad7d88fb3b40a7e1adda6b2627e112deeeeb4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-21 21:06:35 +0530

    Modified on 21-05-2024

--- src/STRINGS/IsomorphicString.java
+++ src/STRINGS/IsomorphicString.java
@@ -1,8 +1,15 @@
 package STRINGS;
+import java.lang.*;
+import java.util.HashMap;
 
 public class IsomorphicString {
     public static void main(String[] args) {
 
     }
-    static 
+    static boolean Isomorphic(String s , String t){
+        HashMap<Character, Character> hm = new HashMap<>();
+        
+
+        return true;
+    }
 }
