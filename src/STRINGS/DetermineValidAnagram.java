39bbdd5dff3d0ab0cfdc56ce03b14a95655a96e7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-21 08:32:06 +0530

    Modified on 21-05-2024

--- src/STRINGS/DetermineValidAnagram.java
+++ src/STRINGS/DetermineValidAnagram.java
@@ -3,7 +3,7 @@ import  java.lang.*;
 
 public class DetermineValidAnagram {
     public static void main(String[] args) {
-String string1 = "dormitoryx";
+String string1 = "dormitory";
 String string2 = "dirty room";
 
         System.out.println(ValidAnagrams(string1, string2));
@@ -11,28 +11,6 @@ String string2 = "dirty room";
     }
     static boolean ValidAnagrams(String string1 , String string2)
     {
-        string1 = string1.toLowerCase();
-        string2 = string2.toLowerCase();
-
-        string1 = string1.replace(" ", "");
-        string2 = string2.replace(" ", "");
-
-        int[] counts = new int[26];
-
-        for (int i = 0; i < string1.length() ; i++) {
-            counts[string1.charAt(i) - 'a']++;
-        }
-
-        for (int i = 0; i < string2.length(); i++) {
-            counts[string2.charAt(i) - 'a']--;
-        }
-
-        for(int count : counts){
-            if (count != 0){
-                return  false;
-            }
-        }
-
-     return true;
+       
     }
 }
