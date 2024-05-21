642aeb30fda0f7f939b111981039885232f69fc1
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-21 11:33:26 +0530

    Modified on 21-05-2024

--- src/STRINGS/FirstUniqueCharcterInAString.java
+++ src/STRINGS/FirstUniqueCharcterInAString.java
@@ -1,9 +1,30 @@
 package STRINGS;
 
+import java.util.HashMap;
+
 public class FirstUniqueCharcterInAString {
     public static void main(String[] args) {
   String s = "fourByFourFrog";
         System.out.println();
+    }
+
+    public int FirstUnique(String s){
+        int index =-1;
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i <s.length() ; i++) {
+
+            char c = s.charAt(i);
+            int freq = hm.getOrDefault(c, 0);
+            hm.put(c, (freq + 1));
+        }
+        for (int i = 0; i <s.length() ; i++) {
+            if (hm.get(s.charAt(i)) == 1)
+            {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 
 }
