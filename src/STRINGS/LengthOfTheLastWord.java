674cad78e2a4c3a416db5cc89207810a33ea3e65
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-21 10:29:54 +0530

    Modified on 21-05-2024

--- src/STRINGS/LengthOfTheLastWord.java
+++ src/STRINGS/LengthOfTheLastWord.java
@@ -9,7 +9,15 @@ public class LengthOfTheLastWord {
 
     static int lastWordCount(String word){
 
-        
+        int count =0;
+
+        for (int i = word.length()-1; i >= 0 ; i++) {
+            if(word.charAt(i) != ' ') count++;
+
+            else {
+                if (count > 0) return  count;
+            }
+        }
         return count;
     }
 }
