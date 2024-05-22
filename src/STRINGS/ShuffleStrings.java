b0b0b20fe4031938dce03c745d4c5dc8a58cff85
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-22 16:34:41 +0530

    Modified on 22-05-2024

--- src/STRINGS/ShuffleStrings.java
+++ src/STRINGS/ShuffleStrings.java
@@ -9,8 +9,12 @@ public class ShuffleStrings {
     }
 
     static String shuffle(String s , int[] indices){
+char[] str = new char[s.length()];
 
-
+        for (int i = 0; i <s.length() ; i++) {
+            str[indices[i]] = s.charAt(i);
+        }
+ return  String.valueOf(str);
     }
 
 }
