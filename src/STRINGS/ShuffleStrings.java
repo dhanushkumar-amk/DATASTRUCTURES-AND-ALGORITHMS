dcc4827756246f7be673c6e193a3345d90d8582d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-22 16:32:49 +0530

    Modified on 22-05-2024

--- src/STRINGS/ShuffleStrings.java
+++ src/STRINGS/ShuffleStrings.java
@@ -1,6 +1,16 @@
 package STRINGS;
+import  java.lang.*;
 
 public class ShuffleStrings {
-    psvm
-    
+    public static void main(String[] args) {
+    String s = "rockslife";
+    int[] indices = {4,5,6,7,8,0,1,2,3};
+        System.out.println(shuffle(s, indices));
+    }
+
+    static String shuffle(String s , int[] indices){
+
+
+    }
+
 }
