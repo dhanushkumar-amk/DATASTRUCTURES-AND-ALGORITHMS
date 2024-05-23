081f298652ec2f81aeca9966f306dae1006b5ef3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-23 08:57:16 +0530

    Modified on 23-05-2024

--- src/STRINGS/FizzBuzz.java
+++ src/STRINGS/FizzBuzz.java
@@ -9,7 +9,20 @@ public class FizzBuzz {
         System.out.println(fizzBuzz(n));
     }
     static List<String> fizzBuzz(int n){
-    
+    ArrayList<String> li = new ArrayList<>();
+
+        for (int i = 1; i <=n ; i++) {
+            if(i % 3==0 && i % 5 == 0){
+                li.add("FizzBuzz");
+            } else if (i % 3==0) {
+                li.add("Fizz");
+            } else if (i % 5==0) {
+                li.add("buzz");
+            }
+            else {
+                li.add(String.valueOf(i));
+            }
+        }
         return li;
     }
 }
