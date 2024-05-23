999013c479b532bcebffd1ac7002709682730421
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-23 08:49:36 +0530

    Modified on 23-05-2024

--- src/STRINGS/FizzBuzz.java
+++ src/STRINGS/FizzBuzz.java
@@ -1,2 +1,29 @@
-package STRINGS;public class FizzBuzz {
+package STRINGS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        int n= 5;
+        System.out.println(fizzBuzz(n));
+    }
+    static List<String> fizzBuzz(int n){
+        List<String> li = new ArrayList<>();
+
+        for (int i = 1; i <=n; i++) {
+            if(i % 3==0 && i % 5 == 0) {
+                li.add("FizzBuzz");
+            }
+                else if(i % 3== 0){
+                    li.add("Fizz");
+                } else if (i % 5 ==0) {
+                li.add("Buzz");
+            }
+                else {
+                    li.add(String.valueOf(i));
+            }
+        }
+        return li;
+    }
 }
