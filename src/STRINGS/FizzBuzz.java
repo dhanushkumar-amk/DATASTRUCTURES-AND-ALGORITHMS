bb6c1ad179422c5ba4c402cd85d944d4d0e9560b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-23 08:49:46 +0530

    Modified on 23-05-2024

--- src/STRINGS/FizzBuzz.java
+++ src/STRINGS/FizzBuzz.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        int n= 5;
+        int n= 15;
         System.out.println(fizzBuzz(n));
     }
     static List<String> fizzBuzz(int n){
