f556a03051596571da4ce8bdf6e101773cb46111
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-23 07:41:02 +0530

    Modified on 23-05-2024

--- src/STRINGS/ReverseOnlyLetters.java
+++ src/STRINGS/ReverseOnlyLetters.java
@@ -8,10 +8,20 @@ public class ReverseOnlyLetters {
     }
     static String ReverseCharcter(String s){
         char[] arr = s.toCharArray();
-    int left =0
+            int left =0;
             int right = s.length()-1;
 
-    
+    while (left < right){
+        if(!Character.isLetter(arr[left])) left++;
+        else if(!Character.isLetter(arr[right])) right++;
+        else {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
         return new String(arr);
     }
 }
