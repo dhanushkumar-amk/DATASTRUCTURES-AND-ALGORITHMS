ca0db6689d5b6efbf2d8d50e7763801a15903d8a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-29 21:38:05 +0530

    Modified on 29-05-2024

--- src/MATHEMATICALS/SAMPLE.java
+++ src/MATHEMATICALS/SAMPLE.java
@@ -4,5 +4,9 @@ public class SAMPLE {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello world");
+int n = 20;
+        for (int i = 0; i <n ; i++) {
+            System.out.println(i + ' ');
+        }
     }
 }
