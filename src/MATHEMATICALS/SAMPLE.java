cf6a0ab60c5668e6a37728d83e95d57e6987408b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-29 21:34:00 +0530

    Modified on 29-05-2024

--- src/MATHEMATICALS/SAMPLE.java
+++ src/MATHEMATICALS/SAMPLE.java
@@ -3,5 +3,6 @@ package MATHEMATICALS;
 public class SAMPLE {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello world");
     }
 }
