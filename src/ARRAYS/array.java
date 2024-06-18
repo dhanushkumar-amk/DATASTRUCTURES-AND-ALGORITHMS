ed9829859c4339f63269564140d61d149a581b7b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-19 05:05:58 +0530

    modified

--- src/ARRAYS/array.java
+++ src/ARRAYS/array.java
@@ -5,8 +5,11 @@ import java.util.Arrays;
 public class array {
     public static void main(String[] args) {
    // Arrays in java
+        int arr[] = new int[];
+
         // creating of the array
     int num[] = {1,2,3,4,5};
+    // changing the value in array
     num[1] = 45;
 
         System.out.println(num[0]);
