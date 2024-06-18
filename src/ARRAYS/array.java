47a8e6b71218c5d4e0f734aa0b80ea3d6f31c5a3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-19 05:03:14 +0530

    Modified on 19-06-2024

--- src/ARRAYS/array.java
+++ src/ARRAYS/array.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 
 public class array {
     public static void main(String[] args) {
-     int nums[] = {1,2,3,4,5,6,7,8,9,10};
+   // Arrays in java
+        // creating of the array
+    int num[] = {1,2,3,4,5};
+        System.out.println(num);
 
-        change(nums);
-        System.out.println(Arrays.toString(nums));
     }
 
-    static void change(int[] nums){
-        nums[0] = 99;
-    }
+
 }
