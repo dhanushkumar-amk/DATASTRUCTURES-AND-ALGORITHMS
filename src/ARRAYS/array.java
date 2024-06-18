cd9252343451fde93630047fbb1d8c9fe7ecf35e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-19 05:05:15 +0530

    Modified on 19-06-2024

--- src/ARRAYS/array.java
+++ src/ARRAYS/array.java
@@ -7,7 +7,10 @@ public class array {
    // Arrays in java
         // creating of the array
     int num[] = {1,2,3,4,5};
+    num[1] = 45;
+
         System.out.println(num[0]);
+        System.out.println(num[1]);
     }
 
 
