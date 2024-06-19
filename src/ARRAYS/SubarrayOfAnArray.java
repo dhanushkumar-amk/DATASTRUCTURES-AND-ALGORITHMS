f06dc94f371680a3d28f20afa2d793e7a498b696
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-20 05:22:46 +0530

    modified

--- src/ARRAYS/SubarrayOfAnArray.java
+++ src/ARRAYS/SubarrayOfAnArray.java
@@ -3,9 +3,9 @@ package ARRAYS;
 public class SubarrayOfAnArray {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
-        SubArray(arr)
+        subArray(arr);
     }
-    static  void subArray(int arr[]){
+  public  static  void subArray(int arr[]){
         for (int i = 0; i <arr.length ; i++) {
             for (int j = i; j <arr.length ; j++) {
                 for (int k = i; k <=j ; k++) {
