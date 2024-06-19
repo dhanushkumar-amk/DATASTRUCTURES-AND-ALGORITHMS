2022688983a2b0c7a425af640b8857ba34744ee6
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-19 21:22:53 +0530

    modified

--- src/ARRAYS/RemoveDuplicates.java
+++ src/ARRAYS/RemoveDuplicates.java
@@ -24,7 +24,7 @@ public class RemoveDuplicates {
         for (int j = 1; j <nums.length ; j++) {
            if(nums[i] != nums[j]){
                i++;
-               nums[i+1] = nums[j];
+               nums[i] = nums[j];
            }
         }
         return  i + 1;
