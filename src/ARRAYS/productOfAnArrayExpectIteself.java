f5c0966174445ef9f700214b3e58c45b671e6ebd
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-19 18:48:02 +0530

    Modified on 19-06-2024

--- src/ARRAYS/productOfAnArrayExpectIteself.java
+++ src/ARRAYS/productOfAnArrayExpectIteself.java
@@ -44,13 +44,13 @@ public class productOfAnArrayExpectIteself {
             int right[] = new int[nums.length];
 
             left[0] = 1;
-        for (int i = 1; i <nums.length ; i++) {
-            left[i]= nums[i-1] * nums[i];
+        for (int i = 1; i <nums.length; i++) {
+            left[i]= left[i-1] * nums[i];
         }
 
         right[nums.length-1] = 1;
         for (int i = nums.length-2; i >= 0 ; i--) {
-            right[i] = nums[i+1] * nums[i];
+            right[i] = right[i+1] * nums[i];
         }
          int[] ans = new int[nums.length];
 
