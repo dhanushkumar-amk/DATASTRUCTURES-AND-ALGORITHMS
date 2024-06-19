2397d8c43165d8ddb8d5efd0388c726d6a4c382e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-19 21:06:47 +0530

    modified

--- src/JAVA_LEARNING/DATA_TYPES.java
+++ src/JAVA_LEARNING/DATA_TYPES.java
@@ -1,2 +1,13 @@
+<<<<<<< HEAD
 package JAVA_LEARNING;public class DATA_TYPES {
+=======
+package JAVA_LEARNING;
+
+public class DATA_TYPES {
+    // data types is used to store the value in variable
+    int num = 10; // 4 bit of storage
+    String name = "Dhanush";
+    char letter = 'A'; // 2 bits
+    boolean choose = true;
+>>>>>>> be331f1 (modified)
 }
