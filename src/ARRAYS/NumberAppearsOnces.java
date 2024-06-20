7839261ec5df1bba7f1d9a709937e1ad2af8756a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-20 18:34:36 +0530

    Modified on 20-06-2024

--- src/ARRAYS/NumberAppearsOnces.java
+++ src/ARRAYS/NumberAppearsOnces.java
@@ -3,9 +3,13 @@ package ARRAYS;
 public class NumberAppearsOnces {
     public static void main(String[] args) {
         int arr[] = {1,1,2,2,3,4,4,5,5};
-
+        System.out.println(AppearsOnces(arr));
     }
     public static int AppearsOnces(int arr[]){
-        
+        int xor =0;
+        for (int i = 0; i < arr.length; i++) {
+            xor = xor ^ arr[i];
+        }
+        return  xor;
     }
 }
