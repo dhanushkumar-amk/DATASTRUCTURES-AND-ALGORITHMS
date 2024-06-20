c9f40f23de1a5c691e8e67b36658513d2c795b46
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-20 06:08:09 +0530

    modified

--- src/ARRAYS/FrequencyElementInArray.java
+++ src/ARRAYS/FrequencyElementInArray.java
@@ -2,7 +2,7 @@ package ARRAYS;
 
 public class FrequencyElementInArray {
     public static void main(String[] args) {
-        int[] nums = {20,20,30,30,30,30};
+        int[] nums = {20,20,30,30,30,30,10};
 
 FreqElement(nums);
     }
