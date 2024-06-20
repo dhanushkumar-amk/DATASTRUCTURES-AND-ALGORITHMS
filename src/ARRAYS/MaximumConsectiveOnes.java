83bd3fa47ec0c1af0e994521c22a7ee0e0a4565e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-20 18:05:33 +0530

    Modified on 20-06-2024

--- src/ARRAYS/MaximumConsectiveOnes.java
+++ src/ARRAYS/MaximumConsectiveOnes.java
@@ -1,4 +1,21 @@
 package ARRAYS;
 
 public class MaximumConsectiveOnes {
+    public static void main(String[] args) {
+        int arr[] = {1,1,0,0,1,1,1,1,0,0,1};
+        System.out.println(maxCon(arr));
+
+    }
+public static int maxCon(int[] arr){
+        int count = 0;
+        int maxCount = 0;
+
+    for (int i = 0; i < arr.length ; i++) {
+        if(arr[i] == 1 ) count++;
+        else count = 0;
+    maxCount = Math.max(maxCount, count);
+    }
+    return  maxCount;
 }
+}
+
