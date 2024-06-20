ae2124edeada7454c5660fc564b817ca532487b1
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-20 06:02:35 +0530

    Modified on 20-06-2024

--- src/ARRAYS/FrequencyElementInArray.java
+++ src/ARRAYS/FrequencyElementInArray.java
@@ -3,6 +3,18 @@ package ARRAYS;
 public class FrequencyElementInArray {
     public static void main(String[] args) {
         int[] nums = {20,20,30,30,30,30};
+
+FreqElement(nums);
     }
-    
+    public static  void FreqElement(int[] nums){
+        int frequency = 1;
+        int i =1;
+        while (i<nums.length){
+            while(i< nums.length && nums[i] == nums[i-1]){
+                frequency += 1;
+                i++;
+            }
+            System.out.println(nums[i-1] + " " + frequency);
+        }
+    }
 }
