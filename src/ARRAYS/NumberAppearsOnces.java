45583b9818e3321f618a17d945bbd43de2392be8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-20 18:22:41 +0530

    Modified on 20-06-2024

--- src/ARRAYS/NumberAppearsOnces.java
+++ src/ARRAYS/NumberAppearsOnces.java
@@ -1,2 +1,11 @@
-package ARRAYS;public class NumberAppearsOnces {
+package ARRAYS;
+
+public class NumberAppearsOnces {
+    public static void main(String[] args) {
+        int arr[] = {1,1,2,2,3,4,4,5,5};
+
+    }
+    public static int AppearsOnces(int arr[]){
+        
+    }
 }
