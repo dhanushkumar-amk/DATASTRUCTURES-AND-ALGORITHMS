b67a7e954376a3b1cc99efe1fa89f3184249d3a8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-20 17:52:53 +0530

    Modified on 20-06-2024

--- src/ARRAYS/missingNumber.java
+++ src/ARRAYS/missingNumber.java
@@ -4,7 +4,7 @@ public class missingNumber {
     public static void main(String[] args) {
         int[] arr = {1,2,3,5};
 //        System.out.println(MissingNumber(arr));
-        int n = 5;
+        int n = arr.length;
         System.out.println(missingNumber(arr,n));
 
     }
