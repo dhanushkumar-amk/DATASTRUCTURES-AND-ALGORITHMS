a0abc38ea3250d81a6b4be04202ccf3db9c21378
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-20 20:31:10 +0530

    Modified on 20-06-2024

--- src/ARRAYS/moveZeroToEnd.java
+++ src/ARRAYS/moveZeroToEnd.java
@@ -3,6 +3,39 @@ package ARRAYS;
 public class moveZeroToEnd {
     public static void main(String[] args) {
         int arr[] = {1,2,0,3,0,0,0,5,4};
+
+        zeroToEnd(arr);
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
-    
+    public static int[] zeroToEnd(int arr[]){
+        int j = -1;
+
+        for ( int i = 0; i < arr.length ; i++) {
+            if(arr[i] == 0){
+                j = i;
+                break;;
+            }
+        }
+
+        for (int i = j+1; i <arr.length ; i++) {
+            if(arr[i] != 0){
+          int start = 0;
+          int end = arr.length-1;
+
+          while (start < end){
+              int temp = arr[start];
+              arr[start] = arr[end];
+              arr[end] = temp;
+              start++;
+              end--;
+          }
+          j++;
+            }
+        }
+    return arr;
+    }
+
+
 }
