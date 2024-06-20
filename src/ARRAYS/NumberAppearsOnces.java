eb6b31cb4305062495107587e52f064b93bb0f9f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-20 18:35:25 +0530

    modified

--- src/ARRAYS/NumberAppearsOnces.java
+++ src/ARRAYS/NumberAppearsOnces.java
@@ -8,6 +8,8 @@ public class NumberAppearsOnces {
     public static int AppearsOnces(int arr[]){
         int xor =0;
         for (int i = 0; i < arr.length; i++) {
+            // note a ^ a = 0;
+            // if any number gets the same name as xor operator it return 0
             xor = xor ^ arr[i];
         }
         return  xor;
