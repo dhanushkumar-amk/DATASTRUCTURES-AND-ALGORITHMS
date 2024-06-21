0007da4ba16277ac483eb35f448d31d9f3907b32
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-21 05:39:25 +0530

    Modified on 21-06-2024

--- src/ARRAYS/BasicLinearSearch.java
+++ src/ARRAYS/BasicLinearSearch.java
@@ -1,2 +1,18 @@
-package ARRAYS;public class BasicLinearSearch {
+package ARRAYS;
+
+public class BasicLinearSearch {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,46,7,6};
+        int target = 46;
+        System.out.println(LinearSearch(arr, target));
+    }
+    public static int LinearSearch(int[] arr, int target){
+
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] == target){
+                return  i;
+            }
+        }
+        return -1;
+    }
 }
