68a8ba21dd1d15be374197f67defbf76f3417027
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-21 05:33:35 +0530

    modified

--- src/SEARCHING/LINERARSEARCH/LinearSearch.java
+++ src/SEARCHING/LINERARSEARCH/LinearSearch.java
@@ -1,2 +1,14 @@
-package SEARCHING.LINERARSEARCH;public class LinearSearch {
+package SEARCHING.LINERARSEARCH;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,46,7,6};
+        int target = 46;
+
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] == target)
+                System.out.println("The value Avalible iin the index of : "+ i);
+        }
+        System.out.println("The value is not avaliable");
+    }
 }
