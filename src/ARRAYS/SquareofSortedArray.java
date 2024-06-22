6e3d3f970df462b05bf7edb8c1f8a0ec99a73776
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-22 11:27:25 +0530

    Modified on 22-06-2024

--- src/ARRAYS/SquareofSortedArray.java
+++ src/ARRAYS/SquareofSortedArray.java
@@ -1,2 +1,8 @@
-package ARRAYS;public class SquareofSortedArray {
+package ARRAYS;
+
+public class SquareofSortedArray {
+    public static void main(String[] args) {
+        int arr[] = {2,4,7,9};
+    }
+    
 }
