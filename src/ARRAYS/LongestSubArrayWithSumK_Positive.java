e1ad7964170168ecb7b00c6e17c708b21ada3091
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-22 08:41:42 +0530

    modified

--- src/ARRAYS/LongestSubArrayWithSumK_Positive.java
+++ src/ARRAYS/LongestSubArrayWithSumK_Positive.java
@@ -2,6 +2,9 @@ package ARRAYS;
 
 public class LongestSubArrayWithSumK_Positive {
     public static void main(String[] args) {
+        int[] arr = {1,2,3,1,1,1,1,4,2,3};
+        int k = 3;
         
     }
+    public static  int subArray(int[] arr, int k){}
 }
