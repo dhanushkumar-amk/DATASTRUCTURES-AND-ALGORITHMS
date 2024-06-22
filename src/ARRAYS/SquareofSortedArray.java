87a03ab0e6517f56762d2186d92288adbc88111e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-22 11:39:30 +0530

    Modified on 22-06-2024

--- src/ARRAYS/SquareofSortedArray.java
+++ src/ARRAYS/SquareofSortedArray.java
@@ -3,6 +3,32 @@ package ARRAYS;
 public class SquareofSortedArray {
     public static void main(String[] args) {
         int arr[] = {2,4,7,9};
+        int[] result = sortedSquare(arr);
+        for (int i = 0; i < result.length ; i++) {
+            System.out.print("The amswer is : " +  result[i]);
+        }
     }
-    
+    public static int[]  sortedSquare(int arr[]){
+        int[] result= new int[arr.length];
+
+        // square the each value in the array
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i] = arr[i] * arr[i];
+        }
+
+        int head = 0;
+        int tail = arr.length-1;
+
+        for (int position = arr.length-1;position >= 0 ; position--) {
+            if(arr[head] > arr[tail] ){
+                result[position] = arr[head];
+            head++;
+            }else {
+                result[position] = arr[tail];
+                tail--;
+            }
+        }
+        return  result;
+
+    }
 }
