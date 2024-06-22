1baf976382f8d0289ebcd429ebfc146532920e7d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-22 09:00:12 +0530

    modified

--- src/ARRAYS/LongestSubArrayWithSumK_PositiveAndNegative.java
+++ src/ARRAYS/LongestSubArrayWithSumK_PositiveAndNegative.java
@@ -1,5 +1,8 @@
 package ARRAYS;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubArrayWithSumK_PositiveAndNegative {
     public static void main(String[] args) {
         int[] arr = {1,2,3,1,1,1,1,4,2,3};
@@ -8,6 +11,26 @@ public class LongestSubArrayWithSumK_PositiveAndNegative {
     }
     public static  int subArray(int[] arr, int k){
 
+        int n = arr.length;
+
+        Map<Integer, Integer>PresumMap = new HashMap<>();
+        int sum =0;
+        int maxLength = 0;
+        for (int i = 0; i <n ; i++) {
+            sum += arr[i];
+
+            if(sum == k){
+                maxLength = Math.max(maxLength, i + 1);
+            }
+
+            int remove = sum - k;
+
+            if(PresumMap.containsKey(remove)){
+                int length =  
+            }
+
+
+        }
 
         return k;
     }
