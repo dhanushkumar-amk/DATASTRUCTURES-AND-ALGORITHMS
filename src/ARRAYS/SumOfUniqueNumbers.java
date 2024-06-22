798ffb8dd95ad1d7a0e1d9be7a44894136d46362
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-22 11:53:19 +0530

    modified

--- src/ARRAYS/SumOfUniqueNumbers.java
+++ src/ARRAYS/SumOfUniqueNumbers.java
@@ -3,7 +3,7 @@ package ARRAYS;
 public class SumOfUniqueNumbers {
     public static void main(String[] args) {
         int arr[] = {1,2,3,2};
-
+        System.out.println(UniqueElementsSum(arr));
     }
     public  static  int UniqueElementsSum(int arr[]){
         int[] frequency = new int[101];
