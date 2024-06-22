f763c294b289b9dbdba9c0a862e3a8756d82e7a4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-22 10:38:06 +0530

    Modified on 22-06-2024

--- src/ARRAYS/RearrangeArrayElementsBySign.java
+++ src/ARRAYS/RearrangeArrayElementsBySign.java
@@ -1,9 +1,35 @@
 package ARRAYS;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class RearrangeArrayElementsBySign {
     public static void main(String[] args) {
-        int[] arr = {3,1,-2,-5,2,-4};
-
+ArrayList<Integer> a = new ArrayList<>(Arrays.asList(1,2,-4,-5,7,-6));
+ArrayList<Integer> ans = RearrangThValueBySign(a);
+        for (int i = 0; i < ans.size() ; i++) {
+            System.out.println(ans.get(i) + " ");
+        }
     }
-    
+    public static ArrayList<Integer> RearrangThValueBySign(ArrayList<Integer> a){
+        int n = a.size();
+
+        ArrayList<Integer> ans = new ArrayList<>(Collections.nCopies(n,0));
+
+        int positiveValue = 0;
+        int negativeValue = 1;
+
+        for (int i = 0; i <n ; i++) {
+            if(a.get(i) < 0){
+                ans.set(negativeValue, a.get(i));
+                negativeValue += 2;
+            }
+            else{
+                ans.set(positiveValue, a.get(i));
+                positiveValue += 2;
+            }
+        }
+        return  ans;
+    }
 }
