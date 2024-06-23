46ed47c17fd08b194ce765e9cb9d9a74f16c13d9
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 17:00:57 +0530

    Modified on 23-06-2024

--- src/ARRAYS/MergeIntravalsInArray.java
+++ src/ARRAYS/MergeIntravalsInArray.java
@@ -1,2 +1,10 @@
-package ARRAYS;public class MergeIntravalsInArray {
+package ARRAYS;
+
+public class MergeIntravalsInArray {
+    public static void main(String[] args) {
+        int[][] nums = {{1,3},{2,6}, {8,10}, {15,18}};
+
+        
+    }
+
 }
