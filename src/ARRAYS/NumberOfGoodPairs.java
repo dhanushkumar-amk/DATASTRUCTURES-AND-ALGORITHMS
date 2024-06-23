31f99a55bc9bfb7db385275769378341b0122437
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 11:02:58 +0530

    Modified on 23-06-2024

--- src/ARRAYS/NumberOfGoodPairs.java
+++ src/ARRAYS/NumberOfGoodPairs.java
@@ -2,6 +2,21 @@ package ARRAYS;
 
 public class NumberOfGoodPairs {
     public static void main(String[] args) {
-        
+        int[] nums= {1,2,3,1,1,3};
+        System.out.println(numInenticalPairs(nums));
+    }
+    public static int numInenticalPairs(int[] nums){
+        int count[] = new int[102];
+
+        for(int num : nums){
+            count[num]++;
+        }
+
+        int totalCount = 0;
+
+        for (int i : count){
+            totalCount += ((i) * (i-1))/2;
+        }
+        return  totalCount;
     }
 }
