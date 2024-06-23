8fb48c716a7fe0a6f56e3b9008d1b9d5096fb1a4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 17:05:55 +0530

    Modified on 23-06-2024

--- src/ARRAYS/MergeIntravalsInArray.java
+++ src/ARRAYS/MergeIntravalsInArray.java
@@ -4,7 +4,12 @@ public class MergeIntravalsInArray {
     public static void main(String[] args) {
         int[][] nums = {{1,3},{2,6}, {8,10}, {15,18}};
 
-        
+
+    }
+    public static int[][] mergeIntervals(int[][] nums){
+        int result = 0;
+
+        return result;
     }
 
 }
