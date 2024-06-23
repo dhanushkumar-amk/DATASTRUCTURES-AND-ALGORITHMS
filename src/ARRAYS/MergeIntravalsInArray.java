87bad7d137fe747914956fe65b0b8e752941c876
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 17:16:09 +0530

    Modified on 23-06-2024

--- src/ARRAYS/MergeIntravalsInArray.java
+++ src/ARRAYS/MergeIntravalsInArray.java
@@ -1,5 +1,10 @@
 package ARRAYS;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class MergeIntravalsInArray {
     public static void main(String[] args) {
         int[][] nums = {{1,3},{2,6}, {8,10}, {15,18}};
@@ -7,9 +12,16 @@ public class MergeIntravalsInArray {
 
     }
     public static int[][] mergeIntervals(int[][] nums){
-        int result = 0;
 
-        return result;
+        if(nums.length <= 1) return  nums;
+
+        Arrays.sort(nums, Comparator.comparingInt(i -> i[0]));
+
+        List<Integer> result = new ArrayList<>();
+
+        int[] newIntervals
+
+        return result
     }
 
 }
