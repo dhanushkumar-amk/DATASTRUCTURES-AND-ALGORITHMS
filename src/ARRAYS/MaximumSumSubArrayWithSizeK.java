9dd0ff2a6811d6c397006a16dc282d771337fed2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 19:53:50 +0530

    modified

--- src/ARRAYS/MaximumSumSubArrayWithSizeK.java
+++ src/ARRAYS/MaximumSumSubArrayWithSizeK.java
@@ -1,2 +1,15 @@
-package ARRAYS;public class MaximumSumSubArrayWithSizeK {
+package ARRAYS;
+
+public class MaximumSumSubArrayWithSizeK {
+    public static void main(String[] args) {
+
+    }
+    public static int maxSubarray(int nums[] , int k){
+
+int maxCount = Integer.MIN_VALUE;
+
+    }
+
+
+
 }
