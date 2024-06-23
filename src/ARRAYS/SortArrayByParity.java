6fa89c35ac28377cbc93b77bb591f9a2777872ad
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 14:01:20 +0530

    Modified on 23-06-2024

--- src/ARRAYS/SortArrayByParity.java
+++ src/ARRAYS/SortArrayByParity.java
@@ -3,8 +3,24 @@ package ARRAYS;
 public class SortArrayByParity {
     public static void main(String[] args) {
         int[] arr = {3,1,2,4};
+        System.out.println(sortOddAndEven(arr));
+
     }
     public static int[] sortOddAndEven(int[] arr){
-        
+        int left = 0;
+        int right =  arr.length-1;
+
+        while(true){
+            while (left < arr.length && arr[left] % 2==0) left++;
+            while(right > 0 && arr[right] %2 ==1) right--;
+
+            if(left > right) break;
+
+            // swap function
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
+        return arr;
     }
 }
