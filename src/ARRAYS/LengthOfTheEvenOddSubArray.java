d4e3851683122f3abc0b5f9a2826431fe89ac1b7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 19:28:54 +0530

    Modified on 23-06-2024

--- src/ARRAYS/LengthOfTheEvenOddSubArray.java
+++ src/ARRAYS/LengthOfTheEvenOddSubArray.java
@@ -1,2 +1,25 @@
-package ARRAYS;public class LengthOfTheEvenOddSubArray {
+package ARRAYS;
+
+public class LengthOfTheEvenOddSubArray {
+    public static void main(String[] args) {
+    int[] arr = {8,10,13,14};
+        System.out.println(evenOdd(arr));
+    }
+
+    public static int evenOdd(int[] arr){
+        int count = 1;
+        int maxCount = 1;
+        for (int i = 1; i <arr.length ; i++) {
+            if(arr[i] % 2 ==0 && arr[i-1] % 2 !=0 || arr[i] % 2 !=0 && arr[i-1] % 2 == 0)
+            {
+                count++;
+                maxCount = Math.max(maxCount, count);
+            }
+            else {
+                count = 1;
+            }
+        }
+        return maxCount;
+    }
+
 }
