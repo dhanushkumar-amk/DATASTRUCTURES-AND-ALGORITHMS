66cb4b504a0f5cff9f0aca758650332ba76a704e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 20:01:47 +0530

    modified

--- src/ARRAYS/MaximumSumSubArrayWithSizeK.java
+++ src/ARRAYS/MaximumSumSubArrayWithSizeK.java
@@ -8,22 +8,18 @@ public class MaximumSumSubArrayWithSizeK {
     }
     public static int maxSubarray(int nums[] , int k){
 
-        int windowSum = 0;
+        int visibleSum = 0;
         int maxSum = Integer.MIN_VALUE;
 
 
         for (int i = 0; i < k ; i++) {
-            windowSum += nums[i];
+            visibleSum += nums[i];
         }
 
         for (int i = k; i <nums.length ; i++) {
-            windowSum = windowSum - nums[i - k] + nums[i];
-            maxSum  = Math.max(maxSum, windowSum);
+            visibleSum = visibleSum - nums[i - k] + nums[i];
+            maxSum  = Math.max(maxSum, visibleSum);
         }
-
         return  maxSum;
     }
-
-
-
 }
