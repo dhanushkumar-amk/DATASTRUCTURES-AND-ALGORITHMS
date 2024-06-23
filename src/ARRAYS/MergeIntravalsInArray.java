1d6243a0cb39ef2421dd0825796c24fda988dc6e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 17:22:48 +0530

    Modified on 23-06-2024

--- src/ARRAYS/MergeIntravalsInArray.java
+++ src/ARRAYS/MergeIntravalsInArray.java
@@ -5,32 +5,31 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-public class MergeIntravalsInArray {
+public class MergeIntervalsInArray {
     public static void main(String[] args) {
-        int[][] nums = {{1,3},{2,6}, {8,10}, {15,18}};
-
-
+        int[][] nums = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] merged = mergeIntervals(nums);
+        System.out.println(Arrays.deepToString(merged));
     }
-    public static int[][] mergeIntervals(int[][] nums){
 
-        if(nums.length <= 1) return  nums;
+    public static int[][] mergeIntervals(int[][] nums) {
+        if (nums.length <= 1) return nums;
 
         Arrays.sort(nums, Comparator.comparingInt(i -> i[0]));
 
-        List<Integer> result = new ArrayList<>();
+        List<int[]> result = new ArrayList<>();
 
-        int[] newIntervals  = nums[0];
-        result.add(newIntervals) ;
+        int[] newInterval = nums[0];
+        result.add(newInterval);
 
-        for(int[] interval : nums){
-            if(interval[0] <= interval[1]);
-                newIntervals[1] = Math.max(newIntervals[1],interval[1]);
-                else{
-                    newIntervals = interval;
-                    result.add(newIntervals);
+        for (int[] interval : nums) {
+            if (interval[0] <= newInterval[1]) {
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            } else {
+                newInterval = interval;
+                result.add(newInterval);
             }
         }
         return result.toArray(new int[result.size()][]);
     }
-
 }
