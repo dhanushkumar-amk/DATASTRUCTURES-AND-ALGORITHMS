444941f5303b33a1b50a3811bfa7bbbaaaec4abc
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 11:34:29 +0530

    Modified on 23-06-2024

--- src/ARRAYS/LeadersInArray.java
+++ src/ARRAYS/LeadersInArray.java
@@ -1,2 +1,12 @@
-package ARRAYS;public class LeadersInArray {
+package ARRAYS;
+
+public class LeadersInArray {
+    public static void main(String[] args) {
+        int[] nums = {10,22,12,3,0,6};
+    }
+    public  static  int[] Leaders(int[] nums){
+
+        return nums;
+    }
 }
+
