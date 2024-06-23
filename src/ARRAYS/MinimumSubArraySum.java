2d1541cf69becfeba6fcfc182cd76c2314720180
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-23 20:20:05 +0530

    modified

--- src/ARRAYS/MinimumSubArraySum.java
+++ src/ARRAYS/MinimumSubArraySum.java
@@ -2,6 +2,11 @@ package ARRAYS;
 
 public class MinimumSubArraySum {
     public static void main(String[] args) {
-        int nums[] =
+        int nums[] ={2,3,1,2,4,3};
+        int target = 7;
+        System.out.println(Minimum(nums,target));
+    }
+    public static int Minimum(int[] nums , int target){
+
     }
 }
