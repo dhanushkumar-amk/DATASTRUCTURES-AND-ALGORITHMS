9ecc31d074372879b3b023745b62b6d2652c750c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 10:38:14 +0530

    Modified on 24-06-2024

--- src/MATRIX/TransposeMatrix.java
+++ src/MATRIX/TransposeMatrix.java
@@ -31,7 +31,6 @@ public class TransposeMatrix {
 
         for (int i = 0; i < matrix.length ; i++) {
             for (int j = 0; j <matrix.length ; j++) {
-
                 result[j][i] = matrix[i][j];
             }
         }
