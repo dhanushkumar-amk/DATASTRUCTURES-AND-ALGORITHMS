47f34e8cdd1d7ef4c9a073c6bad83784be168de3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 13:29:16 +0530

    modified

--- src/MATRIX/SpiralMatrix.java
+++ src/MATRIX/SpiralMatrix.java
@@ -22,7 +22,7 @@ public class SpiralMatrix {
         int left = 0;
         int right = matrix[0].length-1;
 
-        while( top <= bottom && left <= right ){
+        while( top <= bottom && left <= right  ){
             for (int i = left; i <= right; i++) {
                 result.add(matrix[top][i]);
             }
@@ -32,6 +32,10 @@ public class SpiralMatrix {
                 result.add(matrix[i][right]);
             }
             right--;
+
+            if(!(top <= bottom && left <= right)){
+                break;
+            }
 
             for (int i =right; i >= left ; i--) {
                 result.add(matrix[bottom][i]);
