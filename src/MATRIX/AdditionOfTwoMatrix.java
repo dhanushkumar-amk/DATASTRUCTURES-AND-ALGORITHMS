bb080daf5afb3f307583e824826fa05719d5ab83
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 17:42:04 +0530

    Modified on 24-06-2024

--- src/MATRIX/AdditionOfTwoMatrix.java
+++ src/MATRIX/AdditionOfTwoMatrix.java
@@ -5,22 +5,22 @@ public class AdditionOfTwoMatrix {
         int rows = 2, columns = 3;
         int[][] firstMatrix = { {2, 3, 4}, {5, 2, 3} };
         int[][] secondMatrix = { {-4, 5, 3}, {5, 6, 3} };
-
-        // Adding Two matrices
-        int[][] sum = new int[rows][columns];
-        for(int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                sum[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
-            }
-        }
-
-        // Displaying the result
         System.out.println("Sum of two matrices is: ");
+       int[][] sum = additon(firstMatrix,secondMatrix,rows,columns);
         for(int[] row : sum) {
             for (int column : row) {
                 System.out.print(column + "    ");
             }
             System.out.println();
         }
+    }
+    static int[][] additon(int[][] firstMatrix, int[][] secondMatrix, int rows, int columns){
+      int[][] sum = new int[rows][columns];
+        for (int i = 0; i < rows ; i++) {
+            for (int j = 0; j <columns ; j++) {
+                sum[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            }
+        }
+        return sum;
     }
 }
