9ca523f63eddcd1ef3251f17b2fb94bdae49f5d6
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 09:09:26 +0530

    Modified on 24-06-2024

--- src/ARRAYS/MaximumProductAfterKIncrement.java
+++ src/ARRAYS/MaximumProductAfterKIncrement.java
@@ -1,2 +1,38 @@
-package ARRAYS;public class MaximumProductAfterKIncrement {
+package ARRAYS;
+
+import java.util.PriorityQueue;
+
+public class MaximumProductAfterKIncrement {
+    public static void main(String[] args) {
+        int[] arr = {6,3,4,2,5};
+        int k = 4;
+        System.out.println("The maximum Product of the array after k operations is : " + maximumProduct(arr, k));
+    }
+    public  static  int maximumProduct(int[] arr, int k){
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        int mod = 1000000007;
+
+        for (int num : arr)
+            priorityQueue.offer(num);
+
+        for (int i = 0; i <k ; i++) {
+
+            int minElement = priorityQueue.remove();
+            minElement++;
+
+            priorityQueue.offer(minElement);
+
+        }
+
+        long result = 1;
+        while(!priorityQueue.isEmpty())
+        {
+            result *= priorityQueue.poll();
+             result %= mod;
+        }
+        return (int) result;
+
+
+    }
 }
