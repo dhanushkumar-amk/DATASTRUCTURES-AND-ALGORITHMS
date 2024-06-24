19eeeb59f10ae2e7b3cc68417122d40341362ca4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 17:16:11 +0530

    modified

--- src/MATRIX/GenerateSpiralMatrix.java
+++ src/MATRIX/GenerateSpiralMatrix.java
@@ -1,10 +1,46 @@
 package MATRIX;
 
 public class GenerateSpiralMatrix {
+    public static void main(String[] args) {
 
+    }
+    static int[][] generateMatrix(int n){
+        
+        int[][] spiralMatrix = new int[n][n];
+
+        int top = 0;
+        int left= 0;
+        int bottom = n-1;
+        int right = n-1;
+
+        int count = 1;
+        int totalCount = n*n;
+        while(count <= totalCount)
+        {
+            for (int i = top; i <= left && count <= totalCount; i++) {
+                spiralMatrix[top][i] = count++;
+            }
+            top++;
+
+            for (int i = right; i <= bottom && count <= totalCount ; i++) {
+                spiralMatrix[i][right] = count++;
+            }
+            right--;
+
+            for (int i =right ; i <= left && count <= totalCount ; i--) {
+                spiralMatrix[bottom][i] =count++;
+            }
+            bottom++;
+
+            for (int i = bottom; i < top && count <= totalCount; i--) {
+                spiralMatrix[i][left] = count++;
+            }
+        }
+        return  spiralMatrix;
+    }
 }
 
-import java.util.Arrays;
+
 
 class Solution {
     static int[][] generateMatrix(int n) {
