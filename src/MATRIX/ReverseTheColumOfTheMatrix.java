9866f8d928c4186f1828f9f696eac7a25b878058
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 12:42:56 +0530

    Modified on 24-06-2024

--- src/MATRIX/ReverseTheColumOfTheMatrix.java
+++ src/MATRIX/ReverseTheColumOfTheMatrix.java
@@ -8,8 +8,33 @@ public class ReverseTheColumOfTheMatrix {
                 {9,10,11,12},
                 {13,14,15,16}
         };
+        printMatrix(matrix);
+
     }
 
+    public static  void printMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = 0; j < matrix.length ; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
-    
+    static  void  reverseMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length ; i++) {
+            int left = 0;
+            int right = matrix[0].length-1;
+
+            while(left < right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+
+        }
+    }
+
 }
