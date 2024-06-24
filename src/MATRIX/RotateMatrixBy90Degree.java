900658c0d8f64fb4da161e4968c88258d06ee977
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 12:05:24 +0530

    Modified on 24-06-2024

--- src/MATRIX/RotateMatrixBy90Degree.java
+++ src/MATRIX/RotateMatrixBy90Degree.java
@@ -8,11 +8,32 @@ public class RotateMatrixBy90Degree {
                 {9,10,11,12},
                 {13,14,15,16}
         };
-
+        RotateMatrix(matrix);
+        System.out.println("Rotate Image");
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.println(matrix[i][j] + " ");
+            }
+        }
 
     }
-    public static int[][] RotateMatrix(int[][] matrix){
+    public static void RotateMatrix(int[][] matrix){
+        // transpose matrix
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = i; j < matrix[0].length ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
 
-        
+        // reverse the row in the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length/2 ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 -j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
     }
 }
