167e6464aa5e4ff017ff5d2cba4b345e5c0482ef
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 11:51:00 +0530

    Modified on 24-06-2024

--- src/MATRIX/RotateMatrixBy90Degree.java
+++ src/MATRIX/RotateMatrixBy90Degree.java
@@ -1,2 +1,17 @@
-package MATRIX;public class RotateMatrixBy90Degree {
+package MATRIX;
+
+public class RotateMatrixBy90Degree {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+
+
+    }
+    public static int[][] RotateMatrix(int[][] matrix){
+        
+    }
 }
