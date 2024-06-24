70368c2c57db6f835b7180a6d7b842eaaa7c83a6
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 12:07:22 +0530

    modified

--- src/MATRIX/RotateMatrixBy90Degree.java
+++ src/MATRIX/RotateMatrixBy90Degree.java
@@ -21,7 +21,7 @@ public class RotateMatrixBy90Degree {
     public static void RotateMatrix(int[][] matrix){
         // transpose matrix
         for (int i = 0; i < matrix.length ; i++) {
-            for (int j = i; j < matrix[0].length ; j++) {
+            for (int j = i; j < matrix.length ; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
