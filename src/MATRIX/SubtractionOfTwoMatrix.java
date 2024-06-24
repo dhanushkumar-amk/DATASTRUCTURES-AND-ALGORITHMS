d441e3d98be31ee7d3fb2f5a15793413bb222a5f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 17:45:43 +0530

    Modified on 24-06-2024

--- src/MATRIX/SubtractionOfTwoMatrix.java
+++ src/MATRIX/SubtractionOfTwoMatrix.java
@@ -1,2 +1,26 @@
-package MATRIX;public class SubtractionOfTwoMatrix {
+package MATRIX;
+
+public class SubtractionOfTwoMatrix {
+    public static void main(String[] args) {
+        int rows = 2, columns = 3;
+        int[][] firstMatrix = { {2, 3, 4}, {5, 2, 3} };
+        int[][] secondMatrix = { {-4, 5, 3}, {5, 6, 3} };
+        System.out.println("Sum of two matrices is: ");
+        int[][] sum = additon(firstMatrix,secondMatrix,rows,columns);
+        for (int i = 0; i <sum.length; i++) {
+            for (int j = 0; j <sum[0].length ; j++) {
+                System.out.print(sum[i][j] + "    ");
+            }
+            System.out.println();
+        }
+    }
+    static int[][] additon(int[][] firstMatrix, int[][] secondMatrix, int rows, int columns){
+        int[][] sum = new int[rows][columns];
+        for (int i = 0; i < rows ; i++) {
+            for (int j = 0; j <columns ; j++) {
+                sum[i][j] = firstMatrix[i][j] - secondMatrix[i][j];
+            }
+        }
+        return sum;
+    }
 }
