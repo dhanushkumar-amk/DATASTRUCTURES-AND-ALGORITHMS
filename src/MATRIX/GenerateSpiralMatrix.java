48319f6e8a8146975f3d8e910c81f1a3e9dafbf4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 17:28:56 +0530

    modified

--- src/MATRIX/GenerateSpiralMatrix.java
+++ src/MATRIX/GenerateSpiralMatrix.java
@@ -6,8 +6,9 @@ public class GenerateSpiralMatrix {
         int[][] result  =  generateMatrix(n);
         for (int i = 0; i <result.length ; i++) {
             for (int j = 0; j < result.length ; j++) {
-                System.out.print(result[i][j]);
+                System.out.print(result[i][j]+ " ");
             }
+            System.out.println();
         }
 
     }
