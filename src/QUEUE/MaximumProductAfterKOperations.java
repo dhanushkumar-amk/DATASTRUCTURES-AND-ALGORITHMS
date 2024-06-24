ce9544aaf3b7b5cebe3958e910d75d09673238c3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 09:17:45 +0530

    Modified on 24-06-2024

--- src/QUEUE/MaximumProductAfterKOperations.java
+++ src/QUEUE/MaximumProductAfterKOperations.java
@@ -1,8 +1,38 @@
 package QUEUE;
 
+import java.util.PriorityQueue;
+
 public class MaximumProductAfterKOperations {
     public static void main(String[] args) {
-
+        int[] arr = {6,3,4,2,5};
+        int k = 4;
+        System.out.println("The maximum Product of the array after k operations is : " + maximum(arr, k));
     }
-    
+    public static int maximum(int[] arr, int k){
+
+
+        // creating the priority Queue
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        int mod = 1000000007;
+
+        // adding the each element to the priority queue from arr
+        for(int element : arr)
+            priorityQueue.offer(element);
+
+        for (int i = 0; i <k ; i++) {
+
+            int minimumElement = priorityQueue.remove();
+            minimumElement++;
+
+            priorityQueue.offer(minimumElement);
+        }
+
+        long result = 1;
+        while (!priorityQueue.isEmpty())
+        {
+        result *= priorityQueue.poll();
+        result %= mod;
+        }
+    return  (int)  result;
+    }
 }
