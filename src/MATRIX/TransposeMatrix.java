94c55ba6e2bdbc33c3b6c9b2f631761e8d9ba341
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 10:26:16 +0530

    Modified on 24-06-2024

--- src/MATRIX/TransposeMatrix.java
+++ src/MATRIX/TransposeMatrix.java
@@ -15,7 +15,7 @@ public class TransposeMatrix {
     public static  void printMatrix(int[][] matrix){
         for (int i = 0; i < matrix.length ; i++) {
             for (int j = 0; j < matrix.length ; j++) {
-                System.out.print(matrix[i] + " ");
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
