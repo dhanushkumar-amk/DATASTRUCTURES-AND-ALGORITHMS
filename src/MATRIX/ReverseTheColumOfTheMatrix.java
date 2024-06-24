a0b23286d9879de4ae12d48ca70c8cbfb60cf969
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 12:34:53 +0530

    modified

--- src/MATRIX/ReverseTheColumOfTheMatrix.java
+++ src/MATRIX/ReverseTheColumOfTheMatrix.java
@@ -1,2 +1,15 @@
-package MATRIX;public class ReverseTheColumOfTheMatrix {
+package MATRIX;
+
+public class ReverseTheColumOfTheMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+    }
+
+
+    
 }
