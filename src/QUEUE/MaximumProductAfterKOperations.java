864f731cb1a2a10752b1c3e1fe0fb0ec496f1367
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 09:12:15 +0530

    Modified on 24-06-2024

--- src/QUEUE/MaximumProductAfterKOperations.java
+++ src/QUEUE/MaximumProductAfterKOperations.java
@@ -1,2 +1,8 @@
-package QUEUE;public class MaximumProductAfterKOperations {
+package QUEUE;
+
+public class MaximumProductAfterKOperations {
+    public static void main(String[] args) {
+
+    }
+    
 }
