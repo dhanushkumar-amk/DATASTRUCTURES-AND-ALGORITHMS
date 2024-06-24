891d584f9ae78bc56f10fc0aeb741b5ef8dbcd7f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 13:24:31 +0530

    modified

--- src/MATRIX/SpiralMatrix.java
+++ src/MATRIX/SpiralMatrix.java
@@ -1,5 +1,8 @@
 package MATRIX;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpiralMatrix {
     public static void main(String[] args) {
     int[][] matrix = {
@@ -7,8 +10,39 @@ public class SpiralMatrix {
         {4,5,6},
         {7,8,9},
         };
+   List<Integer> spiralOrder =  spiralOrder(matrix);
+        System.out.println(spiralOrder);
     }
-    
 
+     static List<Integer>  spiralOrder(int[][] matrix){
+        ArrayList<Integer> result = new ArrayList<Integer>();
 
+        int top = 0;
+        int bottom = matrix.length-1;
+        int left = 0;
+        int right = matrix[0].length-1;
+
+        while( top <= bottom && left <= right ){
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom ; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            for (int i =right; i >= left ; i--) {
+                result.add(matrix[bottom][i]);
+            }
+            bottom--;
+
+            for (int i = bottom; i >= top ; i--) {
+                result.add(matrix[i][left]);
+            }
+            left++;
+        }
+        return  result;
+    }
 }
