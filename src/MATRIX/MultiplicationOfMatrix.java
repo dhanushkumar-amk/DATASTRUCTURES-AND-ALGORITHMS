289a09d39e34925bdc282a9e4829a2c9c007021c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 16:52:29 +0530

    modified

--- src/MATRIX/MultiplicationOfMatrix.java
+++ src/MATRIX/MultiplicationOfMatrix.java
@@ -3,27 +3,30 @@ package MATRIX;
 public class MultiplicationOfMatrix {
     public static void main(String[] args) {
 
-        int[][] a = {{2,3},
-                {4,5},
+        int[][] a = {
+                {2, 3},
+                {4, 5},
         };
-        int[][] b = {{1,2,3},
-                {4,5,6},
+        int[][] b = {
+                {1, 2, 3},
+                {4, 5, 6},
         };
 
-        int[][] c  = new int[a.length][b[0].length];
-        for (int i = 0; i <a.length ; i++) {
-            for (int j = 0; j <b[0].length ; j++) {
-                for (int k = 0; k < a[0].length ; k++) {
-                    c[i][j] = c[j][j] + a[i][k]* b[k][j];
+        int[][] c = new int[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                for (int k = 0; k < a[0].length; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
                 }
             }
         }
 
-        for (int i = 0; i <a.length ; i++) {
-            for (int j = 0; j <b[0].length ; j++) {
-                System.out.println("the result Matrix is : ");
+        System.out.println("The result Matrix is : ");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
                 System.out.print(c[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
