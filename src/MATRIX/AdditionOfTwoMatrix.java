830ea490c3b815172c88c4355bf4c64a6d3aacc8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 17:36:42 +0530

    Modified on 24-06-2024

--- src/MATRIX/AdditionOfTwoMatrix.java
+++ src/MATRIX/AdditionOfTwoMatrix.java
@@ -2,7 +2,11 @@ package MATRIX;
 
 public class AdditionOfTwoMatrix {
     public static void main(String[] args) {
-        
+        int rows = 2, columns = 3;
+        int[][] firstMatrix = { {2, 3, 4}, {5, 2, 3} };
+        int[][] secondMatrix = { {-4, 5, 3}, {5, 6, 3} };
+
+        int[][] sum = addition(matrix1, )
     }
 
     static int[][] addition(int[][] matrix1, int[][] matrix2, int row, int column){
