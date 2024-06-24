052bc2666073847aedc5c9ae10f26674624f3b16
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 09:59:09 +0530

    Modified on 24-06-2024

--- src/ARRAYS/DisapperingNumbersInArray.java
+++ src/ARRAYS/DisapperingNumbersInArray.java
@@ -36,3 +36,27 @@ public class DisapperingNumbersInArray {
         return result;
     }
 }
+
+
+
+
+/*
+Another solution
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        boolean[] isAvailable = new boolean[nums.length];
+        for(int i=0;i<nums.length;i++){
+            isAvailable[nums[i]-1] = true;
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<isAvailable.length;i++){
+            if(!isAvailable[i]){
+                ans.add(i+1);
+            }
+        }
+        return ans;
+    }
+}
+*/
+
+
