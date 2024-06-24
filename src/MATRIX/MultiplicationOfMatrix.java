3881ca0f91eec6231ee52dbc03b10b0cba5f0d27
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 16:49:40 +0530

    modified

--- src/MATRIX/MultiplicationOfMatrix.java
+++ src/MATRIX/MultiplicationOfMatrix.java
@@ -3,7 +3,27 @@ package MATRIX;
 public class MultiplicationOfMatrix {
     public static void main(String[] args) {
 
-    }
-    
+        int[][] a = {{2,3},
+                {4,5},
+        };
+        int[][] b = {{1,2,3},
+                {4,5,6},
+        };
 
+        int[][] c  = new int[a.length][b[0].length];
+        for (int i = 0; i <a.length ; i++) {
+            for (int j = 0; j <b[0].length ; j++) {
+                for (int k = 0; k < a[0].length ; k++) {
+                    c[i][j] = c[j][j] + a[i][k]* b[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i <a.length ; i++) {
+            for (int j = 0; j <b[0].length ; j++) {
+                System.out.println("the result Matrix is : ");
+                System.out.print(c[i][j] + " ");
+            }
+        }
+    }
 }
