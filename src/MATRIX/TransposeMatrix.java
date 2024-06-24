8e021ad975c6939de321cff58de419145405d8a7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-24 10:25:59 +0530

    Modified on 24-06-2024

--- src/MATRIX/TransposeMatrix.java
+++ src/MATRIX/TransposeMatrix.java
@@ -1,2 +1,28 @@
-package MATRIX;public class TransposeMatrix {
+package MATRIX;
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        //int[][] ans = transpose(matrix);
+        //System.out.println();
+        printMatrix(matrix);
+    }
+
+    public static  void printMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = 0; j < matrix.length ; j++) {
+                System.out.print(matrix[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void transpose(int[][] matrix){
+
+
+    }
 }
