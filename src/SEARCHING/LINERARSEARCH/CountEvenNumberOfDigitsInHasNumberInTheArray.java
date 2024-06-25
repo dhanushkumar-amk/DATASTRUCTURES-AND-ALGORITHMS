da00dd6c000e7e9a0a161550e943d6a6b0856872
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 13:10:31 +0530

    Modified on 25-06-2024

--- src/SEARCHING/LINERARSEARCH/CountEvenNumberOfDigitsInHasNumberInTheArray.java
+++ src/SEARCHING/LINERARSEARCH/CountEvenNumberOfDigitsInHasNumberInTheArray.java
@@ -1,10 +1,15 @@
 package SEARCHING.LINERARSEARCH;
+
+import java.util.Arrays;
+
 // https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
 public class CountEvenNumberOfDigitsInHasNumberInTheArray {
     public static void main(String[] args) {
         int[] nums = {12,345,2,6,7896};
+        int ans = findNumbers(nums);
+        System.out.println(ans);
     }
-    public int findNumbers(int[] nums){
+    public static int findNumbers(int[] nums){
         int count = 0;
         for(int num : nums){
             if(even(num)) count++;
@@ -14,6 +19,12 @@ public class CountEvenNumberOfDigitsInHasNumberInTheArray {
 
     // function to check the given number is even or not
     static  boolean even(int num){
-        
+        int count = 0;
+        while(num > 0){
+            count++;
+            num /= 10;
+        }
+        if(count % 2 == 0) return true;
+        else  return  false;
     }
 }
