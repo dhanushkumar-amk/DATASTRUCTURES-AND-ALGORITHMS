eea4e437f9bee176954c239c82c31a8f0eaf2f9b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 10:22:16 +0530

    modified

--- src/MATRIX/SetMatrixZero.java
+++ src/MATRIX/SetMatrixZero.java
@@ -1,43 +1,73 @@
 package MATRIX;
+import java.util.*;
 
 public class SetMatrixZero {
-    public static void main(String[] args) {
+        static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+            // int[] row = new int[n]; --> matrix[..][0]
+            // int[] col = new int[m]; --> matrix[0][..]
 
-        int[][] a = {
-                {1, 1, 1},
-                {1, 0, 1},
-                {1, 1, 1},
-        };
+            int col0 = 1;
+            // step 1: Traverse the matrix and
+            // mark 1st row & col accordingly:
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (matrix.get(i).get(j) == 0) {
+                        // mark i-th row:
+                        matrix.get(i).set(0, 0);
 
-        
-
-
-    }
-        public void setZeroes(int[][] a{
-            int n=a[0].length, m=a.length;
-            int row[]=new int[m];
-            int col[]=new int[n];
-            int i,j;
-            for(i=0;i<m;i++)
-            {
-                for(j=0;j<n;j++)
-                {
-                    if(a[i][j]==0)
-                    {
-                        row[i]=-1;
-                        col[j]=-1;
+                        // mark j-th column:
+                        if (j != 0)
+                            matrix.get(0).set(j, 0);
+                        else
+                            col0 = 0;
                     }
                 }
             }
-            for(i=0;i<m;i++)
-            {
-                for(j=0;j<n;j++)
-                {
-                    if(row[i]==-1 || col[j]==-1)
-                    {
-                        a[i][j]=0;
+
+            // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+            for (int i = 1; i < n; i++) {
+                for (int j = 1; j < m; j++) {
+                    if (matrix.get(i).get(j) != 0) {
+                        // check for col & row:
+                        if (matrix.get(i).get(0) == 0 || matrix.get(0).get(j) == 0) {
+                            matrix.get(i).set(j, 0);
+                        }
                     }
                 }
+            }
+
+            //step 3: Finally mark the 1st col & then 1st row:
+            if (matrix.get(0).get(0) == 0) {
+                for (int j = 0; j < m; j++) {
+                    matrix.get(0).set(j, 0);
+                }
+            }
+            if (col0 == 0) {
+                for (int i = 0; i < n; i++) {
+                    matrix.get(i).set(0, 0);
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void main(String[] args) {
+            ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+            matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+            matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
+            matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+
+            int n = matrix.size();
+            int m = matrix.get(0).size();
+
+            ArrayList<ArrayList<Integer>> ans = zeroMatrix(matrix, n, m);
+
+            System.out.println("The Final matrix is: ");
+            for (ArrayList<Integer> row : ans) {
+                for (Integer ele : row) {
+                    System.out.print(ele + " ");
+                }
+                System.out.println();
             }
         }
 }
