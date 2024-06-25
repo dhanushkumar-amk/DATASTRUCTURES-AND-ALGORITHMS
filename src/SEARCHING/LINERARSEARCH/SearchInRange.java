a998ef2addad78190905fd5af3b268f3b2d9ae99
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 12:28:50 +0530

    Modified on 25-06-2024

--- src/SEARCHING/LINERARSEARCH/SearchInRange.java
+++ src/SEARCHING/LINERARSEARCH/SearchInRange.java
@@ -2,7 +2,18 @@ package SEARCHING.LINERARSEARCH;
 
 public class SearchInRange {
     public static void main(String[] args) {
+    int[] arr = {1,4,7,9,6,23,2,67};
+    int target = 3;
+    int start = 1;
+    int end = 6;
+        System.out.println(searchinRange(arr,target,start,end));
 
+    }
+    static int searchinRange(int[] arr, int target, int start, int end){
+        for (int i = start; i <=end ; i++) {
+            if (arr[i] == target) return i;
+        }
+        return -1;
     }
 
 }
--- src/STRINGS/SearchInString.java
+++ src/STRINGS/SearchInString.java
@@ -6,6 +6,17 @@ public class SearchInString {
      char target = 'u';
         System.out.println(search2(name,target));
     }
+
+    // one way using for loop
+    static boolean search(String name, char target){
+        if(name.length() == 0) return  false;
+        for (int i = 0; i <name.length() ; i++) {
+            if(target == name.charAt(i)) return true;
+        }
+        return false;
+    }
+
+    // another way using forEach loop
     static boolean search2(String name, char target){
         if(name.length() == 0) return  false;
 
