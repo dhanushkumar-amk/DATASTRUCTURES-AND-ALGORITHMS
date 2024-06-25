2d432815615487d4b93adbd7e61c1a9a7db45812
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 17:57:13 +0530

    Modified on 25-06-2024

--- src/SEARCHING/BINARYSEARCH/CeilOfTheSortedArray.java
+++ src/SEARCHING/BINARYSEARCH/CeilOfTheSortedArray.java
@@ -1,2 +1,27 @@
-package SEARCHING.BINARYSEARCH;public class CeilOfTheSortedArray {
+package SEARCHING.BINARYSEARCH;
+
+public class CeilOfTheSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {10,20,30,40,50};
+        int target = 25;
+        System.out.println(lowerBound(nums,target));
+    }
+    //lower bound is the smallest value that is greater or equal to the target
+    static int lowerBound(int[]nums, int target){
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        int answer = -1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] >= target){
+                answer = mid;
+                high = mid -1;
+            }
+            else low = mid + 1;
+        }
+        return answer;
+    }
+    }
 }
