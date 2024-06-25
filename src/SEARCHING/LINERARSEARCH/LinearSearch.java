52a178271ece2882381030213dea9c585457b030
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 10:49:12 +0530

    modified

--- src/SEARCHING/LINERARSEARCH/LinearSearch.java
+++ src/SEARCHING/LINERARSEARCH/LinearSearch.java
@@ -4,9 +4,9 @@ public class LinearSearch {
     public static void main(String[] args) {
         int arr[] = {1,2,3,46,7,6};
         int target = 46;
-        System.out.println(LinearSearch(arr, target));
+        System.out.println(linearSearch(arr, target));
     }
-        public static int LinearSearch(int[] arr, int target){
+        public static int linearSearch(int[] arr, int target){
 
             for (int i = 0; i <arr.length ; i++) {
                 if(arr[i] == target){
