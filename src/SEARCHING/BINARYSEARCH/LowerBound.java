a0308de289e1b11371391bbbc7e2c261bf812d93
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 17:10:58 +0530

    modified

--- src/SEARCHING/BINARYSEARCH/LowerBound.java
+++ src/SEARCHING/BINARYSEARCH/LowerBound.java
@@ -2,9 +2,25 @@ package SEARCHING.BINARYSEARCH;
 
 public class LowerBound {
     public static void main(String[] args) {
-
+int[] nums = {2,33,44,56,65,67,87,98};
+int target = 68;
+int n = 8;
+        System.out.println(lowerBound(nums,target,n));
     }
-    static int lowerBound(int[]nums, int target){
-        
+    //lower bound is the smallest value that is greater or equal to the target
+    static int lowerBound(int[]nums, int target, int n){
+        int low = 0;
+        int high = n-1;
+        int answer = n;
+
+        while(low <= high){
+int mid = (low + high)/2;
+            if(nums[mid] >= target){
+                answer = mid;
+                high = mid -1;
+            }
+            else low = mid + 1;
+        }
+        return answer;
     }
 }
