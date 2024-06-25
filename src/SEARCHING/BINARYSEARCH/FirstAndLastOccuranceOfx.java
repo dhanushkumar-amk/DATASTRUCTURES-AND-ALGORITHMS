435b4738177194497ac8397e4386845a5dd95421
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 18:16:19 +0530

    Modified on 25-06-2024

--- src/SEARCHING/BINARYSEARCH/FirstAndLastOccuranceOfx.java
+++ src/SEARCHING/BINARYSEARCH/FirstAndLastOccuranceOfx.java
@@ -1,13 +1,56 @@
 package SEARCHING.BINARYSEARCH;
 
+import java.util.Arrays;
+
 public class FirstAndLastOccuranceOfx {
     public static void main(String[] args) {
         int[] nums = {2,4,6,8,8,8,11,13};
-        int x = 8;
+        int target = 8;
+        int n = 8;
+    int[] ans = firstandlast(nums,target,n);
+        System.out.println(Arrays.toString(ans));
     }
-    static  int[] firstandlast(int[] nums, int x){
-        
 
-        return new int[] {-1,-1};
+    // lowerbound
+    static int lowerBound(int[]nums, int target, int n){
+        int low = 0;
+        int high = n-1;
+        int answer = n;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] >= target){
+                answer = mid;
+                high = mid -1;
+            }
+            else low = mid + 1;
+        }
+        return answer;
+    }
+
+    // upperbound
+    static int higherBound(int[] nums,int target, int n){
+        int low = 0;
+        int high = n-1;
+        int answer = n;
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] > target) {
+                answer = mid;
+                high = mid -1;
+            }
+            else low = mid + 1;
+        }
+        return answer;
+    }
+
+
+    static  int[] firstandlast(int[] nums, int target, int n){
+        int lower = lowerBound(nums,target,n);
+        if(lower == n || nums[lower] != target) return new int[] {-1,-1};
+
+        return new  int[]{lower, higherBound(nums,target, n)-1};
+
     }
 }
