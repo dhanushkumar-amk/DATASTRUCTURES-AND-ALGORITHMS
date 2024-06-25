5cf81a93abec431dd48d761d482927013a0e176c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 08:22:05 +0530

    Modified on 25-06-2024

--- src/MATRIX/SetMatrixZero.java
+++ src/MATRIX/SetMatrixZero.java
@@ -3,6 +3,13 @@ package MATRIX;
 public class SetMatrixZero {
     public static void main(String[] args) {
 
+        int[][] matrix = {
+                {1,1,1},
+                {1,0,1},
+                {1,1,1},
+        };
+
+        
     }
-    
+
 }
