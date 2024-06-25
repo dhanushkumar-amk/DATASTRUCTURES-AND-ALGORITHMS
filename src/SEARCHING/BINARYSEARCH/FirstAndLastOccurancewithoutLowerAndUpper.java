db0c9bc9e1e402507d864c73a2b5c763f8a7b33e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 18:40:58 +0530

    Modified on 25-06-2024

--- src/SEARCHING/BINARYSEARCH/FirstAndLastOccurancewithoutLowerAndUpper.java
+++ src/SEARCHING/BINARYSEARCH/FirstAndLastOccurancewithoutLowerAndUpper.java
@@ -1,2 +1,60 @@
-package SEARCHING.BINARYSEARCH;public class FirstAndLastOccurancewithoutLowerAndUpper {
+package SEARCHING.BINARYSEARCH;
+
+import java.util.Arrays;
+
+public class FirstAndLastOccurancewithoutLowerAndUpper {
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 6, 8, 8, 8, 11, 13};
+        int target = 8;
+        int n = 8;
+        int[] ans = firstandlast(nums, target, n);
+        System.out.println(Arrays.toString(ans));
+    }
+    // another way without using lower bound
+    static int firstOccurance(int[] nums, int target, int n) {
+        int low = 0;
+        int high = n - 1;
+        int first = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return first;
+    }
+
+
+    static int lastOccurance(int[] nums, int target, int n) {
+        int low = 0;
+        int high = n - 1;
+        int last = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            } else if (nums[mid] < target) {
+                high = mid - 1;
+            }
+            else  low = mid + 1;
+        }
+        return  last;
+    }
+    static int[] firstandlast(int[] nums, int target, int n){
+        int first  = firstOccurance(nums, target,n);
+        if(first == -1) return new int[] {-1,-1};
+        int last = lastOccurance(nums,target,n);
+        return new int[] {first,last};
+    }
+
+
 }
+
+
