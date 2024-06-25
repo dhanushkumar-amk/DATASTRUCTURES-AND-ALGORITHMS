2b44947fe4faa1b76544e4264dc2d16ac46096a5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 12:54:48 +0530

    modified

--- src/SEARCHING/LINERARSEARCH/SearchIn2DArray.java
+++ src/SEARCHING/LINERARSEARCH/SearchIn2DArray.java
@@ -1,5 +1,7 @@
 package SEARCHING.LINERARSEARCH;
 
+import java.util.Arrays;
+
 public class SearchIn2DArray {
     public static void main(String[] args) {
         int[][] arr = {
@@ -9,14 +11,14 @@ public class SearchIn2DArray {
         };
         int target = 8;
         int[] ans = search(arr,target);
-        System.out.println(ans.toString());
+        System.out.println(Arrays.toString(ans));
     }
     static int[] search(int[][] arr, int target){
         for (int row = 0; row <arr.length ; row++) {
             for (int column = 0; column <arr.length ; column++) {
-                if(arr[row][column] == target) return ;
+                if(arr[row][column] == target) return new int[]{row,column} ;
             }
         }
-        return -1;
+        return new int[]{-1,-1};
     }
 }
