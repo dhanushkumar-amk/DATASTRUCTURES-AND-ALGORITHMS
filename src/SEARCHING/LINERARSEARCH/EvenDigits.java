ba6d028f60adf5c68075c9394858e72dc64e98ab
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 12:58:00 +0530

    modified

--- src/SEARCHING/LINERARSEARCH/EvenDigits.java
+++ src/SEARCHING/LINERARSEARCH/EvenDigits.java
@@ -2,6 +2,9 @@ package SEARCHING.LINERARSEARCH;
 
 public class EvenDigits {
     public static void main(String[] args) {
+        int[] nums = {12,345,2,6,7896};
+    }
+    public int findNumbers(int[] nums){
         
     }
 }
