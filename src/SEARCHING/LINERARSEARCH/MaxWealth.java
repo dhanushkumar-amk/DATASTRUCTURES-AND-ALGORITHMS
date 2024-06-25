5176ec03ec784b474fd1c24f8c64d15967fb7889
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 13:30:14 +0530

    Modified on 25-06-2024

--- src/SEARCHING/LINERARSEARCH/MaxWealth.java
+++ src/SEARCHING/LINERARSEARCH/MaxWealth.java
@@ -17,7 +17,7 @@ int max = Integer.MIN_VALUE;
         for (int person = 0; person <accounts.length ; person++) {
             int rowSum = 0;
             for (int account = 0; account < accounts[person].length ; account++) {
-                rowSum = accounts[person][account];
+                rowSum += accounts[person][account];
             }
             if(rowSum > max){
                 max= rowSum;
