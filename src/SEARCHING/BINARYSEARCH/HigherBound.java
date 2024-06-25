ec7214e43243d454b3a596433019de7ada052016
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 17:22:32 +0530

    Modified on 25-06-2024

--- src/SEARCHING/BINARYSEARCH/HigherBound.java
+++ src/SEARCHING/BINARYSEARCH/HigherBound.java
@@ -8,6 +8,7 @@ public class HigherBound {
         System.out.println(higherBound(nums,target,n));
     }
 // upper bound is the smallest index such that arr[mid]> answer
+    // the smallest number that greater than than the target
     static int higherBound(int[] nums,int target, int n){
         int low = 0;
         int high = n-1;
@@ -15,7 +16,12 @@ public class HigherBound {
 
         while(low <= high) {
             int mid = (low + high) / 2;
-
+            if(nums[mid] > target) {
+                answer = mid;
+                high = mid -1;
+            }
+            else low = mid -1;
         }
+        return answer;
     }
 }
