3a486921d459b5f8eaacdf5e6da1e13d8e26d63c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 10:26:14 +0530

    Modified on 25-06-2024

--- src/MATRIX/RotateMatrixby180Deg.java
+++ src/MATRIX/RotateMatrixby180Deg.java
@@ -4,6 +4,10 @@ public class RotateMatrixby180Deg {
     public static void main(String[] args) {
 
     }
-    public static  
+    public static void rotate(int[][] matrix) {
+        for (int i = n-1; i < ; i++) {
+
+        }
+    }
 }
 
