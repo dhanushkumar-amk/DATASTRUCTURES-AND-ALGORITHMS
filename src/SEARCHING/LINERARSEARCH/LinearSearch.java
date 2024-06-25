4c25b700009449590e613b68c758606b1998a204
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 12:16:24 +0530

    Modified on 25-06-2024

--- src/SEARCHING/LINERARSEARCH/LinearSearch.java
+++ src/SEARCHING/LINERARSEARCH/LinearSearch.java
@@ -8,7 +8,7 @@ public class LinearSearch {
     }
         public static int linearSearch(int[] arr, int target){
 
-        
+
             for (int i = 0; i <arr.length ; i++) {
                 if(arr[i] == target){
                     return  i;
--- src/SEARCHING/LINERARSEARCH/SearchInString.java
+++ src/SEARCHING/LINERARSEARCH/SearchInString.java
@@ -1,5 +1,17 @@
 package SEARCHING.LINERARSEARCH;
 
 public class SearchInString {
+    public static void main(String[] args) {
+        String name = "Dhanush";
+        char target = 'u';
+        System.out.println(search(name , target));
+    }
+    static boolean search(String name, char target){
+        if(name.length() == 0) return  false;
 
+        for (int i = 0; i <name.length() ; i++) {
+            if(target == name.charAt(i)) return true;
+        }
+        return false;
+    }
 }
