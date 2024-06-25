1852d9b71baa4edf4fe94ed4f8fee8cc2e6f1275
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 12:33:21 +0530

    modified

--- src/SEARCHING/LINERARSEARCH/SearchInRange.java
+++ src/SEARCHING/LINERARSEARCH/SearchInRange.java
@@ -10,8 +10,11 @@ public class SearchInRange {
 
     }
     static int searchinRange(int[] arr, int target, int start, int end){
+        // base condition
+        if(arr.length == 0) return -1;
+
         for (int i = start; i <=end ; i++) {
-            if (arr[i] == target) return i+1;
+            if (arr[i] == target) return i;
         }
         return -1;
     }
--- src/SEARCHING/LINERARSEARCH/maximumNumber.java
+++ src/SEARCHING/LINERARSEARCH/maximumNumber.java
@@ -1,2 +1,18 @@
-package SEARCHING.LINERARSEARCH;public class maximumNumber {
+package SEARCHING.LINERARSEARCH;
+
+public class maximumNumber {
+    public static void main(String[] args) {
+        int[] arr = {22,33,55,77,111};
+        System.out.println(maximum(arr));
+    }
+    static int maximum(int[] arr){
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
