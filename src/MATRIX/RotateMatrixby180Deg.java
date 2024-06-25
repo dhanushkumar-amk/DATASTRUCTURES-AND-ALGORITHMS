1ba7f48b906a50181a728b38cc6641368f830e27
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 10:27:52 +0530

    Modified on 25-06-2024

--- src/MATRIX/RotateMatrixby180Deg.java
+++ src/MATRIX/RotateMatrixby180Deg.java
@@ -2,11 +2,15 @@ package MATRIX;
 
 public class RotateMatrixby180Deg {
     public static void main(String[] args) {
+int n = 3;
 
     }
-    public static void rotate(int[][] matrix) {
-        for (int i = n-1; i < ; i++) {
-
+    public static void rotate(int[][] matrix, int n) {
+        for (int i = n-1; i >= 0 ; i--) {
+            for (int j = n-1; j >=0 ; j--) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
