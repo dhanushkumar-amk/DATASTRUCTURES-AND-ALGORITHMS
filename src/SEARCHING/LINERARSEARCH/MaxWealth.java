e51c7ed231164f55a4657d1e9c9b87b94625cc77
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 13:21:50 +0530

    Modified on 25-06-2024

--- src/SEARCHING/LINERARSEARCH/MaxWealth.java
+++ src/SEARCHING/LINERARSEARCH/MaxWealth.java
@@ -2,6 +2,11 @@ package SEARCHING.LINERARSEARCH;
 // https://leetcode.com/problems/richest-customer-wealth/
 public class MaxWealth {
     public static void main(String[] args) {
-        int[][] 
+        int[][] accounts = {
+                {4,5,1},
+                {1,3,3}
+        };
+
     }
+    
 }
