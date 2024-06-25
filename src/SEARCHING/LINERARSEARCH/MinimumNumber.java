503deaa73ccdac45dc211a1848760c6cdc6884f7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 12:36:18 +0530

    modified

--- src/SEARCHING/LINERARSEARCH/MinimumNumber.java
+++ src/SEARCHING/LINERARSEARCH/MinimumNumber.java
@@ -1,2 +1,18 @@
-package SEARCHING.LINERARSEARCH;public class minimumNumberi {
+package SEARCHING.LINERARSEARCH;
+
+public class MinimumNumber {
+    public static void main(String[] args) {
+        int[] arr = {22,33,55,77,111};
+        System.out.println(minimum(arr));
+    }
+    static int minimum(int[] arr){
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
 }
