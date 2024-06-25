fb5f2bec572581d9da71529a0430cd0c888d0060
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 12:22:17 +0530

    Modified on 25-06-2024

--- src/STRINGS/SearchInString.java
+++ src/STRINGS/SearchInString.java
@@ -1,2 +1,19 @@
-package STRINGS;public class SearchInString {
+package STRINGS;
+
+public class SearchInString {
+    public static void main(String[] args) {
+     String name = "Dhanush";
+     char target = 'u';
+        System.out.println(search2(name,target));
+    }
+    static boolean search2(String name, char target){
+        if(name.length() == 0) return  false;
+
+        // using for each loop
+        // first change he given string into array  using toCharArray() method
+
+        for(char ch : name.toCharArray())
+            if(ch == target) return true;
+        return false;
+    }
 }
