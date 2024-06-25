1f1c57265f2ef56f1ad3edb85aea743fa17a7262
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 10:01:19 +0530

    Modified on 25-06-2024

--- src/MATRIX/SetMatrixZero.java
+++ src/MATRIX/SetMatrixZero.java
@@ -12,7 +12,7 @@ public class SetMatrixZero {
         public void setZeroes(int[][] matrix){
 
 
-        
+
             for (int i = 0; i < matrix.length ; i++) {
                 for (int j = 0; j <matrix[0].length ; j++) {
 
