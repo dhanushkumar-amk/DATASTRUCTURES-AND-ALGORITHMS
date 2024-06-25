dbec2f605f1cc341de5a43d5d784c99a85ac9c9e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 17:36:39 +0530

    Modified on 25-06-2024

--- src/SEARCHING/BINARYSEARCH/SearchInsertPosition.java
+++ src/SEARCHING/BINARYSEARCH/SearchInsertPosition.java
@@ -1,4 +1,24 @@
 package SEARCHING.BINARYSEARCH;
 
 public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1,2,4,7};
+        int value = 6;
+        int n = 4;
+        System.out.println();
+    }
+    static int insertPosition(int[] nums, int value, int n){
+        int low = 0;
+        int high = n-1;
+        int answer  = n;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] >= value){
+                answer = mid;
+                high = mid - 1;
+            }
+            else  low = mid + 
+        }
+    }
 }
