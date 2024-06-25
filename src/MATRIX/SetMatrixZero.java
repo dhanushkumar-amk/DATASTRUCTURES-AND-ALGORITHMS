44ce2f4f07ed8c0991723181839dfeed91068158
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 10:12:07 +0530

    Modified on 25-06-2024

--- src/MATRIX/SetMatrixZero.java
+++ src/MATRIX/SetMatrixZero.java
@@ -8,18 +8,33 @@ public class SetMatrixZero {
                 {1, 0, 1},
                 {1, 1, 1},
         };
+
+
     }
         public void setZeroes(int[][] matrix){
+        boolean  firstRow= false, firstColumn = false;
+            for (int i = 0; i < matrix.length ; i++)
+                for (int j = 0; j <matrix[0].length ; j++)
 
+                    if(matrix[i][j] == 0){
+                        if(i == 0) firstRow = true;
+                        if(j == 0) firstColumn = true;
 
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+            for (int i = 0; i < matrix.length ; i++)
+                for (int j = 0; j <matrix[0].length ; j++)
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                        matrix[i][j] = 0;
 
-            for (int i = 0; i < matrix.length ; i++) {
-                for (int j = 0; j <matrix[0].length ; j++) {
+            if(firstRow)
+                for (int j = 0; j <matrix[0].length ; j++)
+                    matrix[0][j]= 0;
 
+            if (firstColumn)
+                for (int i = 0; i < matrix.length ; i++) {
+                    matrix[i][0]= 0;
                 }
-            }
-
         }
-
-
 }
