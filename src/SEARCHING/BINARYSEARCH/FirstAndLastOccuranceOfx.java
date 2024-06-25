7ce8626a3b089503d2bd03d8365ed50ebaa90f4b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-25 18:08:58 +0530

    modified

--- src/SEARCHING/BINARYSEARCH/FirstAndLastOccuranceOfx.java
+++ src/SEARCHING/BINARYSEARCH/FirstAndLastOccuranceOfx.java
@@ -1,2 +1,13 @@
-package SEARCHING.BINARYSEARCH;public class FirstAndLastOccuranceOfx {
+package SEARCHING.BINARYSEARCH;
+
+public class FirstAndLastOccuranceOfx {
+    public static void main(String[] args) {
+        int[] nums = {2,4,6,8,8,8,11,13};
+        int x = 8;
+    }
+    static  int[] firstandlast(int[] nums, int x){
+        
+
+        return new int[] {-1,-1};
+    }
 }
