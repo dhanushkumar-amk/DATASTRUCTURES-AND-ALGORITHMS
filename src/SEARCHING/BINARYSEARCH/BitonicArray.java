26d4c0798a7ad789e2a283abe5555cb946cab7c2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 12:32:18 +0530

    modified

--- src/SEARCHING/BINARYSEARCH/BitonicArray.java
+++ src/SEARCHING/BINARYSEARCH/BitonicArray.java
@@ -2,7 +2,20 @@ package SEARCHING.BINARYSEARCH;
 
 public class BitonicArray {
     public static void main(String[] args) {
+int[] nums = {3,9,10,2017,5,1};
 
     }
-    
+    static  int bitonicElement(int[] nums, int target){
+        int n = nums.length;
+        int low = 0;
+        int high = n -1;
+
+        while (low <= high){
+            int mid = (low + high)/2;
+
+            if()
+        }
+
+        return  -1;
+    }
 }
