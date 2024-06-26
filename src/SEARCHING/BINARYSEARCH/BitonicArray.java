c3ecd5ba5d298d770e5ace3f0a9a08e6fbedf225
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 12:40:43 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/BitonicArray.java
+++ src/SEARCHING/BINARYSEARCH/BitonicArray.java
@@ -1,21 +1,142 @@
 package SEARCHING.BINARYSEARCH;
 
 public class BitonicArray {
-    public static void main(String[] args) {
-int[] nums = {3,9,10,2017,5,1};
+    // Java code to search key in bitonic array
 
-    }
-    static  int bitonicElement(int[] nums, int target){
-        int n = nums.length;
-        int low = 0;
-        int high = n -1;
 
-        while (low <= high){
-            int mid = (low + high)/2;
-
-            if()
+        // Function for binary search
+        // in ascending part
+        static int ascendingBinarySearch(int arr[],
+                                         int low,
+                                         int high,
+                                         int key)
+        {
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (arr[mid] == key)
+                {
+                    return mid;
+                }
+                if (arr[mid] > key)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
         }
 
-        return  -1;
+        // Function for binary search in
+        // descending part of array
+        static int descendingBinarySearch(int arr[],
+                                          int low,
+                                          int high,
+                                          int key)
+        {
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (arr[mid] == key)
+                {
+                    return mid;
+                }
+                if (arr[mid] < key)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+        // finding bitonic point
+        static int findBitonicPoint(int arr[],
+                                    int n,
+                                    int l,
+                                    int r)
+        {
+            int mid;
+            int bitonicPoint = 0;
+            mid = (r + l) / 2;
+            if (arr[mid] > arr[mid - 1]
+                    && arr[mid] > arr[mid + 1])
+            {
+                return mid;
+            }
+            else {
+                if (arr[mid] > arr[mid - 1]
+                        && arr[mid] < arr[mid + 1])
+                {
+                    bitonicPoint = findBitonicPoint(arr, n, mid, r);
+                }
+                else {
+                    if (arr[mid] < arr[mid - 1]
+                            && arr[mid] > arr[mid + 1])
+                    {
+                        bitonicPoint = findBitonicPoint(arr, n, l, mid);
+                    }
+                }
+            }
+            return bitonicPoint;
+        }
+
+        // Function to search key in bitonic array
+        static int searchBitonic(int arr[], int n,
+                                 int key, int index)
+        {
+            if (key > arr[index])
+            {
+                return -1;
+            }
+            else if (key == arr[index])
+            {
+                return index;
+            }
+            else {
+                int temp = ascendingBinarySearch(
+                        arr, 0, index - 1, key);
+                if (temp != -1)
+                {
+                    return temp;
+                }
+
+                // Search in right of k
+                return descendingBinarySearch(arr, index + 1,
+                        n - 1, key);
+            }
+        }
+
+        // Driver code
+        public static void main(String args[])
+        {
+            int arr[] = { -8, 1, 2, 3, 4, 5, -2, -3 };
+            int key = 5;
+            int n, l, r;
+            n = arr.length;
+            l = 0;
+            r = n - 1;
+            int index;
+            index = findBitonicPoint(arr, n, l, r);
+
+            int x = searchBitonic(arr, n, key, index);
+
+            if (x == -1) {
+                System.out.println("Element Not Found");
+            }
+            else {
+                System.out.println("Element Found at index "
+                        + x);
+            }
+        }
     }
-}
+
+
+
+
