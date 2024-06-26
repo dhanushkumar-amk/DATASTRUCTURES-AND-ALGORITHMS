84ef910cb59cf23d58766281fc5d1a7759b7ae05
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 08:06:21 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/CountOccuranceOfTheElementsInArray.java
+++ src/SEARCHING/BINARYSEARCH/CountOccuranceOfTheElementsInArray.java
@@ -3,59 +3,63 @@ package SEARCHING.BINARYSEARCH;
 import java.util.Arrays;
 
 public class CountOccuranceOfTheElementsInArray {
-    public static void main(String[] args) {
-        int[] nums = {2, 4, 6, 8, 8, 8, 11, 13};
-        int target = 8;
-        int n = 8;
-        int[] ans = firstandlast(nums, target, n);
-        System.out.println(Arrays.toString(ans));
-    }
-    // another way without using lower bound
-    static int firstOccurance(int[] nums, int target, int n) {
-        int low = 0;
-        int high = n - 1;
+    public static int firstOccurrence(int[] arr, int n, int k) {
+        int low = 0, high = n - 1;
         int first = -1;
+
         while (low <= high) {
             int mid = (low + high) / 2;
-
-            if (nums[mid] == target) {
+            // maybe an answer
+            if (arr[mid] == k) {
                 first = mid;
+                // look for smaller index on the left
                 high = mid - 1;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
+            } else if (arr[mid] < k) {
+                low = mid + 1; // look on the right
             } else {
-                high = mid - 1;
+                high = mid - 1; // look on the left
             }
         }
         return first;
     }
 
-
-    static int lastOccurance(int[] nums, int target, int n) {
-        int low = 0;
-        int high = n - 1;
+    public static int lastOccurrence(int[] arr, int n, int k) {
+        int low = 0, high = n - 1;
         int last = -1;
+
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (nums[mid] == target) {
+            // maybe an answer
+            if (arr[mid] == k) {
                 last = mid;
+                // look for larger index on the right
                 low = mid + 1;
-            } else if (nums[mid] < target) {
-                high = mid - 1;
+            } else if (arr[mid] < k) {
+                low = mid + 1; // look on the right
+            } else {
+                high = mid - 1; // look on the left
             }
-            else  low = mid + 1;
         }
-        return  last;
-    }
-    static int[] firstandlast(int[] nums, int target, int n){
-        int first  = firstOccurance(nums, target,n);
-        if(first == -1) return new int[] {-1,-1};
-        int last = lastOccurance(nums,target,n);
-        return new int[] {first,last};
+        return last;
     }
 
-  public static  int[] count(int[] nums, int target, int n){
-        int[] ans = firstandlast(nums,target,n);
-    if(ans[0].firstOccurance == -1)
+    public static int[] firstAndLastPosition(int[] arr, int n, int k) {
+        int first = firstOccurrence(arr, n, k);
+        if (first == -1) return new int[] { -1, -1};
+        int last = lastOccurrence(arr, n, k);
+        return new int[] {first, last};
+    }
+
+    public static int count(int arr[], int n, int x) {
+        int[] ans = firstAndLastPosition(arr, n, x);
+        if (ans[0] == -1) return 0;
+        return (ans[1] - ans[0] + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr =  {2, 4, 6, 8, 8, 8, 11, 13};
+        int n = 8, x = 8;
+        int ans = count(arr, n, x);
+        System.out.println("The number of occurrences is: " + ans);
     }
 }
