6b4be5adbb3d26299803b02df9c414ca502f9fbf
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 17:42:37 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/FindSquareOfAnNumber.java
+++ src/SEARCHING/BINARYSEARCH/FindSquareOfAnNumber.java
@@ -1,14 +1,27 @@
 package SEARCHING.BINARYSEARCH;
 
 public class FindSquareOfAnNumber {
-    public static void main(String[] args) {
-
-    }
-    static int  squareNumber(int[] nums){
-        int low = 1;
-        int high = nums.length
-
+    public static int floorSqrt(int n) {
+        int low = 1, high = n;
+        //Binary search on the answers:
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            long val = mid * mid;
+            if (val <= (long)(n)) {
+                //eliminate the left half:
+                low = (int)(mid + 1);
+            } else {
+                //eliminate the right half:
+                high = (int)(mid - 1);
+            }
+        }
         return high;
+    }
+
+    public static void main(String[] args) {
+        int n = 28;
+        int ans = floorSqrt(n);
+        System.out.println("The floor of square root of " + n + " is: " + ans);
     }
 
 }
