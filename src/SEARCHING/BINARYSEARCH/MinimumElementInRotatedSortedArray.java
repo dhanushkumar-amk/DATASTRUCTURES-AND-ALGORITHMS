f4140848786683d8db715485c3c38d967a269960
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 10:00:04 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/MinimumElementInRotatedSortedArray.java
+++ src/SEARCHING/BINARYSEARCH/MinimumElementInRotatedSortedArray.java
@@ -1,2 +1,43 @@
-package SEARCHING.BINARYSEARCH;public class MinimumElementInRotatedSortedArray {
+package SEARCHING.BINARYSEARCH;
+
+public class MinimumElementInRotatedSortedArray {
+    public static int findMin(int []arr) {
+        int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //search space is already sorted
+            //then arr[low] will always be
+            //the minimum in that search space:
+            if (arr[low] <= arr[high]) {
+                ans = Math.min(ans, arr[low]);
+                break;
+            }
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                // keep the minimum:
+                ans = Math.min(ans, arr[low]);
+
+                // Eliminate left half:
+                low = mid + 1;
+
+            } else { //if right part is sorted:
+
+                // keep the minimum:
+                ans = Math.min(ans, arr[mid]);
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int ans = findMin(arr);
+        System.out.println("The minimum element is: " + ans );
+    }
 }
