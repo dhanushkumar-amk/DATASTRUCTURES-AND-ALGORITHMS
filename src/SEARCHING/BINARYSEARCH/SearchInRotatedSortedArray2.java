67b87336a2443cf194f0503addec63d2014faa1b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 09:34:16 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/SearchInRotatedSortedArray2.java
+++ src/SEARCHING/BINARYSEARCH/SearchInRotatedSortedArray2.java
@@ -26,8 +26,19 @@ public class SearchInRotatedSortedArray2 {
                 high = mid - 1;
                 continue;
             }
-
-            
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target <=arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (arr[mid] <= target && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
         }
         return false;
     }
