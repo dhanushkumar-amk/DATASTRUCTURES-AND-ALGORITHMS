fb2b9d74a9e400b5ed7a64f651a6f2f58ea9a76e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 08:11:35 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/SearchInRotatedSortedArray1.java
+++ src/SEARCHING/BINARYSEARCH/SearchInRotatedSortedArray1.java
@@ -4,11 +4,14 @@ import  java.util.*;
 public class SearchInRotatedSortedArray1 {
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(7, 8, 9, 1, 2, 3, 4, 5, 6));
-        int n = 9, k = 1;
+        int n = 9, target = 1;
         int ans = search(arr, n, k);
         if (ans == -1)
             System.out.println("Target is not present.");
         else
             System.out.println("The index is: " + ans);
+    }
+    public static int search(ArrayList<Integer> arr, int n, int target){
+        
     }
 }
