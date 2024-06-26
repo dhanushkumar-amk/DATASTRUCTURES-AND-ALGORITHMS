dcf241de20e31a7691af2c9269d9e1aef97ba215
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 12:50:55 +0530

    modified

--- src/SEARCHING/BINARYSEARCH/PeakElementINArray.java
+++ src/SEARCHING/BINARYSEARCH/PeakElementINArray.java
@@ -3,8 +3,6 @@ import java.util.*;
 public class PeakElementINArray {
     public static int findPeakElement(ArrayList<Integer> arr) {
         int n = arr.size(); // Size of array
-
-        // Edge cases:
         if (n == 1) return 0;
         if (arr.get(0) > arr.get(1)) return 0;
         if (arr.get(n - 1) > arr.get(n - 2)) return n - 1;
@@ -12,13 +10,13 @@ public class PeakElementINArray {
         int low = 1, high = n - 2;
         while (low <= high) {
             int mid = (low + high) / 2;
-if(arr.get(mid) < arr.get(mid -1) && arr.get(mid) > arr.get(mid+1)) return mid;
+            if (arr.get(mid) < arr.get(mid - 1) && arr.get(mid) > arr.get(mid + 1)) return mid;
 
-if(arr.get(mid) > arr.get(mid-1)) low = mid + 1;
-else high = mid -1;
-        return -1;
+            if (arr.get(mid) > arr.get(mid - 1)) low = mid + 1;
+            else high = mid - 1;
+            return -1;
+        }
     }
-
     public static void main(String[] args) {
         ArrayList<Integer> arr =
                 new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 5, 1));
