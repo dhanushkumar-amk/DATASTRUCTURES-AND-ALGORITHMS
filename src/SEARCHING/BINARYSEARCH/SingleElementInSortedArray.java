d3ad29f477f9802013b2e1766e76e1a6a3b86cfc
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 10:46:42 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/SingleElementInSortedArray.java
+++ src/SEARCHING/BINARYSEARCH/SingleElementInSortedArray.java
@@ -6,12 +6,19 @@ public class SingleElementInSortedArray {
         int[] nums = {1,1,2,2,3,4,4,5,5};
         System.out.println(singleElement(nums));
     }
-static int singleElement(int[] nums){
+
+
+
+    // simple way
+        static int singleElement(int[] nums){
         int xor = 0;
-    for(int i = 0; i <nums.length ; i++) {
+        for(int i = 0; i <nums.length ; i++) {
         xor = xor ^ nums[i];
     }
     return xor;
 }
+
+// another way using binary search
+
 }
 
