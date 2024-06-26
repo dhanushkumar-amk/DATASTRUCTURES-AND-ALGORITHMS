26483247188ca1c59ce09a2adc013fe61de31748
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 17:40:42 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/FindSquareOfAnNumber.java
+++ src/SEARCHING/BINARYSEARCH/FindSquareOfAnNumber.java
@@ -4,5 +4,11 @@ public class FindSquareOfAnNumber {
     public static void main(String[] args) {
 
     }
+    static int  squareNumber(int[] nums){
+        int low = 1;
+        int high = nums.length
+
+        return high;
+    }
 
 }
