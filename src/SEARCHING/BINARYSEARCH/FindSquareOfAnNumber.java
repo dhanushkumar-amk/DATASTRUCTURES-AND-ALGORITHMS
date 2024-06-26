8d057bd3ba8ecd88b67a62b889b555c88a281516
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 20:04:45 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/FindSquareOfAnNumber.java
+++ src/SEARCHING/BINARYSEARCH/FindSquareOfAnNumber.java
@@ -10,22 +10,26 @@ public class FindSquareOfAnNumber {
      public static int floorSqrt(int n) {
 
     // iif the n is 0 or 1
-    // if(n==1 || n == 0) return n;
+    if(n==1 || n == 0) return n;
 
-    int low = 2;
+    int low = 1;
         int high = n;
         int ans = 0;
 
         while (low <= high){
         int mid = (low + high)/2;
 
-        if(mid * mid == n) return mid;
+        if(mid * mid == n){
+            return mid;
+        }
         else if(mid * mid > n){
             low = mid + 1;
             ans = mid;
         }
-        else high = mid - 1;
+        else{
+            high = mid - 1;
+            }
         }
-            return ans;
+         return ans;
     }
 }
