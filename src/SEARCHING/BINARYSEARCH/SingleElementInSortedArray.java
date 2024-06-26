fdd4b6044eb3375a9f215508f5cc0bc740c4ebd9
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 12:08:26 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/SingleElementInSortedArray.java
+++ src/SEARCHING/BINARYSEARCH/SingleElementInSortedArray.java
@@ -21,10 +21,24 @@ public class SingleElementInSortedArray {
 
 // another way using binary search
 static int SingleElement(int[] nums){
+        int n = nums.length;
+        if(n == 1) return  nums[0];
 
+        if(nums[0] != nums[1]) return  nums[0];
+        if(nums[n-1] != nums[n-2]) return  nums[n-1];
 
-        return;
-}
+        int low = 0;
+        int high = n-1;
+        while (low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return  nums[mid];
+
+            if(nums[mid] % 2 == 1 && nums[mid - 1] == nums[mid] || nums[mid] % 2==0 && nums[mid] == nums[mid + 1]) low = mid + 1;
+            else  high = mid - 1;
+        }
+    return  -1;
+    }
 
 }
 
