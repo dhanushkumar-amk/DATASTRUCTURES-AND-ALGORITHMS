30749b050cc9d9f26fb256cd44bd31c5edd77e61
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 08:52:49 +0530

    Modified on 26-06-2024

--- src/SEARCHING/BINARYSEARCH/SearchInRotatedSortedArray2.java
+++ src/SEARCHING/BINARYSEARCH/SearchInRotatedSortedArray2.java
@@ -1,5 +1,17 @@
 package SEARCHING.BINARYSEARCH;
 
 public class SearchInRotatedSortedArray2 {
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+        int target = 3;
+        boolean ans = searchInARotatedSortedArrayII(arr, target);
+        if (ans == false)
+            System.out.println("Target is not present.");
+        else
+            System.out.println("Target is present in the array.");
+    }
+
     
+
+
 }
