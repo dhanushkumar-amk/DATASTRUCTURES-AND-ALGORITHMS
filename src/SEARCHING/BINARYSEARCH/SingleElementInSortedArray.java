ea984bfd44cf8d2a6c8ad666ea897941a6d53f69
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-26 10:42:14 +0530

    modified

--- src/SEARCHING/BINARYSEARCH/SingleElementInSortedArray.java
+++ src/SEARCHING/BINARYSEARCH/SingleElementInSortedArray.java
@@ -1,5 +1,14 @@
 package SEARCHING.BINARYSEARCH;
 
 public class SingleElementInSortedArray {
-    
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,2,3,4,4,5,5,5};
+
+    }
+static int singleElement(int[] nums){
+        int xor = 0;
+        
 }
+}
+
