08bb2320a974026e88ea3c5b8c20f97a32da87f7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-27 05:39:18 +0530

    modified

--- src/SEARCHING/BINARYSEARCH/KOKOEatingBanana.java
+++ src/SEARCHING/BINARYSEARCH/KOKOEatingBanana.java
@@ -1,8 +1,47 @@
 package SEARCHING.BINARYSEARCH;
 
 public class KOKOEatingBanana {
-    public static void main(String[] args) {
+    public static int findMax(int[] v) {
+        int maxi = Integer.MIN_VALUE;;
+        int n = v.length;
+        //find the maximum:
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, v[i]);
+        }
+        return maxi;
+    }
 
+    public static int calculateTotalHours(int[] v, int hourly) {
+        int totalH = 0;
+        int n = v.length;
+        //find total hours:
+        for (int i = 0; i < n; i++) {
+            totalH += Math.ceil((double)(v[i]) / (double)(hourly));
+        }
+        return totalH;
+    }
+
+    public static int minimumRateToEatBananas(int[] v, int h) {
+        int low = 1, high = findMax(v);
+
+        //apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int totalH = calculateTotalHours(v, mid);
+            if (totalH <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        int[] v = {7, 15, 6, 3};
+        int h = 8;
+        int ans = minimumRateToEatBananas(v, h);
+        System.out.println("Koko should eat at least " + ans + " bananas/hr.");
     }
 
 }
