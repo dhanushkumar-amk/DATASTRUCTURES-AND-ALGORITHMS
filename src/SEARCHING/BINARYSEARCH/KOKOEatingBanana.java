8c6a65de5c09d71c76824a915d1878b278fec894
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-27 05:52:38 +0530

    Modified on 27-06-2024

--- src/SEARCHING/BINARYSEARCH/KOKOEatingBanana.java
+++ src/SEARCHING/BINARYSEARCH/KOKOEatingBanana.java
@@ -44,11 +44,36 @@ public class KOKOEatingBanana {
         System.out.println("Koko should eat at least " + ans + " bananas/hr.");
     }
 
-    public static void main(String[] args) {
-        int[] v = {7, 15, 6, 3};
-        int h = 8;
-        int ans = minimumRateToEatBananas(v, h);
-        System.out.println("Koko should eat at least " + ans + " bananas/hr.");
-    }
+
 
 }
+
+/*
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        long sum = 0;
+        for (int n : piles) {
+            sum += n;
+        }
+        int min = (int) Math.ceilDiv(sum, h);
+        int max = (int) Math.ceilDiv(sum, h - piles.length + 1);
+        int mid = 0;
+        while (min < max) {
+            mid = (max + min)/2;
+            int time = 0;
+            for (int i : piles) {
+                time += (i + mid - 1)/mid;
+                if (time > h) {
+                    break;
+                }
+            }
+            if (time > h) {
+                min = mid + 1;
+            } else {
+                max = mid;
+            }
+        }
+        return max;
+    }
+}*/
