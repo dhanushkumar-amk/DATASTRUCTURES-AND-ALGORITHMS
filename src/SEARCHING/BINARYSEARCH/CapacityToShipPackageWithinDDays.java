a1b03eba2cbad0db9071f648d09895ffb7cc4a63
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-28 20:31:07 +0530

    Modified on 28-06-2024

--- src/SEARCHING/BINARYSEARCH/CapacityToShipPackageWithinDDays.java
+++ src/SEARCHING/BINARYSEARCH/CapacityToShipPackageWithinDDays.java
@@ -1,5 +1,47 @@
 package SEARCHING.BINARYSEARCH;
 
 public class CapacityToShipPackageWithinDDays {
-    
+    public static int findDays(int[] weights, int cap) {
+        int days = 1; //First day.
+        int load = 0;
+        int n = weights.length; //size of array.
+        for (int i = 0; i < n; i++) {
+            if (load + weights[i] > cap) {
+                days += 1; //move to next day
+                load = weights[i]; //load the weight.
+            } else {
+                //load the weight on the same day.
+                load += weights[i];
+            }
+        }
+        return days;
+    }
+
+    public static int leastWeightCapacity(int[] weights, int d) {
+        //Find the maximum and the summation:
+        int low = Integer.MIN_VALUE, high = 0;
+        for (int i = 0; i < weights.length; i++) {
+            high += weights[i];
+            low = Math.max(low, weights[i]);
+        }
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int numberOfDays = findDays(weights, mid);
+            if (numberOfDays <= d) {
+                //eliminate right half
+                high = mid - 1;
+            } else {
+                //eliminate left half
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+        int[] weights = {5, 4, 5, 2, 3, 4, 5, 6};
+        int d = 5;
+        int ans = leastWeightCapacity(weights, d);
+        System.out.println("The minimum capacity should be: " + ans);
+    }
 }
