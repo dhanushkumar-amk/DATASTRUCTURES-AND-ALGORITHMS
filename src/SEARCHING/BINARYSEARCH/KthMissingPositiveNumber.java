fe9886fa7f97e4c2f2f679a23f90a5a13ffd18d8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-29 10:22:46 +0530

    Modified on 29-06-2024

--- src/SEARCHING/BINARYSEARCH/KthMissingPositiveNumber.java
+++ src/SEARCHING/BINARYSEARCH/KthMissingPositiveNumber.java
@@ -21,3 +21,28 @@ public class KthMissingPositiveNumber {
         System.out.println("The missing number is: " + ans);
     }
 }
+
+
+// another simple solution
+
+/*
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high){
+            int mid =(low + high)/2;
+
+            if(arr[mid] - mid - 1 < k){
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return k + high + 1;
+    }
+}
+ */
