caa0ab97b968093467e7025d8421f919534507f5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-29 13:02:38 +0530

    Modified on 29-06-2024

--- src/SEARCHING/BINARYSEARCH/SearchIn2DArray.java
+++ src/SEARCHING/BINARYSEARCH/SearchIn2DArray.java
@@ -1,5 +1,17 @@
 package SEARCHING.BINARYSEARCH;
-
+import java.util.*;
 public class SearchIn2DArray {
-    
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        matrix.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4)));
+        matrix.add(new ArrayList<>(Arrays.asList(5, 6, 7, 8)));
+        matrix.add(new ArrayList<>(Arrays.asList(9, 10, 11, 12)));
+
+        boolean result = searchMatrix(matrix, 8);
+        System.out.println(result ? "true" : "false");
+    }
+
+    static boolean searchMatrix(ArrayList<Integer> matrix, int target)
+
 }
