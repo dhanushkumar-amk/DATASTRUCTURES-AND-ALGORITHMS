100c0df3746cb071da7a9b3fd3ad7f028aa80728
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-29 08:40:51 +0530

    modified

--- src/SEARCHING/BINARYSEARCH/KthMissingPositiveNumber.java
+++ src/SEARCHING/BINARYSEARCH/KthMissingPositiveNumber.java
@@ -1,10 +1,24 @@
 package SEARCHING.BINARYSEARCH;
 
 public class KthMissingPositiveNumber {
+    public static int missingK(int[] vec, int n, int k) {
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int missing = vec[mid] - (mid + 1);
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return k + high + 1;
+    }
     public static void main(String[] args) {
-    int[] weight = {1,2,3,4,5,6,7,8,9,10};
-    int days = 5;
-        System.out.println();
+        int[] vec = {4, 7, 9, 10};
+        int n = 4, k = 4;
+        int ans = missingK(vec, n, k);
+        System.out.println("The missing number is: " + ans);
     }
 
 
