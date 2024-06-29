4646217c115d2a4e478a15ce860646fe90457e38
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-29 12:47:16 +0530

    Modified on 29-06-2024

--- src/SEARCHING/BINARYSEARCH/PaintersPartions.java
+++ src/SEARCHING/BINARYSEARCH/PaintersPartions.java
@@ -2,7 +2,50 @@ package SEARCHING.BINARYSEARCH;
 
 public class PaintersPartions {
     public static void main(String[] args) {
+int[] arr = {10,20,30,40};
+int a = 2;
+int b = 2;
+        System.out.println();
+    }
+    static boolean isPossible(int[] arr, int b, int mid){
+        int painters = 1;
+        int pbc = 0;
 
+        for (int i = 0; i <arr.length; i++) {
+            if(arr[i] > mid) return false;
+
+            if(pbc + arr[i] <= mid) {
+                pbc += arr[i];
+            }
+            else {
+                painters++;
+                if(painters > b) return false;
+           pbc = arr[i];
+            }
+            return true;
+        }
+    }
+
+    static  int maxTime(int[]arr, int a, int b){
+
+        int low = 0;
+        int high = 0;
+
+        for (int i = 0; i <arr.length ; i++) {
+            high += arr[i];
+        }
+        int res = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(isPossible(arr, b,mid)){
+                res = mid;
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
     }
 
 
