abec1b9531005260efa516778f55fb88e22937fa
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-29 08:30:18 +0530

    Modified on 29-06-2024

--- src/SEARCHING/BINARYSEARCH/KthMissingPositiveNumber.java
+++ src/SEARCHING/BINARYSEARCH/KthMissingPositiveNumber.java
@@ -1,2 +1,11 @@
-package SEARCHING.BINARYSEARCH;public class KthMissingPositiveNumber {
+package SEARCHING.BINARYSEARCH;
+
+public class KthMissingPositiveNumber {
+    public static void main(String[] args) {
+    int[] weight = {1,2,3,4,5,6,7,8,9,10};
+    int days = 5;
+        System.out.println();
+    }
+    
+
 }
