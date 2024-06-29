f31b82d99418dc2c2f8e741b61edfc49788bbaa3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-29 13:47:29 +0530

    Modified on 29-06-2024

--- src/SEARCHING/BINARYSEARCH/SearchIn2DMatrix2.java
+++ src/SEARCHING/BINARYSEARCH/SearchIn2DMatrix2.java
@@ -1,11 +1,27 @@
 package SEARCHING.BINARYSEARCH;
-
+import java.util.*;
 public class SearchIn2DMatrix2 {
 
-    
-    int n = matrix.size();
-    int m = matrix.get(0).size();
-    int row = 0, col = m - 1;
+    public static boolean searchElement(ArrayList<ArrayList<Integer>> matrix, int target) {
+        int n = matrix.size();
+        int m = matrix.get(0).size();
+
+        int row = 0;
+        int col = m-1;
+
+        while()
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        matrix.add(new ArrayList<>(Arrays.asList(1, 4, 7, 11, 15)));
+        matrix.add(new ArrayList<>(Arrays.asList(2, 5, 8, 12, 19)));
+        matrix.add(new ArrayList<>(Arrays.asList(3, 6, 9, 16, 22)));
+        matrix.add(new ArrayList<>(Arrays.asList(10, 13, 14, 17, 24)));
+        matrix.add(new ArrayList<>(Arrays.asList(18, 21, 23, 26, 30)));
+
+        boolean result = searchElement(matrix, 8);
+        System.out.println(result ? "true" : "false");
+    }
 
 
 }
