019adedf3dcff72294b7c3f3fe57e61b392388cd
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-30 18:48:27 +0530

    Modified on 30-06-2024

--- src/SEARCHING/BINARYSEARCH/MedianOfTheMatrix.java
+++ src/SEARCHING/BINARYSEARCH/MedianOfTheMatrix.java
@@ -1,2 +1,18 @@
-package SEARCHING.BINARYSEARCH;public class MedianOfTheMatrix {
+package SEARCHING.BINARYSEARCH;
+
+public class MedianOfTheMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {5, 11, 20, 35, 45},
+                {1,2,3,4,5},
+                {13,15,16,19,21},
+        };
+        int median = findMedian(matrix);
+        System.out.println(median);
+    }
+
+    static int findMedian(int[][] matrix){
+
+        // find min and ma
+    }
 }
