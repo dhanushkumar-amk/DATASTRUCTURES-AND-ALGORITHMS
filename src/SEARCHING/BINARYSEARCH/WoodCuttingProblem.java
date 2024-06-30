088070e886a9fda5535f04e9ad98910fdb487250
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-30 19:30:25 +0530

    modified

--- src/SEARCHING/BINARYSEARCH/WoodCuttingProblem.java
+++ src/SEARCHING/BINARYSEARCH/WoodCuttingProblem.java
@@ -2,4 +2,10 @@ package SEARCHING.BINARYSEARCH;
 
 public class WoodCuttingProblem {
 
+    public static void main(String[] args) {
+        int[] treeHeight = {20,15,10,17};
+        int target = 7;
+    
+    }
+
 }
