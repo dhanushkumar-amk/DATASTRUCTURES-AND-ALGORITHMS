296ddf38060bfe17c16aecc138ae076ae05eec9c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-30 19:02:46 +0530

    modified

--- src/SEARCHING/BINARYSEARCH/MedianOfTheMatrix.java
+++ src/SEARCHING/BINARYSEARCH/MedianOfTheMatrix.java
@@ -13,6 +13,39 @@ public class MedianOfTheMatrix {
 
     static int findMedian(int[][] matrix){
 
-        // find min and ma
+        // init min and max value first value
+        int min = matrix[0][0];
+        int max = matrix[0][0];
+        int medianPosition = (matrix.length * matrix[0].length + 1)/2;
+        // find min and max value
+        for (int i = 0; i <matrix.length ; i++) {
+            if(matrix[i][0] < min) min = matrix[i][0];
+            if(matrix[i][matrix[0].length-1] > max) max = matrix[i][matrix[0].length-1];
+        }
+
+        // binary search
+        while(min < max){
+            int mid = (min + max)/2;
+            int count = 0;
+            for (int i = 0; i <matrix.length ; i++) {
+                int temp = findCount(matrix[i], mid);
+                count += temp;
+            }
+
+            if(count <= medianPosition) min = mid + 1;
+            else max = mid;
+        }
+        return min;
+    }
+
+
+    static int findCount(int[] arr, int mid){
+        int count = 0;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] <= mid) count++;
+            else break;
+        }
+        return  count;
+
     }
 }
