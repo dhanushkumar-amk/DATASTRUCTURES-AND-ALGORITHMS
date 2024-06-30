1816d1529581276c8c7c5afd3df701262780b90e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-30 19:45:52 +0530

    modified

--- src/SEARCHING/BINARYSEARCH/WoodCuttingProblem.java
+++ src/SEARCHING/BINARYSEARCH/WoodCuttingProblem.java
@@ -5,7 +5,35 @@ public class WoodCuttingProblem {
     public static void main(String[] args) {
         int[] treeHeight = {20,15,10,17};
         int target = 7;
-    
+        System.out.println(machineHeight(treeHeight, target));
     }
 
+    static int machineHeight(int[] treeHeight, int target){
+        // find max value
+        int max = 0;
+        for (int i = 0; i < treeHeight.length ; i++) {
+            if(treeHeight[i] > max) max = treeHeight[i];
+        }
+        int low = 0;
+        int high = max;
+        int mid = 0;
+        while(low <= high){
+            mid = (low + high)/2;
+
+           int woodCount = findWoodCount( treeHeight, mid);
+           if(woodCount == target || low == mid) return mid;
+           else if (woodCount < target) low = mid;
+           else high = mid;
+        }
+        return -1;
+    }
+
+
+    static int findWoodCount(int[] treeHeight, int mid){
+       int woodCount = 0;
+        for (int i = 0; i < treeHeight.length; i++) {
+            if(treeHeight[i] > mid) woodCount +=(treeHeight[i] - mid);
+        }
+        return woodCount;
+    }
 }
