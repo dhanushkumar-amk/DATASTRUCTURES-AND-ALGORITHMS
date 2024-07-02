9d63e668a903e869b12b716fb6900a7cd23a83aa
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-03 05:09:20 +0530

    Modified on 03-07-2024

--- src/SORTING/CYCLICSORT/SAMPLE.java
+++ src/SORTING/CYCLICSORT/SAMPLE.java
@@ -1,7 +1,23 @@
 package SORTING.CYCLICSORT;
 
+import java.util.Arrays;
+
 public class SAMPLE {
     public static void main(String[] args) {
-        
+        int[] arr = {3,4,5,2,1};
+    cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static  void cyclicSort(int[] arr){
+        // check - swap - move
+        int i = 0;
+        while(i < arr.length){
+            int correctIndex = arr[i] -1;
+            if(arr[i] != arr[correctIndex]){
+                int temp =arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }
+        }
     }
 }
