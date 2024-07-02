81d239bb627e41e8c77bd1b6e463d6c784472f6e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-03 05:14:53 +0530

    modified

--- src/SORTING/CYCLICSORT/SAMPLE.java
+++ src/SORTING/CYCLICSORT/SAMPLE.java
@@ -1,5 +1,4 @@
 package SORTING.CYCLICSORT;
-
 import java.util.Arrays;
 
 public class SAMPLE {
@@ -8,16 +7,23 @@ public class SAMPLE {
     cyclicSort(arr);
         System.out.println(Arrays.toString(arr));
     }
+
+    // the cyclic sort is usd when the number of array gives 1 to range we se defentely  cyclic sort
     static  void cyclicSort(int[] arr){
-        // check - swap - move
+        // check - swap - move is cyclic method
         int i = 0;
         while(i < arr.length){
             int correctIndex = arr[i] -1;
+            // if the element is not in correct index then swap else i moves forward
+
+            // check
             if(arr[i] != arr[correctIndex]){
+                //swap
                 int temp =arr[i];
                 arr[i] = arr[correctIndex];
                 arr[correctIndex] = temp;
             }
+            // move
             else i++;
         }
     }
