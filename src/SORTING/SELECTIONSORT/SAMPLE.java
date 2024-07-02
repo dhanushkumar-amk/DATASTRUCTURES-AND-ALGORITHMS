3eeaab99aa226617f45898fa4ed23232ffa82c9a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-03 04:39:55 +0530

    modified

--- src/SORTING/SELECTIONSORT/SAMPLE.java
+++ src/SORTING/SELECTIONSORT/SAMPLE.java
@@ -1,7 +1,32 @@
 package SORTING.SELECTIONSORT;
 
+import java.util.Arrays;
+
 public class SAMPLE {
     public static void main(String[] args) {
-        
+        int[] arr = {4,5,3,2,1};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertionSort(int[] arr){
+
+        for (int i = 0; i <arr.length ; i++) {
+        // find the max element in the remaining array and swap it
+        int lastIndex = arr.length - i -1;
+        int max = findMaxElement(arr, 0, lastIndex);
+        //swap
+         int temp = arr[max];
+         arr[max] = arr[lastIndex];
+         arr[lastIndex] = temp;
+        }
+    }
+
+
+    static int findMaxElement(int[] arr, int start, int end){
+        int max = start;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[max] <  arr[i]) max  = arr[i];
+        }
+    return  max;
     }
 }
