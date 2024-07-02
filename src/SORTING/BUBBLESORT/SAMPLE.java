94f09343b8145c94cb47e2480df12f2b47c8e324
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-02 20:59:30 +0530

    Modified on 02-07-2024

--- src/SORTING/BUBBLESORT/SAMPLE.java
+++ src/SORTING/BUBBLESORT/SAMPLE.java
@@ -2,6 +2,24 @@ package SORTING.BUBBLESORT;
 
 public class SAMPLE {
     public static void main(String[] args) {
-        
+        int[] arr = {3,4,5,2,1};
+        int[] ans = bubbleSort(arr);
+        for (int i = 0; i <ans.length ; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
+
+    static void bubbleSort(int[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            // each step  max item will come to the last
+            for (int j = 1; j < arr.length - i ; j++) {
+                //swap if the current element of j is smaller than previous element
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
     }
 }
