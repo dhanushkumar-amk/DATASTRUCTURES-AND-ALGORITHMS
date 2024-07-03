45e207caad3b672e8ca64be6ee3f9a5d5ae9ec9e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-03 18:38:13 +0530

    modified

--- src/SORTING/CYCLICSORT/MissingNumber.java
+++ src/SORTING/CYCLICSORT/MissingNumber.java
@@ -1,8 +1,29 @@
 package SORTING.CYCLICSORT;
 
+import java.util.Arrays;
+
 public class MissingNumber {
     public static void main(String[] args) {
-
+        int[] arr = {0,1,2,4};
+    int ans =  missingNumber(arr);
+        System.out.print(Arrays.toString(ans));
     }
+        static int missingNumber(int[] arr){
         
+        int i = 0;
+        while(i < arr.length){
+            int correctIndex= arr[i];
+
+            if(arr[i] < arr.length && arr[i] != arr[correctIndex]){
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp
+            } else i++;
+        }
+            // search for first missing number
+            for (int index = 0; index <arr.length ; index++) {
+                if(arr[index] != index)return  index;
+            }
+            return arr.length;
+    }
 }
