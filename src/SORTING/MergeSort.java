93de4d89c73ddc272f08d30e18267b705e4924a0
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-04 05:01:05 +0530

    modified

--- src/SORTING/MergeSort.java
+++ src/SORTING/MergeSort.java
@@ -17,7 +17,12 @@ public class MergeSort {
         // to find partition
         static int partition(List<Integer> arr, int low, int high){
             int pivot = arr.get(low);
-            int 
+            int i = low;
+            int j = high;
+
+            while(i < j){
+                
+            }
         }
     }
 }
