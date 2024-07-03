9c2d08f50936aed64bd95a3af4983287fef8294e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-04 05:13:11 +0530

    modified

--- src/SORTING/MergeSort.java
+++ src/SORTING/MergeSort.java
@@ -1,52 +1,77 @@
 package SORTING;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MergeSort {
-    static void quickSort(List<Integer>arr, int low, int high) {
-        while (low < high) {
-            int partitionIndex = partition(arr, low, high);
-            quickSort(arr, low, partitionIndex - 1);
-            quickSort(arr, partitionIndex + 1, high);
+
+    public static void mergeSort(List<Integer> arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
         }
     }
 
-        // to find partition
-        static int partition(List<Integer> arr, int low, int high){
-            int pivot = arr.get(low);
-            int i = low;
-            int j = high;
+    public static void merge(List<Integer> arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
 
-            while(i < j){
-                while(arr.get(i) <= pivot && i <= high-1) i++;
-                while(arr.get(j) > pivot && j >= low) j++;
+        List<Integer> leftArray = new ArrayList<>();
+        List<Integer> rightArray = new ArrayList<>();
 
-            if(i < j){
-                int temp = arr.get(i);
-                arr.set(i, arr.get(j));
-                arr.set(j,temp);
-              }
+        for (int i = 0; i < n1; ++i)
+            leftArray.add(arr.get(left + i));
+        for (int j = 0; j < n2; ++j)
+            rightArray.add(arr.get(mid + 1 + j));
+
+        int i = 0, j = 0;
+
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray.get(i) <= rightArray.get(j)) {
+                arr.set(k, leftArray.get(i));
+                i++;
+            } else {
+                arr.set(k, rightArray.get(j));
+                j++;
             }
-            int temp = arr.get(i);
-            arr.set(i, arr.get(j));
-            arr.set(j, temp);
-            return j;
+            k++;
         }
+
+        while (i < n1) {
+            arr.set(k, leftArray.get(i));
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr.set(k, rightArray.get(j));
+            j++;
+            k++;
+        }
+    }
 
     public static void main(String args[]) {
-        List<Integer> arr = new ArrayList<>();
-        arr = Arrays.asList(new Integer[] {4, 6, 2, 5, 7, 9, 1, 3});
+        List<Integer> arr = new ArrayList<>(Arrays.asList(4, 6, 2, 5, 7, 9, 1, 3));
         int n = arr.size();
-        System.out.println("Before Using insertion Sort: ");
+
+        System.out.println("Before Merge Sort:");
         for (int i = 0; i < n; i++) {
             System.out.print(arr.get(i) + " ");
         }
         System.out.println();
-        arr = .quickSort(arr);
-        System.out.println("After insertion sort: ");
+
+        mergeSort(arr, 0, n - 1);
+
+        System.out.println("After Merge Sort:");
         for (int i = 0; i < n; i++) {
             System.out.print(arr.get(i) + " ");
         }
         System.out.println();
     }
-    }
+}
