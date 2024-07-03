06a5ba2b205120035a3f39e335f0d89229d86395
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-03 19:30:51 +0530

    modified

--- src/SORTING/CYCLICSORT/FindDuplicateNumber.java
+++ src/SORTING/CYCLICSORT/FindDuplicateNumber.java
@@ -1,2 +1,13 @@
-package SORTING.CYCLICSORT;public class FindDuplicateNumber {
+package SORTING.CYCLICSORT;
+
+public class FindDuplicateNumber {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,4};
+
+    }
+
+    public int findDuplicate(int[] nums) {
+
+    }
 }
