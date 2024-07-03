fcf31e127b159a5f80a088c3bb7c54b697ac762e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-03 18:40:58 +0530

    modified

--- src/SORTING/CYCLICSORT/MissingNumber.java
+++ src/SORTING/CYCLICSORT/MissingNumber.java
@@ -5,16 +5,13 @@ import java.util.Arrays;
 public class MissingNumber {
     public static void main(String[] args) {
         int[] arr = {0,1,2,4};
-int ans = missingNumber(arr);
+        int ans = missingNumber(arr);
         System.out.print(ans);
-
     }
         static int missingNumber(int[] arr){
-
         int i = 0;
         while(i < arr.length){
             int correctIndex= arr[i];
-
             if(arr[i] < arr.length && arr[i] != arr[correctIndex]){
                 int temp = arr[i];
                 arr[i] = arr[correctIndex];
