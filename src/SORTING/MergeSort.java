ab355507ae89587b198095504e457ff1dc821415
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-04 04:58:33 +0530

    Modified on 04-07-2024

--- src/SORTING/MergeSort.java
+++ src/SORTING/MergeSort.java
@@ -8,7 +8,13 @@ public class MergeSort {
     }
     static void quickSort(List<Integer>arr, int low, int high){
         while(low < high){
-            
+            int partitionIndex = partion(arr, low, high);
+            quickSort(arr, low , partionIndex -1);
+            quickSort(arr, partionIndex+1, high);
         }
+
+
+        // to find partition
+        static 
     }
 }
