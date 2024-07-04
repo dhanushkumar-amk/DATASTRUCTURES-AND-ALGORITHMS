07265fb9e5d5993bc6bacda4f2786828e52faab0
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-04 19:18:54 +0530

    Modified on 04-07-2024

--- src/SORTING/MergeSort.java
+++ src/SORTING/MergeSort.java
@@ -1,5 +1,22 @@
 package SORTING;
 
+
+
+
 public class MergeSort {
-    
+    public static void main(String[] args) {
+        int n = 7;
+        int arr[] = { 9, 4, 7, 6, 3, 1, 5 };
+        System.out.println("Before sorting array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        Solution.mergeSort(arr, 0, n - 1);
+        System.out.println("After sorting array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
