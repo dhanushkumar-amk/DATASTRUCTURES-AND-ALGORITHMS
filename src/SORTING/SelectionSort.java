7070cbc37869282c5f3f719b80ea61b97cbb7558
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-12 04:52:14 +0530

    Modified on 12-07-2024

--- src/SORTING/SelectionSort.java
+++ src/SORTING/SelectionSort.java
@@ -1,2 +1,11 @@
-package SORTING;public class SelectionSort {
+package SORTING;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,1,5};
+    }
+    static void selection(int[] arr){
+        
+    }
+
 }
