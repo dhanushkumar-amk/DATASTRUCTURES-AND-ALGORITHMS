cffcbc68c85cf3adea59021839de1696cba0ac6d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-12 04:05:20 +0530

    modified

--- src/SORTING/BubbleSort1.java
+++ src/SORTING/BubbleSort1.java
@@ -1,2 +1,12 @@
-package SORTING;public class BubbleSort1 {
+package SORTING;
+
+public class BubbleSort1 {
+    public static void main(String[] args) {
+    int[] arr = {3,6,1,4,2,5};
+
+    }
+    static void BubbleSort(int[] arr){
+        
+    }
+
 }
