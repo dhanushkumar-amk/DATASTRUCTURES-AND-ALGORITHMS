6a406078785ee9cb8f46fc1cb33c0c00290f2e7a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-12 04:13:59 +0530

    Modified on 12-07-2024

--- src/SORTING/BubbleSort1.java
+++ src/SORTING/BubbleSort1.java
@@ -6,7 +6,13 @@ public class BubbleSort1 {
 
     }
     static void BubbleSort(int[] arr){
-        
+        int n = arr.length;
+        for (int i = 0; i < n - 1 ; i++) {
+            // j < n - i - 1  because the each iteration the last element is goes to currect index so every time weleave the one last element and
+            for (int j = 0; j < n - i - 1; j++) {
+
+            }
+        }
     }
 
 }
