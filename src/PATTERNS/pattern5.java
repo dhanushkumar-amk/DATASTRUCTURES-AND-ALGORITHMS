a5e063f5bc9615bcf7bb56f2d0841b2db864d4a5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 17:55:52 +0530

    Modified on 13-07-2024

--- src/PATTERNS/pattern5.java
+++ src/PATTERNS/pattern5.java
@@ -7,10 +7,15 @@ public class pattern5 {
         pattern(n);
     }
     static  void  pattern(int n){
+        
         for (int i = 0; i <=n ; i++) {
-            for (int j = 0; j <=i ; j++) {
-                System.out.print(" + ");
+            for (int k = 1; k <= n-i ; k++) {
+                System.out.print(" ");
             }
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(" *  ");
+            }
+            System.out.println();
         }
     }
 }
