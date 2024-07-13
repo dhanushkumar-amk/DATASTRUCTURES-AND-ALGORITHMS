4c06c33fec978b24b8d6069ff319a81ab1152ff5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 14:05:57 +0530

    Modified on 13-07-2024

--- src/SORTING/CountSort.java
+++ src/SORTING/CountSort.java
@@ -1,2 +1,31 @@
-package SORTING;public class CountSort {
+package SORTING;
+
+import java.util.Arrays;
+
+public class CountSort {
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,3,2,1};
+        countSort(arr);
+        System.out.println("args = " + Arrays.toString(arr));
+    }
+    static void countSort(int[] arr){
+        if(arr == null && arr.length <= 1) return;
+        int max = arr[0];
+
+        for (int  num : arr)
+            if(num > max) max = num;
+
+        int[] count = new int[max + 1];
+
+        for(int num : arr) count[num]++;
+
+        int index = 0;
+        for (int i = 0; i <max ; i++) {
+            while(count[i] > 0) {
+                arr[index] = i;
+                index++;
+                count[i]--;
+            }
+        }
+    }
 }
