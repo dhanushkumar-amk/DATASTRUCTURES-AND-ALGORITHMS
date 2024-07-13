27bc25411467209d137401a414d9d380a34289d7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 17:29:29 +0530

    modified

--- src/PATTERNS/pattern2.java
+++ src/PATTERNS/pattern2.java
@@ -1,2 +1,18 @@
-package PATTERNS;public class pattern2 {
+package PATTERNS;
+
+public class pattern2 {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("The output is : ");
+        pattern(n);
+    }
+    static  void  pattern(int n){
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n ; j++) {
+                if(i * j < 10) System.out.print("0");
+                System.out.print(i * j);
+            }
+            System.out.println();
+        }
+    }
 }
