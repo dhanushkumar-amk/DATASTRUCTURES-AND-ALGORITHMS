e8af5ceea9c6262ca77cd9c00a00293ddfd6957e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 17:36:58 +0530

    Modified on 13-07-2024

--- src/PATTERNS/pattern2.java
+++ src/PATTERNS/pattern2.java
@@ -9,10 +9,21 @@ public class pattern2 {
     static  void  pattern(int n){
         for (int i = 1; i <=n ; i++) {
             for (int j = 1; j <=n ; j++) {
-                if(i * j < 10) System.out.print("0");
+                if(i * j < 10) System.out.print("0"); // for printing zero in th front
                 System.out.print(i * j + " ");
             }
             System.out.println();
         }
     }
 }
+
+// output :
+
+/*
+        01 02 03 04 05
+        02 04 06 08 10
+        03 06 09 12 15
+        04 08 12 16 20
+        05 10 15 20 25
+
+ */
--- src/PATTERNS/pattern3.java
+++ src/PATTERNS/pattern3.java
@@ -1,2 +1,19 @@
-package PATTERNS;public class pattern3 {
+package PATTERNS;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("The output is : ");
+        pattern(n);
+    }
+    static  void  pattern(int n){
+     int count;
+        for (int i = 0; i < n ; i++) {
+            count = i + 1;
+            for (int j = 0; j <n ; j++) {
+                System.out.print( count++ + " ");
+            }
+            System.out.println();
+        }
+    }
 }
