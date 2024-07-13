0d1295accd245959684fb0d086e58b94150c8233
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 17:29:46 +0530

    Modified on 13-07-2024

--- src/PATTERNS/pattern2.java
+++ src/PATTERNS/pattern2.java
@@ -10,7 +10,7 @@ public class pattern2 {
         for (int i = 1; i <=n ; i++) {
             for (int j = 1; j <=n ; j++) {
                 if(i * j < 10) System.out.print("0");
-                System.out.print(i * j);
+                System.out.print(i * j + " ");
             }
             System.out.println();
         }
