eb0c5c59dd2e03b609b5eb01255585aced3f4688
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 17:18:26 +0530

    modified

--- src/PATTERNS/pattern1.java
+++ src/PATTERNS/pattern1.java
@@ -1,2 +1,17 @@
-package PATTERNS;public class pattern1 {
+package PATTERNS;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    }
+    static  void  pattern(int n){
+        int count = 1;
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(count++);
+            }
+            System.out.println();
+        }
+    }
 }
