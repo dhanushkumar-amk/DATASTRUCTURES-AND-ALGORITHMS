ce5ef3c2928f89e9197f441bb7c27a3a8a183be6
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 17:41:37 +0530

    Modified on 13-07-2024

--- src/PATTERNS/pattern4.java
+++ src/PATTERNS/pattern4.java
@@ -1,2 +1,17 @@
-package PATTERNS;public class pattern4 {
+package PATTERNS;
+
+public class pattern4 {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("The output is : ");
+        pattern(n);
+    }
+    static  void  pattern(int n){
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <i ; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
 }
