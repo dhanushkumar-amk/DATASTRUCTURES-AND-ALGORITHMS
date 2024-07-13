a17d5843647389a06919310030d562c05914911d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 17:22:25 +0530

    modified

--- src/PATTERNS/pattern1.java
+++ src/PATTERNS/pattern1.java
@@ -9,6 +9,7 @@ public class pattern1 {
         int count = 1;
         for (int i = 0; i <n ; i++) {
             for (int j = 0; j < n; j++) {
+               if(count < 10) System.out.print("0");
                 System.out.print(count++ + " ");
             }
             System.out.println();
