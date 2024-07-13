3d9bd3ac5f82417b878c5100a486b57cb09dd3e0
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 18:00:08 +0530

    Modified on 13-07-2024

--- src/PATTERNS/pattern6.java
+++ src/PATTERNS/pattern6.java
@@ -1,2 +1,21 @@
-package PATTERNS;public class patern6 {
+package PATTERNS;
+
+public class pattern6 {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("The output is : ");
+        pattern(n);
+    }
+    static  void  pattern(int n){
+
+        for (int i = 0; i <=n ; i++) {
+            for (int k = 1; k <= n-i ; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*");  // same code as pattern 5 just remove the space on the both side of the star
+            }
+            System.out.println();
+        }
+    }
 }
