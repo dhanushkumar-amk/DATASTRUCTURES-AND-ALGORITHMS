eee8fe4799f36b3751dea955022f50a79e394b0e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 18:09:08 +0530

    modified

--- src/PATTERNS/pattern9.java
+++ src/PATTERNS/pattern9.java
@@ -1,2 +1,17 @@
-package PATTERNS;public class pattern9 {
+package PATTERNS;
+
+public class pattern9 {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("The output is : ");
+        pattern(n);
+    }
+    static  void  pattern(int n){
+        for (int i = 1; i <=n ; i++) {
+            for (int j = i; j <=n ; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
 }
