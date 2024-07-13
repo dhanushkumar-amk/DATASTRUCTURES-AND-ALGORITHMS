521b47acbe142deef3bf37dff1c00f41bb1d4d5d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 14:27:28 +0530

    Modified on 13-07-2024

--- src/SORTING/BucketSort.java
+++ src/SORTING/BucketSort.java
@@ -1,9 +1,12 @@
 package SORTING;
 
+import java.util.Arrays;
+
 public class BucketSort {
     public static void main(String[] args) {
     int[] arr = {2,3,5,4,1};
-
+    bucketSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
     static void bucketSort(int[] arr){
         
