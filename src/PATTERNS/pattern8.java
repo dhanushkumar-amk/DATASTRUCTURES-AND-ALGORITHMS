a84075b1fbe7a43c62543ae6ed693d75d9cb43f5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 18:07:29 +0530

    modified

--- src/PATTERNS/pattern8.java
+++ src/PATTERNS/pattern8.java
@@ -10,10 +10,11 @@ public class pattern8 {
         int count = 1;
 
         for (int i = 1; i <=n ; i++) {
-            for (int j = 1; j <=n ; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(count++ + " ");
-                count = 1;
+
             }
+            count = 1;
             System.out.println();
         }
     }
