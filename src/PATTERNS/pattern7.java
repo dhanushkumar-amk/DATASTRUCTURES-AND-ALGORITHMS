4ec34590ef6a2ae1c5fe1440f81823f4058052c4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 18:03:59 +0530

    Modified on 13-07-2024

--- src/PATTERNS/pattern7.java
+++ src/PATTERNS/pattern7.java
@@ -1,2 +1,17 @@
-package PATTERNS;public class pattern7 {
+package PATTERNS;
+
+public class pattern7 {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("The output is : ");
+        pattern(n);
+    }
+    static  void  pattern(int n){
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n ; j++) {
+                System.out.println("* ");
+            }
+            System.out.println();
+        }
+    }
 }
