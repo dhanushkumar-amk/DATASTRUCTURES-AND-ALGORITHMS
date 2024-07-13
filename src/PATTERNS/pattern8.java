e62b063897e16730972369c0cfa6821ba4dce608
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-13 18:06:59 +0530

    Modified on 13-07-2024

--- src/PATTERNS/pattern8.java
+++ src/PATTERNS/pattern8.java
@@ -1,2 +1,20 @@
-package PATTERNS;public class pattern8 {
+package PATTERNS;
+
+public class pattern8 {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("The output is : ");
+        pattern(n);
+    }
+    static  void  pattern(int n){
+        int count = 1;
+
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n ; j++) {
+                System.out.print(count++ + " ");
+                count = 1;
+            }
+            System.out.println();
+        }
+    }
 }
