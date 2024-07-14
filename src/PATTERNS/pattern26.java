6030bb348aba2740986dd38e0c3f55de69160099
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:28:57 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern26.java
+++ src/PATTERNS/pattern26.java
@@ -6,6 +6,23 @@ public class pattern26 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = rows; i >= 1; i--)
+        {
+            //Printing rows-i spaces at the beginning of each row
 
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i value i times at the end of each row
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(i+" ");
+            }
+
+            System.out.println();
+        }
     }
 }
