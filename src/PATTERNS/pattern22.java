8e90a1a701ce6649be5b339db7a5ed5fda5dbb18
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:20:54 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern22.java
+++ src/PATTERNS/pattern22.java
@@ -6,6 +6,44 @@ public class pattern22 {
         pattern(n);
     }
     static void pattern(int n){
+        for (int i = 1; i <= n; i++)
+        {
+            //Printing i spaces at the beginning of each row
 
+            for (int j = 1; j < i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i to rows value at the end of each row
+
+            for (int j = i; j <=n; j++)
+            {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = n-1; i >= 1; i--)
+        {
+            //Printing i spaces at the beginning of each row
+
+            for (int j = 1; j < i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i to rows value at the end of each row
+
+            for (int j = i; j <= n; j++)
+            {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
     }
 }
