8a60986260de085c8f824e88ca689d1ef7fde520
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 12:05:18 +0530

    modified

--- src/PATTERNS/pattern55.java
+++ src/PATTERNS/pattern55.java
@@ -6,6 +6,21 @@ public class pattern55 {
         pattern(rows);
     }
     static void pattern(int rows){
+        char ch;
+
+        for (int i = rows; i >= 1; i--)
+        {
+            ch = 'A';
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(ch+" ");
+
+                ch++;
+            }
+
+            System.out.println();
+        }
 
     }
 }
