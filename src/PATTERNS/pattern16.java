92594b7ff0156915e63ee2bc4f4efe2d3dee9f93
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:08:27 +0530

    modified

--- src/PATTERNS/pattern16.java
+++ src/PATTERNS/pattern16.java
@@ -6,9 +6,21 @@ public class pattern16 {
         pattern(n);
     }
     static void pattern(int n){
-        for (int i = n; i >= 1; i--)
+        for (int i = 1; i <= n; i++)
         {
-            for (int j = i; j >= 1; j--)
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = n-1; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
             {
                 System.out.print(j+" ");
             }
