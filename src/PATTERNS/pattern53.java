3a7f6f9233dc2ea07797cece19a1d83f3b16bf85
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 12:03:58 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern53.java
+++ src/PATTERNS/pattern53.java
@@ -6,6 +6,20 @@ public class pattern53 {
         pattern(rows);
     }
     static void pattern(int rows){
+        char ch;
 
+        for (int i = 1; i <= rows; i++)
+        {
+            ch = 'Z';
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(ch+" ");
+
+                ch--;
+            }
+
+            System.out.println();
+        }
     }
 }
