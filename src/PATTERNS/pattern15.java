ceb52d35b406569c931436763e549cdee758ab4e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:06:55 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern15.java
+++ src/PATTERNS/pattern15.java
@@ -6,6 +6,14 @@ public class pattern15 {
         pattern(n);
     }
     static void pattern(int n){
-       
+        for (int i = n; i >= 1; i--)
+        {
+            for (int j = i; j >= 1; j--)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
     }
 }
