ef89d940a32a509d590bf6af1a76edbb71b148f9
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:10:15 +0530

    modified

--- src/PATTERNS/pattern17.java
+++ src/PATTERNS/pattern17.java
@@ -6,6 +6,26 @@ public class pattern17 {
         pattern(n);
     }
     static void pattern(int n){
+        for (int i =n; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j+" ");
+            }
 
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
     }
 }
