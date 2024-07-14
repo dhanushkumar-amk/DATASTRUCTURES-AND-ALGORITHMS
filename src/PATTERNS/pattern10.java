1044e2307353f5167aaee40847ae021e474fb1d9
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:00:33 +0530

    modified

--- src/PATTERNS/pattern10.java
+++ src/PATTERNS/pattern10.java
@@ -2,7 +2,16 @@ package PATTERNS;
 
 public class pattern10 {
     public static void main(String[] args) {
-
+        int n = 5;
+        pattern(n);
     }
+    static  void pattern(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
 
+            System.out.println();
+        }
+    }
 }
