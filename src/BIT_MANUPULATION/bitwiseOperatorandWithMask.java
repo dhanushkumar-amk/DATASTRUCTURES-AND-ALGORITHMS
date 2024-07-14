3c85b6a60b19a4a853f32c5ee1df508d31dd9db9
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 18:29:21 +0530

    modified

--- src/BIT_MANUPULATION/bitwiseOperatorandWithMask.java
+++ src/BIT_MANUPULATION/bitwiseOperatorandWithMask.java
@@ -1,2 +1,10 @@
-package BIT_MANUPULATION;public class bitwiseOperatorandWithMask {
+package BIT_MANUPULATION;
+
+public class bitwiseOperatorandWithMask {
+    public static void main(String[] args) {
+        int n = 36;
+        int i = 3;
+        int onMask = ~(1 << i);
+        System.out.println(n & onMask);
+    }
 }
