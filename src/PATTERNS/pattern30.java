7576261ffea0819889e8ab41c26c18d92e98a740
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:32:09 +0530

    modified

--- src/PATTERNS/pattern30.java
+++ src/PATTERNS/pattern30.java
@@ -6,6 +6,45 @@ public class pattern30 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            //Printing i spaces at the beginning of each row
+
+            for (int j = 1; j < i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i to rows value at the end of each row
+
+            for (int j = i; j <= rows; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = rows-1; i >= 1; i--)
+        {
+            //Printing i spaces at the beginning of each row
+
+            for (int j = 1; j < i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i to rows value at the end of each row
+
+            for (int j = i; j <= rows; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
 
     }
 }
