fdf2e3a4a122a24edf28a42758b7e8d8f3403a67
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:07:42 +0530

    modified

--- src/PATTERNS/pattern16.java
+++ src/PATTERNS/pattern16.java
@@ -1,2 +1,19 @@
-package PATTERNS;public class pattern16 {
+package PATTERNS;
+
+public class pattern16 {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    }
+    static void pattern(int n){
+        for (int i = n; i >= 1; i--)
+        {
+            for (int j = i; j >= 1; j--)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
+    }
 }
