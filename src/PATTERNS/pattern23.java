2cd9a3ad0ddfe1873329e8daee8405c29d5fd477
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:24:36 +0530

    modified

--- src/PATTERNS/pattern23.java
+++ src/PATTERNS/pattern23.java
@@ -2,10 +2,27 @@ package PATTERNS;
 
 public class pattern23 {
     public static void main(String[] args) {
-        int n = 5;
-        pattern(n);
+        int rows = 5;
+        pattern(rows);
     }
-    static void pattern(int n){
+    static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            //Printing rows-i spaces at the beginning of each row
 
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing 1 to i value at the end of each row
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
     }
 }
