f9a3adbda90868f6882090e9d18fca7a7085d0d8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:35:41 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern31.java
+++ src/PATTERNS/pattern31.java
@@ -5,7 +5,18 @@ public class pattern31 {
         int rows = 5;
         pattern(rows);
     }
-    static void pattern(int rows){
 
+    static void pattern(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j % 2 == 0) { // the number is even then puut 0
+                    System.out.print(0);
+                } else {
+                    System.out.print(1);
+                }
+            }
+
+            System.out.println();
+        }
     }
 }
