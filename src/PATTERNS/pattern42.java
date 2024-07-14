d929c99e07f99e44fb9173e2f841fa1e063186a4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:50:02 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern42.java
+++ src/PATTERNS/pattern42.java
@@ -1,2 +1,11 @@
-package PATTERNS;public class pattern42 {
+package PATTERNS;
+
+public class pattern42 {
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+    static void pattern(int rows){
+
+    }
 }
