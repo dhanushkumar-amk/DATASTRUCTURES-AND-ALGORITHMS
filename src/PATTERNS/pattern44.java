db32dc345d70a00d82d0123a8875cd51a1016b23
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:52:53 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern44.java
+++ src/PATTERNS/pattern44.java
@@ -1,2 +1,11 @@
-package PATTERNS;public class pattern44 {
+package PATTERNS;
+
+public class pattern44 {
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+    static void pattern(int rows){
+
+    }
 }
