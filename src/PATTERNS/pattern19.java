a962f1848b16c8ce700615f24cb67f1fc9ed2f48
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:14:51 +0530

    modified

--- src/PATTERNS/pattern19.java
+++ src/PATTERNS/pattern19.java
@@ -6,7 +6,24 @@ public class pattern19 {
         pattern(n);
     }
     static void pattern(int n){
+        for (int i = 1; i <= n; i++)
+        {
+            //Printing spaces at the beginning of each row
 
+            for (int j = 2*n-i; j > i; j--)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing 1 to i value at the end of each row
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
     }
 }
 
