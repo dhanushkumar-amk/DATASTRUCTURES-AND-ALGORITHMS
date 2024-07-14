4872007c20a9f220d8b63e6aa34ea701ba231183
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:48:56 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern41.java
+++ src/PATTERNS/pattern41.java
@@ -6,6 +6,44 @@ public class pattern41 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            //Printing rows-i spaces at the beginning of each row
 
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i stars at the end of each row
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = rows-1; i >= 1; i--)
+        {
+            //Printing rows-i spaces at the beginning of each row
+
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i stars at the end of each row
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
     }
 }
