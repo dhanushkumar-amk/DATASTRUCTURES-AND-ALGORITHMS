b8218ce553c9f96ec1db5c4cd911f9fbe3723d04
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:43:39 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern37.java
+++ src/PATTERNS/pattern37.java
@@ -6,6 +6,19 @@ public class pattern37 {
         pattern(rows);
     }
     static void pattern(int rows){
+        int num = 1;
+
+        for (int i = rows; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(num+" ");
+
+                num++;
+            }
+
+            System.out.println();
+        }
 
     }
 }
