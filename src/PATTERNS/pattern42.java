ee65d56f0e59e2dcb4b5107a2935827eac0208b8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:50:30 +0530

    modified

--- src/PATTERNS/pattern42.java
+++ src/PATTERNS/pattern42.java
@@ -5,7 +5,39 @@ public class pattern42 {
         int rows = 5;
         pattern(rows);
     }
-    static void pattern(int rows){
+    static void pattern(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            //Printing spaces at the beginning of each row
 
+            for (int j = 2 * rows - i; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            //Printing i stars at the end of each row
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = rows - 1; i >= 1; i--) {
+            //Printing spaces at the beginning of each row
+
+            for (int j = 2 * rows - i; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            //Printing i stars at the end of each row
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
     }
 }
