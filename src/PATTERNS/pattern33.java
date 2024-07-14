5f303a8db3c13514423431e91c4ea63443963007
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:38:52 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern33.java
+++ src/PATTERNS/pattern33.java
@@ -7,5 +7,19 @@ public class pattern33 {
     }
     static void pattern(int rows){
 
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(1);
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
     }
 }
