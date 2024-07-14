3f5766849eaf3fa2416b6126b1ed635dbe30d7c9
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:37:12 +0530

    modified

--- src/PATTERNS/pattern32.java
+++ src/PATTERNS/pattern32.java
@@ -6,6 +6,36 @@ public class pattern32 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            int num;
 
+            if(i%2 == 0)
+            {
+                num = 0;
+
+                for (int j = 1; j <= rows; j++)
+                {
+                    System.out.print(num);
+
+                    num = (num == 0)? 1 : 0;
+                }
+            }
+            else
+            {
+                num = 1;
+
+                for (int j = 1; j <= rows; j++)
+                {
+                    System.out.print(num);
+
+                    num = (num == 0)? 1 : 0;
+                }
+            }
+
+            System.out.println();
+        }
     }
 }
+
+// complex type pattern
