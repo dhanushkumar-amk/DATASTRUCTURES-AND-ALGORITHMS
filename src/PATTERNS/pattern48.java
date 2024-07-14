ed22992ffedb1c37e6ec77a287f6d4f3beb537b5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:57:27 +0530

    modified

--- src/PATTERNS/pattern48.java
+++ src/PATTERNS/pattern48.java
@@ -5,7 +5,20 @@ public class pattern48 {
         int rows = 5;
         pattern(rows);
     }
-    static void pattern(int rows){
 
+    static void pattern(int rows) {
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == rows) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+        }
     }
 }
