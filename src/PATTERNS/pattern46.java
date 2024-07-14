e7ce6fd212460e5dfe2f881e12d1c84316d1896c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:54:41 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern46.java
+++ src/PATTERNS/pattern46.java
@@ -1,2 +1,11 @@
-package PATTERNS;public class pattern46 {
+package PATTERNS;
+
+public class pattern46 {
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+    static void pattern(int rows){
+
+    }
 }
