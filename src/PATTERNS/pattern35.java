b3a91b2c58e13cd1e58db7f02384a0ee8b7299de
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:41:41 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern35.java
+++ src/PATTERNS/pattern35.java
@@ -6,6 +6,18 @@ public class pattern35 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            int num = i;
 
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(num+" ");
+
+                num = num+rows-j;
+            }
+
+            System.out.println();
+        }
     }
 }
