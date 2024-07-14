58870bf9ccfed61b4f2652497f0bdc0096425d56
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 12:04:39 +0530

    modified

--- src/PATTERNS/pattern54.java
+++ src/PATTERNS/pattern54.java
@@ -6,6 +6,18 @@ public class pattern54 {
         pattern(rows);
     }
     static void pattern(int rows){
+        char ch = 'A';
 
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(ch+" ");
+            }
+
+            ch++;
+
+            System.out.println();
+        }
     }
 }
