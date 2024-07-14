73448e972252d9c193281951ff43e271fa356d5d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:53:27 +0530

    modified

--- src/PATTERNS/pattern44.java
+++ src/PATTERNS/pattern44.java
@@ -6,6 +6,21 @@ public class pattern44 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if(j == 1 || j == i || i == rows)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
 
+            System.out.println();
+        }
     }
 }
