4f1fa3205009ccb84d8b97e96be4e961d790b5ca
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:01:15 +0530

    modified

--- src/PATTERNS/pattern10.java
+++ src/PATTERNS/pattern10.java
@@ -10,8 +10,15 @@ public class pattern10 {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
-
             System.out.println();
         }
     }
 }
+
+/*
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+ */
