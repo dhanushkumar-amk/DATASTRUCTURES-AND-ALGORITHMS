1dec814a873dc65e6393fd517713e92881689b42
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:48:06 +0530

    modified

--- src/PATTERNS/pattern40.java
+++ src/PATTERNS/pattern40.java
@@ -6,6 +6,26 @@ public class pattern40 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
 
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = rows-1; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
     }
 }
