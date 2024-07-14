ac76dd930a8d84e91da4dbb1dd5469ab710b0bad
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 12:06:24 +0530

    modified

--- src/PATTERNS/pattern56.java
+++ src/PATTERNS/pattern56.java
@@ -6,6 +6,48 @@ public class pattern56 {
         pattern(rows);
     }
     static void pattern(int rows){
+        char ch;
 
+        //Printing upper half of the pattern
+
+        for (int i = 1; i <= rows; i++)
+        {
+            ch='A';
+
+            for (int j = 2*rows-i; j > i; j--)
+            {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j >= 1; j--)
+            {
+                System.out.print(ch+" ");
+
+                ch++;
+            }
+
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = rows-1; i >= 1; i--)
+        {
+            ch='A';
+
+            for (int j = 2*rows-i; j > i; j--)
+            {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j >= 1; j--)
+            {
+                System.out.print(ch+" ");
+
+                ch++;
+            }
+
+            System.out.println();
+        }
     }
 }
