917b1425c2cddb2e8f9de0486dd2c4ab54733eb2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:43:12 +0530

    modified

--- src/PATTERNS/pattern37.java
+++ src/PATTERNS/pattern37.java
@@ -1,2 +1,11 @@
-package PATTERNS;public class pattern37 {
+package PATTERNS;
+
+public class pattern37 {
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+    static void pattern(int rows){
+
+    }
 }
