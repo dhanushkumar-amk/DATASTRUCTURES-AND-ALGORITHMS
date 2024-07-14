6d7df055b3112828b3faf4164d169d8418b394d3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:54:53 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern46.java
+++ src/PATTERNS/pattern46.java
@@ -6,6 +6,21 @@ public class pattern46 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= rows; j++)
+            {
+                if (i == 1 || j == 1 || j == rows || i == rows)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
 
+            System.out.println();
+        }
     }
 }
