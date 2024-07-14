fa4389a0a7ee9b1fa453d1bcfac9e9d4ba36d690
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 18:36:49 +0530

    modified

--- src/BIT_MANUPULATION/ComputeXorOperator.java
+++ src/BIT_MANUPULATION/ComputeXorOperator.java
@@ -2,7 +2,18 @@ package BIT_MANUPULATION;
 
 public class ComputeXorOperator {
     public static void main(String[] args) {
-        
+        int n = 45;
+        System.out.println(computeXOR(n));
     }
-
+    public static int computeXOR(int n)
+    {
+        if (n % 4 == 0)
+            return n;
+        if (n % 4 == 1)
+            return 1;
+        if (n % 4 == 2)
+            return n + 1;
+        else
+            return 0;
+    }
 }
