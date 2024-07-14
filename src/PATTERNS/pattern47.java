a3ddbf5d3a25e96b5053d79c50130c07a41fdfbd
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:55:48 +0530

    modified

--- src/PATTERNS/pattern47.java
+++ src/PATTERNS/pattern47.java
@@ -6,6 +6,26 @@ public class pattern47 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
 
+            for (int j = 1; j <= i; j++)
+            {
+                if (j == 1 || j == i || i == rows)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
     }
 }
