03ea49cd8ffd477cda8f264f223838c4d1b951b6
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 17:42:09 +0530

    modified

--- src/BIT_MANUPULATION/BitwiseOperator.java
+++ src/BIT_MANUPULATION/BitwiseOperator.java
@@ -3,5 +3,7 @@ package BIT_MANUPULATION;
 public class BitwiseOperator {
     public static void main(String[] args) {
         System.out.println( 5 | 9);
+        System.out.println(5 & 9);
+        System.out.println(5 ^ 9);
     }
 }
