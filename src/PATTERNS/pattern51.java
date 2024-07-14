c31630c1a24785e7770afb40852361c798fbbfc9
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 12:01:30 +0530

    modified

--- src/PATTERNS/pattern51.java
+++ src/PATTERNS/pattern51.java
@@ -6,6 +6,47 @@ public class pattern51 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+
+            for (int j = 1; j <= 2*(rows-i); j++)
+            {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = rows-1; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+
+            for (int j = 1; j <= 2*(rows-i); j++)
+            {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
 
     }
 }
