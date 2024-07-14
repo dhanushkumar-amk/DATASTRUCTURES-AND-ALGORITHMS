ae6fef7490e7e3fbbe47964de5d2b2b186bd474f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:11:00 +0530

    modified

--- src/PATTERNS/pattern18.java
+++ src/PATTERNS/pattern18.java
@@ -1,2 +1,12 @@
-package PATTERNS;public class pattern18 {
+package PATTERNS;
+
+public class pattern18 {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    }
+    static void pattern(int n){
+
+        
+    }
 }
