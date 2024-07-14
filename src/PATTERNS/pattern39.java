df2a68888d46d6e72cfbe4cbe5bb154092247145
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:46:03 +0530

    modified

--- src/PATTERNS/pattern39.java
+++ src/PATTERNS/pattern39.java
@@ -6,6 +6,48 @@ public class pattern39 {
         pattern(rows);
     }
     static void pattern(int rows){
+//Printing upper half of the pattern
 
+        for (int i = rows; i >= 1; i--)
+        {
+            for (int j = rows; j >= i; j--)
+            {
+                System.out.print(j+" ");
+            }
+
+            for (int j = 1; j < (2*i)-1; j++)
+            {
+                System.out.print(i+" ");
+            }
+
+            for (int j = i+1; j <= rows; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = 2; i <= rows; i++)
+        {
+            for (int j = rows; j >= i; j--)
+            {
+                System.out.print(j+" ");
+            }
+
+            for (int j = 1; j < (2*i)-1; j++)
+            {
+                System.out.print(i+" ");
+            }
+
+            for (int j = i+1; j <= rows; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
     }
 }
