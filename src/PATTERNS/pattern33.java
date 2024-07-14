e5f2bb0eb120c19b9e2cc0483178207959ad1f14
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:38:03 +0530

    modified

--- src/PATTERNS/pattern33.java
+++ src/PATTERNS/pattern33.java
@@ -1,5 +1,11 @@
 package PATTERNS;
 
 public class pattern33 {
-    
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+    static void pattern(int rows){
+
+    }
 }
