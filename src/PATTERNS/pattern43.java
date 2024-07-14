44ae188aca16dd0cff4c517f191616c1f5dac0ca
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:52:03 +0530

    modified

--- src/PATTERNS/pattern43.java
+++ src/PATTERNS/pattern43.java
@@ -6,6 +6,44 @@ public class pattern43 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = rows; i >= 1; i--)
+        {
+            //Printing rows-i spaces at the beginning of each row
 
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i stars at the end of each row
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = 2; i <= rows; i++)
+        {
+            //Printing rows-i spaces at the beginning of each row
+
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i stars at the end of each row
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
     }
 }
