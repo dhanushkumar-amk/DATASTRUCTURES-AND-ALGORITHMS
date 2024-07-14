98e35443e2f1e2f5d55454849604d218b5e1cf40
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:15:48 +0530

    modified

--- src/PATTERNS/pattern20.java
+++ src/PATTERNS/pattern20.java
@@ -6,6 +6,23 @@ public class pattern20 {
         pattern(n);
     }
     static void pattern(int n){
+        for (int i =n; i >= 1; i--)
+        {
+            //Printing spaces at the beginning of each row
 
+            for (int j = 2*n-i; j > i; j--)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing 1 to i value at the end of each row
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
     }
 }
