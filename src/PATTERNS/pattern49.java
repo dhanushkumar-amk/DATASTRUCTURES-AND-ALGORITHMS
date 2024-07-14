485c7d8a1ef9a80aa67caf6357dc08c0d732760f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:59:03 +0530

    modified

--- src/PATTERNS/pattern49.java
+++ src/PATTERNS/pattern49.java
@@ -7,5 +7,50 @@ public class pattern49 {
     }
     static void pattern(int rows){
 
+        for (int i = rows; i >= 1; i--)
+        {
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                if (j == 1 || j == i || i == rows)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        //Printing lower half of the pattern
+
+        for (int i = 2; i <= rows; i++)
+        {
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                if (j == 1 || j == i || i == rows)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
     }
 }
