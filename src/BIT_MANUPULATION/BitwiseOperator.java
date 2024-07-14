f99cac45b74911d1924d17e5fd7ff5aacca46e84
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 17:59:30 +0530

    Modified on 14-07-2024

--- src/BIT_MANUPULATION/BitwiseOperator.java
+++ src/BIT_MANUPULATION/BitwiseOperator.java
@@ -6,5 +6,11 @@ public class BitwiseOperator {
         System.out.println( 5 | 9); // 13
         System.out.println(5 & 9); // 1
         System.out.println(5 ^ 9); // 12
+
+    // advance operator
+        System.out.println( 5 << 1);
+        System.out.println(5 << 2);
+        System.out.println(5 >> 1);
+        System.out.println(5 >> 2);
     }
 }
