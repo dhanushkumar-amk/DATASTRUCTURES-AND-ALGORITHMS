67c44fa359f79cbada4c541d31f8fe8a0a5083ec
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:02:42 +0530

    modified

--- src/PATTERNS/pattern12.java
+++ src/PATTERNS/pattern12.java
@@ -1,2 +1,16 @@
-package PATTERNS;public class pattern12 {
+package PATTERNS;
+
+public class pattern12 {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    }
+    static  void pattern(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
