1e2ec016757b330f77222dcd4669446b632e7b66
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:36:09 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern32.java
+++ src/PATTERNS/pattern32.java
@@ -1,2 +1,11 @@
-package PATTERNS;public class pattern32 {
+package PATTERNS;
+
+public class pattern32 {
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+    static void pattern(int rows){
+
+    }
 }
