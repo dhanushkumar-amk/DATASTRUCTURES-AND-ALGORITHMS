23a03dc2cb8c48a1ee8ce536fb01a965360e7408
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:27:45 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern25.java
+++ src/PATTERNS/pattern25.java
@@ -1,5 +1,14 @@
 package PATTERNS;
 
 public class pattern25 {
-    
+
+
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+
+    static void pattern(int rows) {
+
+    }
 }
