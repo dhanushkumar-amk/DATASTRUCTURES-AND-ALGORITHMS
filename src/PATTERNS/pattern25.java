389ebfd8bb99d626152fde656ec61b407393c858
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:28:01 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern25.java
+++ src/PATTERNS/pattern25.java
@@ -9,6 +9,23 @@ public class pattern25 {
     }
 
     static void pattern(int rows) {
+        for (int i = 1; i <= rows; i++)
+        {
+            //Printing rows-i spaces at the beginning of each row
 
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i value i times at the end of each row
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(i+" ");
+            }
+
+            System.out.println();
+        }
     }
 }
