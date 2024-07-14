40931720fe9e8c876c5b436581d8a5c998556241
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 18:27:57 +0530

    modified

--- src/BIT_MANUPULATION/sitBitOrWithMask.java
+++ src/BIT_MANUPULATION/sitBitOrWithMask.java
@@ -1,2 +1,10 @@
-package BIT_MANUPULATION;public class sitBitOrWithMask {
+package BIT_MANUPULATION;
+
+public class sitBitOrWithMask {
+    public static void main(String[] args) {
+        int n = 36;
+        int i = 3;
+        int onMask = 1 << i;
+        System.out.println(n | onMask);
+    }
 }
