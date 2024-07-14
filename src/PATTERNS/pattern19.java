5fde2245f93acb67040ee9fdaaa06050124a47b1
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:13:09 +0530

    modified

--- src/PATTERNS/pattern19.java
+++ src/PATTERNS/pattern19.java
@@ -1,2 +1,12 @@
-package PATTERNS;public class pattern19 {
+package PATTERNS;
+
+public class pattern19 {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    }
+    static void pattern(int n){
+
+    }
 }
+
