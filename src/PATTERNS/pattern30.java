abc283450518c2fa11f247838adacfb27d60704f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:31:53 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern30.java
+++ src/PATTERNS/pattern30.java
@@ -1,2 +1,11 @@
-package PATTERNS;public class pattern30 {
+package PATTERNS;
+
+public class pattern30 {
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+    static void pattern(int rows){
+
+    }
 }
