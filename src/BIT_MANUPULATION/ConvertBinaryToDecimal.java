711c56da9daa74d6ad7d70927142bbcd8e425a55
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 16:25:57 +0530

    modified

--- src/BIT_MANUPULATION/ConvertBinaryToDecimal.java
+++ src/BIT_MANUPULATION/ConvertBinaryToDecimal.java
@@ -1,2 +1,20 @@
-package BIT_MANUPULATION;public class ConvertBinaryToDecimal {
+package BIT_MANUPULATION;
+
+public class ConvertBinaryToDecimal {
+    public static void main(String[] args) {
+        String b = "101101";
+        System.out.println(binaryToDecimal(b));
+    }
+    static int binaryToDecimal(String b){
+        int result = 0;
+        int powerof2 = 1;
+
+        for (int i = b.length()-1; i >= 0 ; i--) {
+            if(b.charAt(i) == '1'){
+                result += powerof2;
+            }
+            powerof2  *= 2;
+        }
+        return result;
+    }
 }
