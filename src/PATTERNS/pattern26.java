66018089745479fd17c1953fe7fa4e91f7d7f62d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:28:39 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern26.java
+++ src/PATTERNS/pattern26.java
@@ -1,5 +1,11 @@
 package PATTERNS;
 
 public class pattern26 {
-    
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+    static void pattern(int rows){
+
+    }
 }
