9ce9bc36f365f86e2e25f3dfe9b693fb1b424fbb
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:26:51 +0530

    modified

--- src/PATTERNS/pattern24.java
+++ src/PATTERNS/pattern24.java
@@ -6,6 +6,23 @@ public class pattern24 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            //Printing rows-i spaces at the beginning of each row
 
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i to 2*i value at the end of each row
+
+            for (int j = i; j < 2*i; j++)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
     }
 }
