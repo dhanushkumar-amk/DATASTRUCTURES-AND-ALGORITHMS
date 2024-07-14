00d7267e666c6c42518ae168a0f8bab025162a78
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 12:02:12 +0530

    modified

--- src/PATTERNS/pattern52.java
+++ src/PATTERNS/pattern52.java
@@ -1,2 +1,11 @@
-package PATTERNS;public class pattern52 {
+package PATTERNS;
+
+public class pattern52 {
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+    static void pattern(int rows){
+
+    }
 }
