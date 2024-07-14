a705cca39431875b05d39a12acf8de11fe2ab39c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:39:19 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern34.java
+++ src/PATTERNS/pattern34.java
@@ -1,2 +1,11 @@
-package PATTERNS;public class pattern34 {
+package PATTERNS;
+
+public class pattern34 {
+    public static void main(String[] args) {
+        int rows = 5;
+        pattern(rows);
+    }
+    static void pattern(int rows){
+
+    }
 }
