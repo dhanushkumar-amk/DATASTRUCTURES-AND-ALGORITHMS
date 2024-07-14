492afe097a3abcf47ae1c2fadcb59b03a86f9cae
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:31:26 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern29.java
+++ src/PATTERNS/pattern29.java
@@ -6,6 +6,24 @@ public class pattern29 {
         pattern(rows);
     }
     static void pattern(int rows){
+        for (int i = 1; i <= rows; i++)
+        {
+            //Printing i spaces at the beginning of each row
+
+            for (int j = 1; j < i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing rows to i value at the end of each row
+
+            for (int j = rows; j >= i; j--)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
 
     }
 }
