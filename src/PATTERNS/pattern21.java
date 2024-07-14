d78b23461a600794169b88c18602d4b77046a999
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-14 11:19:29 +0530

    Modified on 14-07-2024

--- src/PATTERNS/pattern21.java
+++ src/PATTERNS/pattern21.java
@@ -6,6 +6,23 @@ public class pattern21 {
         pattern(n);
     }
     static void pattern(int n){
+        for (int i = n; i >= 1; i--)
+        {
+            //Printing spaces at the beginning of each row
 
+            for (int j = 2*n-i; j > i; j--)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing i to 1 value at the end of each row
+
+            for (int j = i; j >= 1; j--)
+            {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
     }
 }
