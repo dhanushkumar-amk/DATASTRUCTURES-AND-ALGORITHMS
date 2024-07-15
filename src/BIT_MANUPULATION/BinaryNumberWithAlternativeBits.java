81eb0b8755b5cec50d702f2fc2ad74281cd7cb01
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-16 04:53:26 +0530

    modified

--- src/BIT_MANUPULATION/BinaryNumberWithAlternativeBits.java
+++ src/BIT_MANUPULATION/BinaryNumberWithAlternativeBits.java
@@ -2,7 +2,13 @@ package BIT_MANUPULATION;
 
 public class BinaryNumberWithAlternativeBits {
     public static void main(String[] args) {
-
+int n = 10;
+        System.out.println(alternativeBits(n));
     }
-    
+    static boolean alternativeBits(int n){
+        int xor = n ^ (n >> 1);
+
+        if(((xor + 1) & xor) == 0) return true;
+        else return  false;
+    }
 }
