e6ef455c5efec243e52290cc0b93deb56d301192
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-16 05:06:28 +0530

    Modified on 16-07-2024

--- src/BIT_MANUPULATION/SwapTwoNumbers.java
+++ src/BIT_MANUPULATION/SwapTwoNumbers.java
@@ -2,11 +2,14 @@ package BIT_MANUPULATION;
 
 public class SwapTwoNumbers {
     public static void main(String[] args) {
-int a = 5;
-int b = 10;
+        int a = 5;
+        int b = 10;
 
-a = a ^ b;
-b = a ^ b;
+        a = a ^ b;
+        b = a ^ b;
+
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
     }
 
 
