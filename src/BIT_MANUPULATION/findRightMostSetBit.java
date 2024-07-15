fec5b998720a6923ecd4f9c92c31f0e2ebf1c3ee
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-16 04:30:34 +0530

    Modified on 16-07-2024

--- src/BIT_MANUPULATION/findRightMostSetBit.java
+++ src/BIT_MANUPULATION/findRightMostSetBit.java
@@ -2,12 +2,13 @@ package BIT_MANUPULATION;
 
 public class findRightMostSetBit {
     public static void main(String[] args) {
-        int n = 0;
+        int n = 10;
         System.out.println(positionOfRightMostSetBit(n));
     }
     static int positionOfRightMostSetBit(int n){
         int mask = 1;
         int position = 0;
+        // base case
          if(n == 0) return -1;
         while((n & mask) == 0 ){
             mask = mask << 1;
