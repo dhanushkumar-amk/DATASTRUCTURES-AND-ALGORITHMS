226278bbe95cea769af116f4e4e0c4b2b276c42f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-17 18:02:14 +0530

    Modified on 17-07-2024

--- src/BIT_MANUPULATION/ReversingTheBitsOfAnInteger.java
+++ src/BIT_MANUPULATION/ReversingTheBitsOfAnInteger.java
@@ -1,5 +1,24 @@
 package BIT_MANUPULATION;
 
 public class ReversingTheBitsOfAnInteger {
-
+    public static void main(String[] args) {
+        int n = 101;
+        System.out.println(reverseBits(n));
+    }
+    static  int reverseBits(int n){
+      int f = 31;
+      int l = 0;
+      int reverse = 0;
+      while (f > l){
+         if((n & (1<<f)) != 0){
+             reverse = reverse | ( 1 << l);
+         }
+         if((n & (1<<l)) != 0){
+             reverse = reverse | (1 << f);
+         }
+         f--;
+         l++;
+      }
+      return  reverse;
+    }
 }
