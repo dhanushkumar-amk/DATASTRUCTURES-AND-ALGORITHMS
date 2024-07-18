3eacb7a7c4e704990dd7bb24736fd9d02acd3294
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-19 04:40:33 +0530

    Modified on 19-07-2024

--- src/LINKED_LIST/LL1.java
+++ src/LINKED_LIST/LL1.java
@@ -1,5 +1,22 @@
 package LINKED_LIST;
 
+
+
+
+
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
 public class LL1 {
-    
+    public static void main(String[] args) {
+        Node p1 = new Node(10);
+        Node p1 = new Node(10);Node p1 = new Node(10);Node p1 = new Node(10);
+    }
 }
