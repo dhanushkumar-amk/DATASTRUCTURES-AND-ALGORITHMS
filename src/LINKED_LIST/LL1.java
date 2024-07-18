0e2b25f4b262cefc5eadeeb974c309c83b685372
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-19 04:43:16 +0530

    Modified on 19-07-2024

--- src/LINKED_LIST/LL1.java
+++ src/LINKED_LIST/LL1.java
@@ -4,7 +4,7 @@ package LINKED_LIST;
 
 
 
-
+// blue print of the node
 class Node{
     int data;
     Node next;
@@ -16,7 +16,13 @@ class Node{
 }
 public class LL1 {
     public static void main(String[] args) {
+        // creating the node
         Node p1 = new Node(10);
-        Node p1 = new Node(10);Node p1 = new Node(10);Node p1 = new Node(10);
+        Node p2 = new Node(20);
+        Node p3 = new Node(30);
+        Node p4 = new Node(40);
+
+        // connect each other or link each other
+        System.out.println(p1.next);
     }
 }
