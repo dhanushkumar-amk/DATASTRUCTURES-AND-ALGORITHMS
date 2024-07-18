ca89c9e499edf85630830d1c76cb93b9f324de23
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-18 05:31:10 +0530

    modified

--- src/LINKED_LIST/SAMPLE.java
+++ src/LINKED_LIST/SAMPLE.java
@@ -3,4 +3,12 @@ package LINKED_LIST;
 public class SAMPLE {
 }
 
-// linke dlist
+// linked list is similar to the arrays
+// linked list  is not be continious like array and also stores the different location in memory location
+
+// one of the main advantage of linked list is size of an linked list is any
+
+
+// starting point of the linked list is called head
+// ending point of linked list is called tail
+// element next to the tail is called null in java or nullpointer in cpp
