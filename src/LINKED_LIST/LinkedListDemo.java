b564b012bdd284f895371dfc94a7999e774ffe6a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-19 04:15:14 +0530

    modified

--- src/LINKED_LIST/LinkedListDemo.java
+++ src/LINKED_LIST/LinkedListDemo.java
@@ -1,2 +1,28 @@
-package LINKED_LIST;public class LinkedListDemo {
+package LINKED_LIST;
+
+
+import java.lang.classfile.components.ClassPrinter;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+
+    }
+
+    private Node he
+
+    private class node {
+        private int value;
+        private node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+        public Node(int value, int next){
+            this.value = value;
+            this.next = next;
+        }
+
+
 }
