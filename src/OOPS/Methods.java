1c8904212dd03b99679cb8ae10604fe599f7abd4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-19 05:55:39 +0530

    modified

--- src/OOPS/Methods.java
+++ src/OOPS/Methods.java
@@ -1,2 +1,28 @@
-package OOPS;public class Methods {
+package OOPS;
+
+
+
+// function inside the class is called method and outside the class is called functions
+// in java every thing is located inside the class so we call method in java
+
+public class Methods {
+    public static void main(String[] args) {
+     // function calling
+     add();
+    }
+
+    // function definition
+    public  static void add(){  // void is a return type    => void refers to return no values
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+    }
 }
+
+
+
+// two parts
+
+// function definition - to define the function
+// function calling - to call the function
