c27dca34cd63bb836d7146e6a634bfee4bea91b1
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-20 05:16:22 +0530

    Modified on 20-07-2024

--- src/OOPS/inheritance.java
+++ src/OOPS/inheritance.java
@@ -3,7 +3,7 @@ package OOPS;
 public class inheritance {
 
 }
-
+// animal is base class or super class
 class animal{
     int noLegs = 4;
 
@@ -15,6 +15,7 @@ class animal{
     }
 }
 
+// dog class is a sub class
 class dog extends animal{
     Boolean canBark = true;
     int noLegs = 5;
@@ -30,10 +31,21 @@ class  call{
 }
 
 // METHOD overriding
-//
+//method overriding is the process of override the methods from the main class to sub class
+// if we have to override the function that compilers only takes the subclass methods not main class method this method is called method overriding
+
 
 
 // inheritance is the idea of reusability
 // it means we can reuse the code from different classes
 
 // we can get the value from one class o another using extends keyword
+
+// types of inheritance
+ // 1 > single inheritance one parent = one child
+// 2 > multi level inheritance grand parent = parent = child
+// 3 > hierarchical inheritance = one parent = multiple child
+// 4 > hybrid inheritance mixing of two or more inheritance
+// 5 > multiple inheritance it has two or more parent but only onr child
+
+// java does not support multiple inheritance directly so it gives the feature of interface
