094db64041e59e95d062909452d141bc1276aded
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-19 19:45:39 +0530

    Modified on 19-07-2024

--- src/OOPS/Classes.java
+++ src/OOPS/Classes.java
@@ -1,2 +1,33 @@
-package OOPS;public class Classes {
-}
+package OOPS;
+
+// classes is a collection variables , identifiers and methods
+
+// object is a instance of an object => copy of an classes
+
+// creating a class
+public class Classes {
+         String name;
+         int rollNo;
+
+         // name and roll no is a member of a class
+
+         public void display(){
+             System.out.println(name);
+             System.out.println(rollNo);
+         }
+
+    public static void main(String[] args) {
+
+        // creating an object
+        Classes person = new Classes();
+        // Classes is a class name
+        // new keyword is used to allocate memory space
+
+        person.name = "Dhanush";
+        person.rollNo = 10;
+        person.display();
+
+    }
+
+    }
+
