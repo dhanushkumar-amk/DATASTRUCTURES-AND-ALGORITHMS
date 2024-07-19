3e85a456b274bcc2efaa65d5739e8b85ab3b5c6c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-20 03:57:34 +0530

    modified

--- src/OOPS/Constructor.java
+++ src/OOPS/Constructor.java
@@ -1,2 +1,18 @@
-package OOPS;public class Constructor {
+package OOPS;
+
+public class Constructor {
+    public static void main(String[] args) {
+        sample obj = new sample();
+        System.out.println(obj.maks);
+    }
+}
+class sample{
+    int rollNO;
+    int maks;
+
+    sample(){
+        System.out.println("This is default constructor");
+        rollNO = 10;
+        maks= 20;
+    }
 }
