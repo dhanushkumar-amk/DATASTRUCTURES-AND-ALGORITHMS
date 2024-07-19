b40be969bbaf2620ab9a46eb93016dff8594d94f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-20 04:39:25 +0530

    modified

--- src/OOPS/inheritance.java
+++ src/OOPS/inheritance.java
@@ -1,2 +1,27 @@
-package OOPS;public class inheritance {
+package OOPS;
+
+public class inheritance {
+
+}
+
+class animal{
+    int noLegs = 4;
+
+    public static  void eat(){
+        System.out.println("I will eat");
+    }
+    public static void walk(){
+        System.out.println("I can be walk");
+    }
+}
+
+class dog extends animal{
+    Boolean canBark = true;
+}
+
+class  call{
+    public static void main(String[] args) {
+        animal obj = new animal();
+        System.out.println(obj.noLegs);
+    }
 }
