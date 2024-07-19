20ef7c4809ded68bdac54a6b6ab039206985e007
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-19 06:17:21 +0530

    modified

--- src/OOPS/Recursion.java
+++ src/OOPS/Recursion.java
@@ -1,2 +1,22 @@
-package OOPS;public class Recursion {
+package OOPS;
+
+// recursion a function calls itself as a function definition
+public class Recursion {
+    public static void main(String[] args) {
+        naturalNumber(10);
+    }
+    public static  void naturalNumber(int n){
+        // base case
+        if(n == 1) System.out.println(1);
+        else {
+            System.out.println(n);
+            // recursive case
+            naturalNumber(n-1);
+        }
+    }
 }
+
+
+// two concepts
+// base case => in this case the function has been ended
+// recursive case => in this case the function calls repeated
