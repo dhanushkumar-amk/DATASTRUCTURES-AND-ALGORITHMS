15ce4a3d5611453462a9020868759fdef23abcfc
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-20 04:51:34 +0530

    modified

--- src/OOPS/Polymorphism.java
+++ src/OOPS/Polymorphism.java
@@ -1,5 +1,4 @@
 package OOPS;
-
 public class Polymorphism {
     void display(){
         System.out.println("I am a void function");
@@ -7,15 +6,27 @@ public class Polymorphism {
 
     void display(int n){
         System.out.println(n);
-   display();
     }
 
 
+    void display(int n, int m){
+        System.out.println(n);
+        System.out.println(m);
+
+    }
 }
+
 class print{
     public static void main(String[] args) {
         Polymorphism obj = new Polymorphism();
+
+        // method overloading
         obj.display();
         obj.display(5);
+        obj.display(10,10);
     }
 }
+
+
+// same function has different parameters or different task
+// here example is display function
