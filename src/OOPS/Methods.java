d92779d60febbe7127c5777cb0546a40300f021c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-19 06:01:30 +0530

    modified

--- src/OOPS/Methods.java
+++ src/OOPS/Methods.java
@@ -9,20 +9,30 @@ public class Methods {
     public static void main(String[] args) {
      // function calling
      add();
+     add1(10,30);
     }
 
     // function definition
+    // void function without parameters
     public  static void add(){  // void is a return type    => void refers to return no values
         int a = 10;
         int b = 20;
         int c = a + b;
         System.out.println(c);
     }
-}
 
+    // void function with parameters
+    public  static  void  add1(int a, int b){
+        int c = a + b;
+    }
+}
 
 
 // two parts
 
 // function definition - to define the function
 // function calling - to call the function
+
+
+// void function  => it does't return value
+// non void function => it returns some value
