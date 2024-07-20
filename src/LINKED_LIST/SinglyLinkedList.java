4108793a313c487a4e094e1fc21eb7c1af52a6dc
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-20 12:07:57 +0530

    Modified on 20-07-2024

--- src/LINKED_LIST/SinglyLinkedList.java
+++ src/LINKED_LIST/SinglyLinkedList.java
@@ -1,2 +1,12 @@
-package LINKED_LIST;public class SinglyLinkedList {
+package LINKED_LIST;
+
+import java.util.LinkedList;
+
+public class SinglyLinkedList {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+    list.add(34);
+    }
+
 }
