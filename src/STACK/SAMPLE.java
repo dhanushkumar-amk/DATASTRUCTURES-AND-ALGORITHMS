1e6b41b6db2ad74c3b7c1a04fffb3a0577fe7d6e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-21 04:31:04 +0530

    Modified on 21-07-2024

--- src/STACK/SAMPLE.java
+++ src/STACK/SAMPLE.java
@@ -1,4 +1,24 @@
 package STACK;
 
+import java.util.Stack;
+
 public class SAMPLE {
+    public static void main(String[] args) {
+
+
+        // creating the stack
+        Stack<Integer> stack = new Stack<>();
+
+        // add the elements to the stack
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        // remove the element in stack
+        stack.pop();
+
+        System.out.println(stack);
+    }
 }
