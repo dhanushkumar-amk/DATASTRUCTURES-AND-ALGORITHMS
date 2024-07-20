7eaa7e315bc043f4f461239f795cbb666e14d29b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-20 12:17:40 +0530

    Modified on 20-07-2024

--- src/LINKED_LIST/CustomLinkedList.java
+++ src/LINKED_LIST/CustomLinkedList.java
@@ -1,2 +1,27 @@
-package LINKED_LIST;public class CustomLinkedList {
+package LINKED_LIST;
+
+public class CustomLinkedList {
+
+    private  Node head;
+    private  Node tail;
+    private int size;
+
+    public  CustomLinkedList() {
+        this.size = 0;
+    }
+
+
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
 }
--- src/LINKED_LIST/SinglyLinkedList.java
+++ src/LINKED_LIST/SinglyLinkedList.java
@@ -5,8 +5,14 @@ import java.util.LinkedList;
 public class SinglyLinkedList {
 
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-    list.add(34);
+
+
+        // LinkedList<Integer> list = new LinkedList<>();
+
+        // to add the element in the linked list
+         // list.add(34);
+
+
     }
 
 }
