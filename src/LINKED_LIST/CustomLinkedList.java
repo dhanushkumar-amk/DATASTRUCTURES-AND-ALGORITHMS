3c715ba6098ae69e7c9d7a914190bb1f40b9968e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-20 12:46:20 +0530

    Modified on 20-07-2024

--- src/LINKED_LIST/CustomLinkedList.java
+++ src/LINKED_LIST/CustomLinkedList.java
@@ -29,7 +29,7 @@ public class CustomLinkedList {
     public void display(){
         Node temp = head;
         while( temp != null){
-            System.out.println(temp.value + " -> ");
+            System.out.print(temp.value + " -> ");
             temp = temp.next;
         }
         System.out.println("End");
