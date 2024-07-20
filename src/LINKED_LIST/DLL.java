003ec257f335b8101efc9c4a4efd4a0c449e189f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-20 18:06:34 +0530

    Modified on 20-07-2024

--- src/LINKED_LIST/DLL.java
+++ src/LINKED_LIST/DLL.java
@@ -1,6 +1,24 @@
 package LINKED_LIST;
 
 public class DLL {
+
+    Node head;
+     public void insertFirst(int val){
+         Node node = new Node(val);
+         node.next = head;
+         node.previous = null;
+
+         if(head != null)
+             head.previous = node;
+
+         head = node;
+     }
+
+
+
+
+
+
     private class Node{
         int val;
         Node next;
@@ -8,8 +26,11 @@ public class DLL {
 
     public Node(int val){
         this.val = val;
-    }
-
-
+      }
+      public Node(int val, Node next, Node previous){
+        this.val = val;
+        this.next = next;
+        this.previous = previous;
+      }
     }
 }
