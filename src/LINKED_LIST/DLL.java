95e2549f3d54013635710e3928a3dc0866b6ed4e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-20 17:55:43 +0530

    Modified on 20-07-2024

--- src/LINKED_LIST/DLL.java
+++ src/LINKED_LIST/DLL.java
@@ -1,2 +1,15 @@
-package LINKED_LIST;public class DLL {
+package LINKED_LIST;
+
+public class DLL {
+    private class Node{
+        int val;
+        Node next;
+        Node previous;
+
+    public Node(int val){
+        this.val = val;
+    }
+
+
+    }
 }
