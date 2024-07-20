5708e346d9a850be4ce5c118718cebacaeed0084
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-20 19:47:21 +0530

    Modified on 20-07-2024

--- src/LINKED_LIST/CircularLinkedList.java
+++ src/LINKED_LIST/CircularLinkedList.java
@@ -1,2 +1,15 @@
-package LINKED_LIST;public class CircularLinkedList {
+package LINKED_LIST;
+
+public class CircularLinkedList {
+    public static void main(String[] args) {
+        CLL list = new CLL();
+
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+
+        list.display();
+    }
 }
