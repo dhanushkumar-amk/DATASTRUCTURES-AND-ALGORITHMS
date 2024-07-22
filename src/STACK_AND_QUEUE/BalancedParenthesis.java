6bfa9b9fd923c6f64cd28d1ea954c74883cb1639
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-22 19:38:29 +0530

    modified

--- src/STACK_AND_QUEUE/BalancedParenthesis.java
+++ src/STACK_AND_QUEUE/BalancedParenthesis.java
@@ -1,7 +1,29 @@
 package STACK_AND_QUEUE;
 
+import java.util.Stack;
+
 public class BalancedParenthesis {
-    public static void main(String[] args) {
-        
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+        for (char it : s.toCharArray()) {
+            if (it == '(' || it == '[' || it == '{')
+                st.push(it);
+            else {
+                if(st.isEmpty()) return false;
+                char ch = st.pop();
+                if((it == ')' && ch == '(') ||  (it == ']' && ch == '[') || (it == '}' && ch == '{')) continue;
+                else return false;
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main (String[] args) {
+
+        String s="()[{}()]";
+        if(isValid(s)==true)
+            System.out.println("True");
+        else
+            System.out.println("False");
     }
 }
