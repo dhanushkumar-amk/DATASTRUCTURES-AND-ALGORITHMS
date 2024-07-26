7658575f333f6762a43d042e6ae1147cb8b236a5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-26 19:53:03 +0530

    modified

--- src/STACK_AND_QUEUE/NextGreaterElement1.java
+++ src/STACK_AND_QUEUE/NextGreaterElement1.java
@@ -1,5 +1,35 @@
 package STACK_AND_QUEUE;
 
+
+import java.util.Stack;
+
 public class NextGreaterElement1 {
-    
+    public static int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int nge[] = new int[n];
+        Stack< Integer > st = new Stack < > ();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (st.isEmpty() == false && st.peek() <= nums[i % n]) {
+                st.pop();
+            }
+
+            if (i < n) {
+                if (st.isEmpty() == false) nge[i] = st.peek();
+                else nge[i] = -1;
+            }
+
+            st.push(nums[i % n]);
+        }
+        return nge;
+    }
+    public static void main(String args[]) {
+        int arr[]={5,7,1,2,6,0};
+
+        int arr2[] = nextGreaterElements(arr);
+        System.out.println("The next greater elements are ");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+
+    }
 }
