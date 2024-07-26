21b1c2c374920359f2ccba04196edfe06a4f0343
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-26 19:54:24 +0530

    Modified on 27-07-2024

--- src/STACK_AND_QUEUE/ImplementMinstack.java
+++ src/STACK_AND_QUEUE/ImplementMinstack.java
@@ -1,49 +1,56 @@
-package STACK_AND_QUEUE;
-
-import java.util.*;
-
-public class ImplementMinstack {
-    Stack < Long > st = new Stack < Long > ();
-    Long mini;
-
-    /** initialize your data structure here. */
-    public MinStack() {
-        mini = Long.MAX_VALUE;
-    }
-
-    public void push(int value) {
-        Long val = Long.valueOf(value);
-        if (st.isEmpty()) {
-            mini = val;
-            st.push(val);
-        } else {
-            if (val < mini) {
-                st.push(2 * val - mini);
-                mini = val;
-            } else {
-                st.push(val);
-            }
-        }
-    }
-
-    public void pop() {
-        if (st.isEmpty()) return;
-
-        Long val = st.pop();
-        if (val < mini) {
-            mini = 2 * mini - val;
-        }
-    }
-
-    public int top() {
-        Long val = st.peek();
-        if (val < mini) {
-            return mini.intValue();
-        }
-        return val.intValue();
-    }
-
-    public int getMin() {
-        return mini.intValue();
-    }
-}
+//package STACK_AND_QUEUE;
+//
+//import java.util.*;
+//
+//public class ImplementMinstack {
+//
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    Stack < Long > st = new Stack < Long > ();
+//    Long mini;
+//
+//
+//
+//    /** initialize your data structure here. */
+//    public long MinStack() {
+//        mini = Long.MAX_VALUE;
+//    }
+//
+//    public void push(int value) {
+//        Long val = Long.valueOf(value);
+//        if (st.isEmpty()) {
+//            mini = val;
+//            st.push(val);
+//        } else {
+//            if (val < mini) {
+//                st.push(2 * val - mini);
+//                mini = val;
+//            } else {
+//                st.push(val);
+//            }
+//        }
+//    }
+//
+//    public void pop() {
+//        if (st.isEmpty()) return;
+//
+//        Long val = st.pop();
+//        if (val < mini) {
+//            mini = 2 * mini - val;
+//        }
+//    }
+//
+//    public int top() {
+//        Long val = st.peek();
+//        if (val < mini) {
+//            return mini.intValue();
+//        }
+//        return val.intValue();
+//    }
+//
+//    public int getMin() {
+//        return mini.intValue();
+//    }
+//}
