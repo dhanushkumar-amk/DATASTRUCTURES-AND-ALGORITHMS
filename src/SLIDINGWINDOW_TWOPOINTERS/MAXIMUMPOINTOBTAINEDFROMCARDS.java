bb00b9ab115cd6b179ccf41bb5399e9a3394587a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-27 15:23:24 +0530

    Modified on 27-07-2024

--- src/SLIDINGWINDOW_TWOPOINTERS/MAXIMUMPOINTOBTAINEDFROMCARDS.java
+++ src/SLIDINGWINDOW_TWOPOINTERS/MAXIMUMPOINTOBTAINEDFROMCARDS.java
@@ -1,2 +1,31 @@
-package SLIDINGWINDOW_TWOPOINTERS;public class MAXIMUMPOINTOBTAINEDFROMCARDS {
+package SLIDINGWINDOW_TWOPOINTERS;
+
+public class MAXIMUMPOINTOBTAINEDFROMCARDS {
+    public static void main(String[] args) {
+        int[] nums ={6,2,3,4,7,2,1,7,1};
+        int k = 4;
+        System.out.println(maximumPoint(nums, k));
+    }
+
+    static int maximumPoint(int[] nums, int k){
+        int n = nums.length;
+        int leftSum = 0;
+        int rightSum = 0;
+        int maxSum = 0;
+
+        for (int i = 0; i < k-1 ; i++) {
+            leftSum += nums[i];
+            maxSum = leftSum;
+        }
+        int rightIndex = n-1;
+        for (int i = k-1; i >= 0 ; i--) {
+            leftSum -= nums[i];
+            rightSum += nums[rightIndex];
+            rightIndex--;
+        }
+
+        maxSum = Math.max(maxSum, leftSum+rightSum);
+
+        return maxSum;
+    }
 }
