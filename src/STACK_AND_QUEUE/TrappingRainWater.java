704978860c7714c09eadc5a0fb46a7edb7ada35c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-27 10:27:11 +0530

    Modified on 27-07-2024

--- src/STACK_AND_QUEUE/TrappingRainWater.java
+++ src/STACK_AND_QUEUE/TrappingRainWater.java
@@ -1,2 +1,26 @@
-package STACK_AND_QUEUE;public class TrappingRainWater {
+package STACK_AND_QUEUE;
+
+public class TrappingRainWater {
+
+    // better solution but easy apporouch
+    static int trap1(int[] arr) {
+        int n = arr.length;
+        int prefix[] = new int[n];
+        int suffix[] = new int[n];
+        prefix[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = Math.max(prefix[i - 1], arr[i]);
+        }
+        suffix[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = Math.max(suffix[i + 1], arr[i]);
+        }
+        int waterTrapped = 0;
+        for (int i = 0; i < n; i++) {
+            waterTrapped += Math.min(prefix[i], suffix[i]) - arr[i];
+        }
+        return waterTrapped;
+    }
+
+    // efficient solution but some tricks
 }
