886c769cab90158d6d20909c21e9cb125cb8a398
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-27 10:29:13 +0530

    Modified on 27-07-2024

--- src/MATHEMATICALS/SumofSeries.java
+++ src/MATHEMATICALS/SumofSeries.java
@@ -1,8 +1,11 @@
-// sum the given series
-
-class SumofSeries {
-    public static int seriesSum(int n) {
-        int sum = ((long)n * (long)(n + 1)) / 2;
-        return sum;
-    }
-}
+//package MATHEMATICALS;// sum the given series
+//
+//class SumofSeries {
+//    public static void main(String[] args) {
+//        System.out.println(seriesSum(n));
+//    }
+//    public static int seriesSum(int n) {
+//        int sum = ((long)n * (long)(n + 1)) / 2;
+//        return sum;
+//    }
+//}
