78d6acaac4fd3ec0b612b020bf4a4c0901161182
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-27 10:22:01 +0530

    Modified on 27-07-2024

--- src/LINKED_LIST/TrappingRainWater.java
+++ src/LINKED_LIST/TrappingRainWater.java
@@ -28,11 +28,29 @@ public class TrappingRainWater {
 
 
     /* easy approach but better solution not efficient solution  */
-
+    static int trap1(int[] arr) {
+        int n = arr.length;
+        int prefix[] = new int[n];
+        int suffix[] = new int[n];
+        prefix[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = Math.max(prefix[i - 1], arr[i]);
+        }
+        suffix[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = Math.max(suffix[i + 1], arr[i]);
+        }
+        int waterTrapped = 0;
+        for (int i = 0; i < n; i++) {
+            waterTrapped += Math.min(prefix[i], suffix[i]) - arr[i];
+        }
+        return waterTrapped;
+    }
 
 
     public static void main(String args[]) {
         int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println("The total unit of water is stored is : " + trap(arr));
+        System.out.println("The total unit of water is stored is : " + trap1(arr));
     }
 }
