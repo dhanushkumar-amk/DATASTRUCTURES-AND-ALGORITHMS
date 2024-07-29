e1df3010f1cdeba6afd50f38fe1d3ade511b5a40
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-29 20:56:31 +0530

    modified

--- src/SLIDINGWINDOW_TWOPOINTERS/MaximumConsecutiveOnes3.java
+++ src/SLIDINGWINDOW_TWOPOINTERS/MaximumConsecutiveOnes3.java
@@ -30,8 +30,9 @@ public class MaximumConsecutiveOnes3 {
                 if(zeros <= k){
                     length = right - left + 1;
                     maxLength = Math.max(length, maxLength);
+
+                    right++;
                 }
-                right++;
             }
         return  maxLength;
     }
