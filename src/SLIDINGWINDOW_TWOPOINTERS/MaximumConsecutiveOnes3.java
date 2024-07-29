96625c3fa3588a3bffbc5405bbc077ed47a43cec
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-29 20:49:32 +0530

    modified

--- src/SLIDINGWINDOW_TWOPOINTERS/MaximumConsecutiveOnes3.java
+++ src/SLIDINGWINDOW_TWOPOINTERS/MaximumConsecutiveOnes3.java
@@ -1,2 +1,41 @@
-package SLIDINGWINDOW_TWOPOINTERS;public class MaximumConsecutiveOnes3 {
+package SLIDINGWINDOW_TWOPOINTERS;
+
+public class MaximumConsecutiveOnes3 {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
+        int k=2;
+        System.out.println("The maximum Consecutive element is : " + maximumConsecutive(nums, k));
+
+    }
+    public static int maximumConsecutive(int[] nums, int k){
+
+        // init points
+
+        int left = 0;
+        int right = 0;
+        int zeros = 0;
+        int length = 0;
+        int maxLength = 0;
+
+        while(right < nums.length){
+            if(nums[right] == 0)
+                zeros++;
+
+            if(zeros > k){
+                if(nums[left] == 0){
+                    zeros--;
+                    left++;
+                }
+
+                if(zeros <= k){
+                    length = right - left + 1;
+                    maxLength = Math.max(maxLength, length);
+                }
+                right++;
+            }
+        }
+
+        return  maxLength;
+        return maxLength;
+    }
 }
