03293c41dafe5a36b320b3c6e556e1110f33e313
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-02 19:25:01 +0530

    modified

--- src/ARRAYS/CountTheNumberInArray.java
+++ src/ARRAYS/CountTheNumberInArray.java
@@ -1,2 +1,18 @@
-package ARRAYS;public class CountTheNumberInArray {
+package ARRAYS;
+
+public class CountTheNumberInArray {
+
+    public static void main(String[] args) {
+
+        int number = 5;
+        int[]  arr = {1,1,2,3,4,5,6,5,5,5,5,5};
+        System.out.println(count(arr, number));
+    }
+    static int count(int[] arr, int number){
+        int count = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i] == number) count++;
+        }
+        return  count;
+    }
 }
