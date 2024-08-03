330312d450fb771cbdb5e6093581b7671c68c3d7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-04 04:47:40 +0530

    modified

--- src/ZZZZZ/Accenture/TrailingZeros.java
+++ src/ZZZZZ/Accenture/TrailingZeros.java
@@ -1,2 +1,18 @@
-package ZZZZZ.Accenture;public class TrailingZeros {
+package ZZZZZ.Accenture;
+
+public class TrailingZeros {
+    public static void main(String[] args) {
+        int n = 100;
+        System.out.println(CountZeros(n));
+    }
+    static  int CountZeros(int n){
+        if(n < 5) return 0; // there is no zeros in the less than 5!(factorial)
+
+        int sum = 0;
+        while(n >= 5){
+            sum += n/5;
+            n /= 5;
+        }
+        return  sum;
+    }
 }
