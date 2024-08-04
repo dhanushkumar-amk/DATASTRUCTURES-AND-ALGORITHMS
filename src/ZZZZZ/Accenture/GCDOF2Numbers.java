ba920245f818292598ec8cb78e0d0fac31fc5d32
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-04 08:06:55 +0530

    modified

--- src/ZZZZZ/Accenture/GCDOF2Numbers.java
+++ src/ZZZZZ/Accenture/GCDOF2Numbers.java
@@ -1,2 +1,17 @@
-package ZZZZZ.Accenture;public class GCDOF2Numbers {
+package ZZZZZ.Accenture;
+
+public class GCDOF2Numbers {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 11;
+        System.out.println(gcd(a, b));
+    }
+    static  int gcd(int a, int b){
+        // base case
+        if(b == 0)
+            return a;
+
+        // recursive case
+        return  gcd(b, a % b);
+    }
 }
