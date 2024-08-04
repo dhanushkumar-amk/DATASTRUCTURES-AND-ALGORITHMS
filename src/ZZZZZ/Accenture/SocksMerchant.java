19f29a03d7549a98a5a93e650ab01335e3263a9c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-04 08:42:47 +0530

    Modified on 04-08-2024

--- src/ZZZZZ/Accenture/SocksMerchant.java
+++ src/ZZZZZ/Accenture/SocksMerchant.java
@@ -4,19 +4,27 @@ import java.util.HashMap;
 
 public class SocksMerchant {
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 1, 2, 1, 3, 2}; // Example array of socks
+        int n = arr.length;
+        int pairs = sock(arr, n);
+        System.out.println("Number of pairs: " + pairs);
     }
-    static int sock(int[] arr, int n){
+
+    static int sock(int[] arr, int n) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i <n ; i++)
-            map.put(arr[i], i+1);
-
-        int pair = 0;
-        for ( int value : map.get(i)){
-            pairs += i.get(i/2);
-
+        // Count occurrences of each sock color
+        for (int i = 0; i < n; i++) {
+            int count = map.getOrDefault(arr[i], 0);
+            map.put(arr[i], count + 1);
         }
-        return  pair;
+
+        int pairs = 0;
+        // Count pairs of each color
+        for (int count : map.values()) {
+            pairs += count / 2;
+        }
+
+        return pairs;
     }
 }
