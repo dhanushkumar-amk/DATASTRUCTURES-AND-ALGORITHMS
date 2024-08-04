81befc5e0d0164260b8e95ba5376ecd3b3e5b796
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-04 17:09:39 +0530

    modified

--- src/ZZZZZ/Accenture/Pattern.java
+++ src/ZZZZZ/Accenture/Pattern.java
@@ -1,2 +1,18 @@
-package ZZZZZ.Accenture;public class Pattern {
+package ZZZZZ.Accenture;
+
+public class Pattern {
+    public static void main(String[] args) {
+       int n = 4;
+       pattern(n);
+    }
+    static  void pattern(int n){
+        int spaces = 2*n- 2;
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 0; j <i ; j++)
+                System.out.println(i);
+
+            for (int j = 0; j < spaces ; j++)
+                System.out.println(" ");
+        }
+    }
 }
