feed518bf96ce84a37db7a63dc1d95e6b4759a93
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-04 05:46:26 +0530

    modified

--- src/ZZZZZ/Accenture/StringAndNumberBitManupulation.java
+++ src/ZZZZZ/Accenture/StringAndNumberBitManupulation.java
@@ -1,2 +1,22 @@
-package ZZZZZ.Accenture;public class StringAndNumberBitManupulation {
+package ZZZZZ.Accenture;
+
+public class StringAndNumberBitManupulation {
+    public static void main(String[] args) {
+        String s = "1C0C1C1A0B1";
+        System.out.println(Manipulation(s));
+    }
+    static  int Manipulation(String s){
+        int n = s.length();
+        int ans  = (int) s.charAt(0) - 48;
+
+        for (int i = 1; i <n; i=i+2){
+            if(s.charAt(i) == 'A')
+                ans = ans & s.charAt(i+1);
+            else if (s.charAt(i) == 'B')
+                ans = ans | s.charAt(i+1);
+            else
+                ans = ans ^ s.charAt(i+1);
+        }
+        return  ans;
+    }
 }
