e82027a4731a68888db148be4a07388f42b8579d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-04 09:03:12 +0530

    modified

--- src/ZZZZZ/Accenture/MatrixSearch.java
+++ src/ZZZZZ/Accenture/MatrixSearch.java
@@ -2,6 +2,27 @@ package ZZZZZ.Accenture;
 
 public class MatrixSearch {
     public static void main(String[] args) {
-        
+        int[][] arr = {
+                {1,2,4},
+                {7,8,9},
+        };
+        int n = 2;
+        int m = 3;
+        int target = 8;
+    }
+    static  int search(int[][] arr, int n, int m, int target){
+        int i = 0;
+        int j = m-1;
+
+        while(i < j && j >= 0){
+            if(arr[i][j] == target)
+                return  1;
+            else if(arr[i][j] > target)
+                j--;
+            else
+                i++;
+        }
+
+        return -1;
     }
 }
