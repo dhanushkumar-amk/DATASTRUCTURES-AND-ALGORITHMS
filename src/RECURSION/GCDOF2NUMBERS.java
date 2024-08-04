abc304ecfde172d5cdf15ad6c6d2a655af79da30
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-04 08:06:22 +0530

    modified

--- src/RECURSION/GCDOF2NUMBERS.java
+++ src/RECURSION/GCDOF2NUMBERS.java
@@ -1,2 +1,17 @@
-package RECURSION;public class GCDOF2NUMBERS {
+package RECURSION;
+
+public class GCDOF2NUMBERS {
+    public static void main(String[] args) {
+      int a = 25;
+      int b = 11;
+        System.out.println(gcd(a, b));
+    }
+    static  int gcd(int a, int b){
+        // base case
+        if(b == 0)
+            return a;
+
+        // recursive case
+        return  gcd(b, a % b);
+    }
 }
