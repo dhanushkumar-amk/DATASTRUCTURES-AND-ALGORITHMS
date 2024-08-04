713b734c9ecfab8a58f01bb64f23547b83839bdf
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-04 08:41:42 +0530

    modified

--- src/ZZZZZ/Accenture/SocksMerchant.java
+++ src/ZZZZZ/Accenture/SocksMerchant.java
@@ -1,2 +1,22 @@
-package ZZZZZ.Accenture;public class SocksMerchant {
+package ZZZZZ.Accenture;
+
+import java.util.HashMap;
+
+public class SocksMerchant {
+    public static void main(String[] args) {
+
+    }
+    static int sock(int[] arr, int n){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i <n ; i++)
+            map.put(arr[i], i+1);
+
+        int pair = 0;
+        for ( int value : map.get(i)){
+            pairs += i.get(i/2);
+
+        }
+        return  pair;
+    }
 }
