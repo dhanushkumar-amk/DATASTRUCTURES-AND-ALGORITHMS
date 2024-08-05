c553f1f3d43a454fa2898fd9f874ff6b8b7f395c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-05 19:18:25 +0530

    modified

--- src/ZZZZZ/Accenture/TwoDiceProblem.java
+++ src/ZZZZZ/Accenture/TwoDiceProblem.java
@@ -2,12 +2,18 @@ package ZZZZZ.Accenture;
 
 public class TwoDiceProblem {
     public static void main(String[] args) {
-
+int  n = 10;
+        System.out.println(countPairs(n));
     }
     static  int countPairs(int n){
         int count = 0;
 
+        // base conditions
         if(n > 12 || n < 2) return  0;
+
+        for (int i = 1; i <= 6 ; i++) {
+            
+        }
 
         return  count;
     }
