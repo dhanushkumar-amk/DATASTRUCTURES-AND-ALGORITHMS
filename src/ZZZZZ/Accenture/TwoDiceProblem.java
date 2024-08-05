482b8b3598899b440f0cb0179936d7d548be12d0
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-05 19:12:35 +0530

    Modified on 05-08-2024

--- src/ZZZZZ/Accenture/TwoDiceProblem.java
+++ src/ZZZZZ/Accenture/TwoDiceProblem.java
@@ -1,2 +1,14 @@
-package ZZZZZ.Accenture;public class TwoDiceProblem {
+package ZZZZZ.Accenture;
+
+public class TwoDiceProblem {
+    public static void main(String[] args) {
+
+    }
+    static  int countPairs(int n){
+        int count = 0;
+
+        if(n > 12 || n < 2) return  0;
+
+        return  count;
+    }
 }
