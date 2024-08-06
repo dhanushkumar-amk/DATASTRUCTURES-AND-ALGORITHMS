6eefb7386243e89c5bf4d363b5eebf179641c202
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-06 19:10:41 +0530

    Modified on 06-08-2024

--- src/ZZZZZ/Accenture/Movehyphen.java
+++ src/ZZZZZ/Accenture/Movehyphen.java
@@ -1,2 +1,17 @@
-package ZZZZZ.Accenture;public class Movehyphen {
+package ZZZZZ.Accenture;
+
+public class Movehyphen {
+    public static void main(String args[]){
+        String s = "hari-sudhan";
+        int n = s.length();
+        System.out.println(hypen(s, n));
+    }
+    public static String hypen(String s, int n){
+        String res = "";
+        for(int i=0; i< n; i++){
+            if(s.charAt(i) == '-') res = s.charAt(i) + res;
+            else res = res + s.charAt(i);
+        }
+        return res;
+    }
 }
