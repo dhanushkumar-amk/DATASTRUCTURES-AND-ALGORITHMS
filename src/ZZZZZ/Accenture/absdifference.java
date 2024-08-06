3923b11e804117a9196c243b7873ec28a775c197
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-06 18:49:47 +0530

    modified

--- src/ZZZZZ/Accenture/absdifference.java
+++ src/ZZZZZ/Accenture/absdifference.java
@@ -1,2 +1,24 @@
-package ZZZZZ.Accenture;public class absdifference {
+package ZZZZZ.Accenture;
+
+public class absdifference {
+    public static void main(String[] args) {
+    int[] arr = {12,3,14,56,77,13};
+    int n = 6;
+    int num = 13;
+    int diff = 2;
+        System.out.println(abs(arr, n, num, diff));
+    }
+    static  int abs(int[] arr, int n, int num, int diff){
+
+        int count = 0;
+
+        for (int i = 0; i <n; i++) {
+            if(Math.abs(arr[i]-num) <= diff) count++;
+        }
+
+        if(count == 0) return  -1;
+
+        return  count;
+    }
+
 }
