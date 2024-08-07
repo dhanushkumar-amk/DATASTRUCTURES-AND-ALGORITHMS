c805e8f04b319a2d1278563f9d1f1fa1fa757c45
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-07 20:31:55 +0530

    Modified on 07-08-2024

--- src/ZZZZZ/Accenture/isoGram.java
+++ src/ZZZZZ/Accenture/isoGram.java
@@ -2,20 +2,27 @@ package ZZZZZ.Accenture;
 
 import java.util.HashMap;
 
-public class isoGram {
+public class IsoGram {
     public static void main(String[] args) {
         String s = "machine";
         System.out.println(isGram(s));
     }
-    static  boolean isGram(String s){
-        HashMap<Character, Integer>  map = new HashMap<>();
 
-        for (int i = 0; i <s.length() ; i++) {
-            char count = map.getOrDefault(s.charAt(i), 0);
-            count[i]++;
-            
-            if(map[s[i]] > 1) return  false;
+    static boolean isGram(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            // Update the frequency count for the character in the map
+            map.put(currentChar, map.getOrDefault(currentChar, 0) + 1);
+
+            // If the character count is greater than 1, it's not an isogram
+            if (map.get(currentChar) > 1) {
+                return false;
+            }
         }
-        return  true;
+
+        // If no character has a count greater than 1, it's an isogram
+        return true;
     }
 }
