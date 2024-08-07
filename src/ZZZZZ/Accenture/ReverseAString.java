b9f94df1ae7fa130afe49e5d48a421d82687593d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-07 20:20:45 +0530

    modified

--- src/ZZZZZ/Accenture/ReverseAString.java
+++ src/ZZZZZ/Accenture/ReverseAString.java
@@ -1,2 +1,25 @@
-package ZZZZZ.Accenture;public class ReverseAString {
+package ZZZZZ.Accenture;
+
+public class ReverseAString {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(reverse(str));
+    }
+   static   String reverse(String str){
+
+        int n = str.length();
+        char temp;
+        int j = n-1;
+
+         for (int i = 0; i <n/2 ; i++) {
+           
+             temp = str.charAt(i);
+             str.charAt(i) = str.charAt(j);
+             str.charAt(j) = temp;
+             i++;
+             j--;
+         }
+
+         return  str;
+    }
 }
