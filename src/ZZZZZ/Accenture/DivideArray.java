9e6fe9c57e11e5f670b6b2562af78fc4d3424516
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-07 20:07:50 +0530

    Modified on 07-08-2024

--- src/ZZZZZ/Accenture/DivideArray.java
+++ src/ZZZZZ/Accenture/DivideArray.java
@@ -1,31 +1,33 @@
-package ZZZZZ.Accenture;
-
 import java.util.ArrayList;
-import java.util.Arrays;
+
+import java.util.Collections;
 
 public class DivideArray {
     public static void main(String[] args) {
-    int n = 5;
-    int[] arr = {3,4,1,7,9};
-        System.out.println(divide(n,arr));
-
+        int n = 5;
+        int[] arr = {3, 4, 1, 7, 9};
+        System.out.println(divide(n, arr));
     }
-    static int divide(int n, int[] arr){
+
+    static int divide(int n, int[] arr) {
         ArrayList<Integer> even = new ArrayList<>();
         ArrayList<Integer> odd = new ArrayList<>();
 
-        for (int i = 0; i <n ; i++) {
-            if(i % 2 == 0)
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0)
                 even.add(arr[i]);
             else
                 odd.add(arr[i]);
         }
 
-      Arrays.sort(even, even.getFirst(), even.getLast());
-      Arrays.sort(odd, odd.getFirst(), odd.getLast());
+        // Sorting even and odd ArrayLists
+        Collections.sort(even);
+        Collections.sort(odd);
 
+        // Getting the second largest elements
+        int evenSecondLargest = even.get(even.size() - 2);
+        int oddSecondLargest = odd.get(odd.size() - 2);
 
-        return  even.get(even.size()-2) + odd.get(odd.size()-2);
-
+        return evenSecondLargest + oddSecondLargest;
     }
 }
