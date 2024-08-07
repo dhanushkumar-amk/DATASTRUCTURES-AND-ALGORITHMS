455485a42c036a8404b16400c4fc16c27acee9b4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-07 19:28:24 +0530

    Modified on 07-08-2024

--- src/ZZZZZ/Accenture/MaxExpotential.java
+++ src/ZZZZZ/Accenture/MaxExpotential.java
@@ -1,2 +1,36 @@
-package ZZZZZ.Accenture;public class MaxExpotential {
+package ZZZZZ.Accenture;
+
+public class MaxExpotential {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 12;
+        System.out.println(exp(a,b));
+    }
+    static  int count(int i){
+
+      int c = 0;
+        while ( i % 2 == 0 && i > 0){
+            c++;
+            i = i/2;
+        }
+        return  c;
+    }
+    static int exp(int a, int b){
+
+        int max = 0;
+        int num = 0;
+
+
+
+        for (int i = a; i <=b ; i++) {
+        int temp = count(i);
+        if(temp > max){
+            max = temp;
+            num = i;
+           }
+        }
+        return  num;
+    }
+
+
 }
