886e2436349e29e756ce341fb14b78c49ad12724
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-07 20:31:27 +0530

    modified

--- src/ZZZZZ/Accenture/isoGram.java
+++ src/ZZZZZ/Accenture/isoGram.java
@@ -1,2 +1,21 @@
-package ZZZZZ.Accenture;public class isoGram {
+package ZZZZZ.Accenture;
+
+import java.util.HashMap;
+
+public class isoGram {
+    public static void main(String[] args) {
+        String s = "machine";
+        System.out.println(isGram(s));
+    }
+    static  boolean isGram(String s){
+        HashMap<Character, Integer>  map = new HashMap<>();
+
+        for (int i = 0; i <s.length() ; i++) {
+            char count = map.getOrDefault(s.charAt(i), 0);
+            count[i]++;
+            
+            if(map[s[i]] > 1) return  false;
+        }
+        return  true;
+    }
 }
