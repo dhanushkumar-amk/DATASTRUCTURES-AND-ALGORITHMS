371dfea6785f33b1ce0d5e2896645f44c47798a3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-07 19:39:49 +0530

    modified

--- src/ZZZZZ/Accenture/CompleteCalculate.java
+++ src/ZZZZZ/Accenture/CompleteCalculate.java
@@ -1,2 +1,19 @@
-package ZZZZZ.Accenture;public class CompleteCalculate {
+package ZZZZZ.Accenture;
+
+public class CompleteCalculate {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 50;
+        System.out.println(calculate(a,b));
+    }
+    static  int  calculate(int a, int b){
+        int sum = 0;
+
+        for (int i = a; i <=b ; i++) {
+            if(i % 15 == 0){
+                sum += i;
+            }
+        }
+        return  sum;
+    }
 }
