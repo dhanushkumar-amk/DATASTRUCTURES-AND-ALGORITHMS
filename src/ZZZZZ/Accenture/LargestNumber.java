9cb0f77f473d6103aa7def061488fa69b0f101f7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-08 18:59:11 +0530

    modified

--- src/ZZZZZ/Accenture/LargestNumber.java
+++ src/ZZZZZ/Accenture/LargestNumber.java
@@ -1,2 +1,18 @@
-package ZZZZZ.Accenture;public class LargestNumber {
+package ZZZZZ.Accenture;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        int[] arr = {33,44,55,66,77,99,88};
+        System.out.println(largestNum(arr));
+    }
+    static  int largestNum(int[] arr){
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] > max)
+                max = arr[i];
+        }
+
+        return  max;
+    }
 }
