c31f3c265cff37f522aae261145821e3b0a0a80a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-08 18:54:08 +0530

    modified

--- src/ZZZZZ/Accenture/ReverseANumber.java
+++ src/ZZZZZ/Accenture/ReverseANumber.java
@@ -2,6 +2,14 @@ package ZZZZZ.Accenture;
 
 public class ReverseANumber {
     public static void main(String[] args) {
-        
+        int n = 1234;
+
+        int reverseNumber = 0;
+        while (n != 0) {
+
+            int digit = n % 10;
+reverseNumber = reverseNumber * 10 + digit;
+
+        }
     }
 }
