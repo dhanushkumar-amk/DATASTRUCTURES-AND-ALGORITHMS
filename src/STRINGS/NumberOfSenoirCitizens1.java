053ade4b960edcc15f56dfab92b9a6e14fb67f14
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-15 18:17:56 +0530

    modified

--- src/STRINGS/NumberOfSenoirCitizens1.java
+++ src/STRINGS/NumberOfSenoirCitizens1.java
@@ -2,6 +2,6 @@ package STRINGS;
 
 public class NumberOfSenoirCitizens1 {
     public static void main(String[] args) {
-        String details = ""
+        String[] details = {"", "", ""};
     }
 }
