45677030d3fc6538853fdb62c0ca11b260bc9c4a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-15 18:20:31 +0530

    modified

--- src/STRINGS/NumberOfSenoirCitizens1.java
+++ src/STRINGS/NumberOfSenoirCitizens1.java
@@ -2,6 +2,16 @@ package STRINGS;
 
 public class NumberOfSenoirCitizens1 {
     public static void main(String[] args) {
-        String[] details = {"", "", ""};
+        String[] details = {"7868190130M7522","5303914400F9211","9273338290F4010"};
+        System.out.println(noOfCitizens(details));
+    }
+    static  int noOfCitizens(String[] details){
+        int count = 0;
+        for (int i = 0; i <details.length ; i++) {
+            String s = details[i].substring(11,13);
+            if(Integer.parseInt(s) > 60)
+                count++;
+        }
+        return  count;
     }
 }
