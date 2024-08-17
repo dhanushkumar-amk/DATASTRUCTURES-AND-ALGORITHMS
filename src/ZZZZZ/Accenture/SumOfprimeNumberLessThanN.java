6f0d5c2b5069732c81510e141de70429972adbec
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-17 16:14:53 +0530

    Modified on 17-08-2024

--- src/ZZZZZ/Accenture/SumOfprimeNumberLessThanN.java
+++ src/ZZZZZ/Accenture/SumOfprimeNumberLessThanN.java
@@ -2,22 +2,33 @@ package ZZZZZ.Accenture;
 
 public class SumOfprimeNumberLessThanN {
     public static void main(String[] args) {
-    int n = 10;
+        int n = 10;
         System.out.println(sumOfPrime(n));
     }
-    static  int sumOfPrime(int n){
+
+    static int sumOfPrime(int n) {
         int sum = 0;
 
-        if(n <= 1)
-            return  0;
-
-
-        for (int i = 2; i <n ; i++) {
-            if(n % i != 0)
+        for (int i = 2; i < n; i++) {
+            if (isPrime(i)) {
                 sum += i;
+            }
         }
 
-        return  sum;
+        return sum;
     }
 
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
