c19d8b24666080b51ab88da73c68afe36870347a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-17 15:30:24 +0530

    Modified on 17-08-2024

--- src/ZZZZZ/Accenture/CalculateSumOfPrimeindexValueInAnArray.java
+++ src/ZZZZZ/Accenture/CalculateSumOfPrimeindexValueInAnArray.java
@@ -8,11 +8,10 @@ public class CalculateSumOfPrimeindexValueInAnArray {
     static  boolean isPrime(int num){
         if(num <= 1)
             return  false;
-
         if(num == 2)
             return false;
 
-        for (int i = 2; i <num ; i++) {
+        for (int i = 2; i <=num ; i++) {
             if(num % i == 0)
                 return  false;
         }
