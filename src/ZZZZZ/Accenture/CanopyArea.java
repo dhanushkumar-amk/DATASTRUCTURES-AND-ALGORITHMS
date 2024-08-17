5450e688439b6ab899f06f712d476ebe2f49bc66
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-17 15:56:01 +0530

    modified

--- src/ZZZZZ/Accenture/CanopyArea.java
+++ src/ZZZZZ/Accenture/CanopyArea.java
@@ -2,7 +2,7 @@ package ZZZZZ.Accenture;
 
 public class CanopyArea {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 6;
         System.out.println(canopyAreaCalculation(n));
     }
     static  int canopyAreaCalculation(int n){
