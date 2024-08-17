656eccb63a718124c6fe28a696d50b602f2dfdb8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-17 16:13:47 +0530

    Modified on 17-08-2024

--- src/ZZZZZ/Accenture/SumOfprimeNumberLessThanN.java
+++ src/ZZZZZ/Accenture/SumOfprimeNumberLessThanN.java
@@ -11,7 +11,8 @@ public class SumOfprimeNumberLessThanN {
         if(n <= 1)
             return  0;
 
-        for (int i = 3; i <=n ; i++) {
+
+        for (int i = 2; i <n ; i++) {
             if(n % i != 0)
                 sum += i;
         }
