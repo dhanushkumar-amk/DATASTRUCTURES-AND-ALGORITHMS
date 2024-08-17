8909b21ee20fe079efd0de03e0b21ef85ebb0440
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-17 15:13:57 +0530

    modified

--- src/MATHEMATICALS/PassThePillow.java
+++ src/MATHEMATICALS/PassThePillow.java
@@ -2,7 +2,16 @@ package MATHEMATICALS;
 
 public class PassThePillow {
     public static void main(String[] args) {
-
+        int n = 4;
+        int time = 5;
+        System.out.println(pass(n, time));
     }
-    
+    static  int pass(int n, int time){
+        int remainder = time % (2*n-2);
+
+        if(remainder <= n-1)
+            return  remainder + 1;
+        else
+            return (2 * n - 2) - remainder + 1;
+    }
 }
