1ffae30ce407c90ad299cc788577659ad4b87c22
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-18 16:27:24 +0530

    Modified on 18-08-2024

--- src/MATHEMATICALS/PrintAllTheDivisorOfN.java
+++ src/MATHEMATICALS/PrintAllTheDivisorOfN.java
@@ -1,2 +1,28 @@
-package MATHEMATICALS;public class PrintAllTheDivisorOfN {
+package MATHEMATICALS;
+
+import java.util.ArrayList;
+
+public class PrintAllTheDivisorOfN {
+    public static void main(String[] args) {
+        int n = 36;
+        ArrayList<Integer> ans = divisor(n);
+        for (int i = 0; i <ans.size() ; i++) {
+            System.out.println(ans.get(i));
+        }
+    }
+    static ArrayList<Integer> divisor (int n){
+        ArrayList<Integer> list = new ArrayList<>();
+
+//        for (int i = 1; i <Math.sqrt(n) ; i++) {
+        for (int i = 1; i*i <= n; i++) {
+            if(n % i == 0){
+                list.add(i);
+
+                if(n / i != i)
+                    list.add(n/i);
+            }
+
+        }
+        return  list;
+    }
 }
