a75200e69edd2e4fb906199bfa4786ceaf02ffd8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-18 16:38:54 +0530

    Modified on 18-08-2024

--- src/MATHEMATICALS/GivenNumberIsPrimeOrnot.java
+++ src/MATHEMATICALS/GivenNumberIsPrimeOrnot.java
@@ -13,7 +13,7 @@ public class GivenNumberIsPrimeOrnot {
     static  boolean isPrime(int n){
         if(n <= 1)
             return  false;
-        for (int i = 2; i <n; i++) {
+        for (int i = 2; i*i <=n; i++) {
             if(n % i == 0)
                 return  false;
         }
