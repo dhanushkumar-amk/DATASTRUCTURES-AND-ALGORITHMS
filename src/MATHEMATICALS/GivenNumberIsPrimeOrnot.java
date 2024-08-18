4f1d99cbdaf69db5d579b1db887f94c7be055873
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-18 16:33:46 +0530

    modified

--- src/MATHEMATICALS/GivenNumberIsPrimeOrnot.java
+++ src/MATHEMATICALS/GivenNumberIsPrimeOrnot.java
@@ -3,7 +3,12 @@ package MATHEMATICALS;
 public class GivenNumberIsPrimeOrnot {
     public static void main(String[] args) {
         int n = 14;
-        System.out.println(isPrime(n));
+        if(isPrime(n) == true) {
+            System.out.println("prime");
+        }
+        else{
+            System.out.println("Not prime");
+        }
     }
     static  boolean isPrime(int n){
         if(n <= 1)
