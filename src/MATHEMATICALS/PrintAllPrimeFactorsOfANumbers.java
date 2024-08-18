ce514103fd946b2dbcc700b372e5d0d8c1d80077
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-18 16:47:06 +0530

    Modified on 18-08-2024

--- src/MATHEMATICALS/PrintAllPrimeFactorsOfANumbers.java
+++ src/MATHEMATICALS/PrintAllPrimeFactorsOfANumbers.java
@@ -1,2 +1,24 @@
-package MATHEMATICALS;public class PrintAllPrimeFactorsOfANumbers {
+package MATHEMATICALS;
+
+import java.util.ArrayList;
+
+public class PrintAllPrimeFactorsOfANumbers {
+    public static void main(String[] args) {
+      int n = 60;
+      ArrayList<Integer> ans = primeFactors(n);
+        for (int i = 0; i <ans.size() ; i++) {
+            System.out.print(ans.get(i));
+        }
+    }
+    static ArrayList<Integer> primeFactors(int n){
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 2; i * i <= n ; i++) {
+            if( n % i == 0){
+                list.add(i);
+            }
+        }
+
+        return  list;
+    }
 }
