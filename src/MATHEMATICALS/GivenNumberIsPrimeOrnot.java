89c1df281642d47525cc1a9002d06e05ab1f524b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-18 16:32:10 +0530

    modified

--- src/MATHEMATICALS/GivenNumberIsPrimeOrnot.java
+++ src/MATHEMATICALS/GivenNumberIsPrimeOrnot.java
@@ -1,2 +1,19 @@
-package MATHEMATICALS;public class GivenNumberIsPrimeOrnot {
+package MATHEMATICALS;
+
+public class GivenNumberIsPrimeOrnot {
+    public static void main(String[] args) {
+        int n = 3;
+
+        System.out.println(isPrime(n));
+    }
+    static  boolean isPrime(int n){
+        if(n <= 1)
+            return  false;
+
+        for (int i = 2; i <=n; i++) {
+            if(n % i == 0)
+                return  false;
+        }
+        return  true;
+    }
 }
--- src/MATHEMATICALS/PrintAllTheDivisorOfN.java
+++ src/MATHEMATICALS/PrintAllTheDivisorOfN.java
@@ -13,15 +13,15 @@ public class PrintAllTheDivisorOfN {
     static ArrayList<Integer> divisor (int n){
         ArrayList<Integer> list = new ArrayList<>();
 
-//        for (int i = 1; i <Math.sqrt(n) ; i++) {
+//        for (int i = 1; i <Math.sqrt(n) ; i++) { // for reduce time complexity use i * i instead of Math.sqrt(n)
         for (int i = 1; i*i <= n; i++) {
             if(n % i == 0){
                 list.add(i);
 
+                // for not repeating same number
                 if(n / i != i)
                     list.add(n/i);
             }
-
         }
         return  list;
     }
