92731e9720ced9ff9ff349b303a22fcbbf2cd7dd
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-21 15:28:42 +0530

    modified

--- src/ZZZZZ/Zoho/Sample.java
+++ src/ZZZZZ/Zoho/Sample.java
@@ -1,4 +1,22 @@
 package ZZZZZ.Zoho;
 
 public class Sample {
+    public static void main(String[] args) {
+        int m1 = 100;
+        int m2 = 100;
+        int m3 = 100;
+        int m4 = 100;
+        int m5 = 100;
+        average(m1,m2,m3, m4, m5);
+    }
+    static  void average(int m1, int m2, int m3, int m4, int m5){
+        int total = 0;
+        float avg = 0;
+
+        total = m1 + m2 + m3 + m4 + m5;
+        avg = total / 5;
+        if(avg > 70)
+            System.out.println("eligible");
+        else System.out.println("not eligible");
+    }
 }
