959e05190264ddb863fb5843ac283c7773797b2b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-21 15:15:29 +0530

    modified

--- src/ARRAYS/max.java
+++ src/ARRAYS/max.java
@@ -1,2 +1,16 @@
-package ARRAYS;public class max {
+package ARRAYS;
+
+public class max {
+    public static void main(String[] args) {
+        int[] arr = {2,3,45,7,8,90};
+        System.out.println(maxNumber(arr));
+    }
+    static  int maxNumber(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i <arr.length ; i++) {
+            if(max >= arr[i])
+                max = arr[i];
+        }
+        return  max;
+    }
 }
