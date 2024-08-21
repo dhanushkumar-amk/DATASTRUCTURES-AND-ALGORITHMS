8f9138e8bfa1ee08ff4d671930ec4d7cf02eef0f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-21 15:16:23 +0530

    Modified on 21-08-2024

--- src/ARRAYS/max.java
+++ src/ARRAYS/max.java
@@ -8,7 +8,7 @@ public class max {
     static  int maxNumber(int[] arr){
         int max = Integer.MIN_VALUE;
         for (int i = 0; i <arr.length ; i++) {
-            if(max >= arr[i])
+            if(arr[i] > max)
                 max = arr[i];
         }
         return  max;
