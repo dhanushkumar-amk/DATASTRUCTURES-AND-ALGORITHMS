595ac6d91234df7141b9c2f3428bf3509f951e32
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-23 18:53:16 +0530

    modified

--- src/MATHEMATICALS/Fibonnci_Series.java
+++ src/MATHEMATICALS/Fibonnci_Series.java
@@ -15,7 +15,9 @@ public class Fibonnci_Series {
 
         for (int i = 2; i <= n ; i++) {
             map[i] = map[i -1] + map[i-2];
-            System.out.println(map[i]);
+            System.out.println(
+                map[0] + " " + map[1] + " " +
+                    map[i]);
         }
     }
 }
