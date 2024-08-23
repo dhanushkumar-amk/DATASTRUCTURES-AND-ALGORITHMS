933e21ed346135bcf9ac85463972e3c86ba06fa3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-23 19:16:58 +0530

    Modified on 23-08-2024

--- src/MATHEMATICALS/SumOfSquareNumbers.java
+++ src/MATHEMATICALS/SumOfSquareNumbers.java
@@ -1,2 +1,31 @@
-package MATHEMATICALS;public class SumOfSquareNumbers {
+package MATHEMATICALS;
+
+public class SumOfSquareNumbers {
+    public static void main(String[] args) {
+        int n = 73;
+        System.out.println(SquareNumber(n));
+    }
+    static  boolean SquareNumber(int n){
+
+        if( n <= 0)
+            return  false;
+
+
+        int left = 0;
+        int right = (int) Math.sqrt(n);
+
+
+        while (left <= right){
+            long sum = left * left + right * right;
+
+            if( sum == n)
+                return  true;
+            else if(sum < n)
+                left++;
+            else
+                right--;
+        }
+
+        return false;
+    }
 }
