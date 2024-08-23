ce3ba104c72fc1c2db893b9edcb727fe9ce2292b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-23 18:43:59 +0530

    modified

--- src/MATHEMATICALS/Fibonnci_Series.java
+++ src/MATHEMATICALS/Fibonnci_Series.java
@@ -1,2 +1,16 @@
-package MATHEMATICALS;public class Fibonnci_Series {
+package MATHEMATICALS;
+
+public class Fibonnci_Series {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fibo(n));
+    }
+    static  int  fibo(int n){
+
+        if(n <= 1)
+            return n;
+
+        return  fibo(n - 1) + fibo(n -2);
+
+    }
 }
