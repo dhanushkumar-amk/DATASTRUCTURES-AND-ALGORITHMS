c8a610d7f16454585cbc55bdf545f2c39c042070
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-24 19:48:05 +0530

    Modified on 24-08-2024

--- src/MATHEMATICALS/SeiveOfEratosthenes.java
+++ src/MATHEMATICALS/SeiveOfEratosthenes.java
@@ -4,11 +4,22 @@ public class SeiveOfEratosthenes {
     public static void main(String[] args) {
         int  n = 40;
         boolean[] arr = new  boolean[n + 1];
-
+      seive(n , arr);
     }
-    static  void seive(int n, boolean arr){
+    static  void seive(int n, boolean[] arr){
 
-        
+        for (int i = 2; i <= n ; i++) {
 
+            if(arr[i] == false){
+                for (int j = i * 2; j <=n ; j+= i) {
+                    arr[j] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i <=n ; i++) {
+            if(arr[i] == false)
+                System.out.println(i);
+        }
     }
 }
