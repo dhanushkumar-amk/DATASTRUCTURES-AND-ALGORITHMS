698482cd8728ebb287072c3e0bba51039b216eeb
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-24 19:45:14 +0530

    Modified on 24-08-2024

--- src/MATHEMATICALS/SeiveOfEratosthenes.java
+++ src/MATHEMATICALS/SeiveOfEratosthenes.java
@@ -1,2 +1,14 @@
-package MATHEMATICALS;public class SeiveOfEratosthenes {
+package MATHEMATICALS;
+
+public class SeiveOfEratosthenes {
+    public static void main(String[] args) {
+        int  n = 40;
+        boolean[] arr = new  boolean[n + 1];
+
+    }
+    static  void seive(int n, boolean arr){
+
+        
+
+    }
 }
