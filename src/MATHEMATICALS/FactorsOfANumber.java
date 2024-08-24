60c76bd70a1c7f26f0a9497cc5a149d2b8c709e2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-25 05:04:10 +0530

    Modified on 25-08-2024

--- src/MATHEMATICALS/FactorsOfANumber.java
+++ src/MATHEMATICALS/FactorsOfANumber.java
@@ -1,2 +1,13 @@
-package MATHEMATICALS;public class FactorsOfANumber {
+package MATHEMATICALS;
+
+public class FactorsOfANumber {
+    public static void main(String[] args) {
+        factors(40);
+    }
+    static  void factors(int n){
+        for (int i = 1; i <= n ; i++) {
+            if( n % i == 0)
+                System.out.print( i + " ");
+        }
+    }
 }
