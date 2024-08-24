28e8236d2b73672c0b83f188eaaef8c6ed9aef88
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-25 04:36:45 +0530

    modified

--- src/MATHEMATICALS/FindSqareRootOfANumber.java
+++ src/MATHEMATICALS/FindSqareRootOfANumber.java
@@ -1,2 +1,39 @@
-package MATHEMATICALS;public class FindSqareRootOfANumber {
+package MATHEMATICALS;
+
+public class FindSqareRootOfANumber {
+    public static void main(String[] args) {
+        int n = 40;
+        int p = 3;
+        System.out.printf("%.3f" , squareRoot(n,p));
+    }
+    static  double squareRoot(int n, int p){
+        int start = 0;
+        int end = n;
+
+        double root = 0.0;
+        while (start <= end){
+            int mid = start + (end - start)/2;
+
+            if(mid * mid == n)
+                return mid;
+
+            if (mid * mid > n)
+                end = mid - 1;
+            else
+                start = mid + 1;
+
+        }
+
+        // to find precision of the value;
+        double icr = 0.1;
+        for (int i = 0; i <p ; i++) {
+            while(root * root < n)
+                root += icr;
+
+            root -= icr;
+            icr /= 10;
+        }
+        return  root;
+
+    }
 }
