a22e4e776fef295dcf4670551d28df7061cf0de5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-25 05:13:42 +0530

    Modified on 25-08-2024

--- src/MATHEMATICALS/factors2.java
+++ src/MATHEMATICALS/factors2.java
@@ -1,2 +1,17 @@
-package MATHEMATICALS;public class factors2 {
+package MATHEMATICALS;
+
+public class factors2 {
+    public static void main(String[] args) {
+        System.out.println(factors(n));
+    }
+    static  void factors(int n){
+        for (int i = 1; i * i <= n  ; i++) {
+            if( n % i == 0){
+                if(n / i == i)
+                    System.out.print( i + " ");
+                else
+                    System.out.print( i + " " + n/i);
+            }
+        }
+    }
 }
