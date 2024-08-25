3a52e437872162cb467f9a4a8f981a5e2886041b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 04:51:21 +0530

    modified

--- src/RECURSION/PrintNTo1And1ToN.java
+++ src/RECURSION/PrintNTo1And1ToN.java
@@ -1,2 +1,17 @@
-package RECURSION;public class PrintNTo1And1ToN {
+package RECURSION;
+
+public class PrintNTo1And1ToN {
+    public static void main(String[] args) {
+        int n = 5;
+printBoth(n);
+    }
+static  void printBoth(int n){
+        if(n == 0)
+            return;
+
+    System.out.println(n); // for n to 1
+    printBoth(n - 1);
+    System.out.println(n); // for 1 to n
+
+    }
 }
