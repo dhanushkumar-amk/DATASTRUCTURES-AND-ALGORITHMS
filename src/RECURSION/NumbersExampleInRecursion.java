706773147ebf9217541ea3e65d1520be30ff7558
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-25 15:05:14 +0530

    modified

--- src/RECURSION/NumbersExampleInRecursion.java
+++ src/RECURSION/NumbersExampleInRecursion.java
@@ -1,2 +1,19 @@
-package RECURSION;public class NumbersExampleInRecursion {
+package RECURSION;
+
+public class NumbersExampleInRecursion {
+    public static void main(String[] args) {
+        int n = 5;
+        numbers(n);
+    }
+    static  void  numbers(int n){
+
+        // base case
+        if( n == 5) {
+            System.out.println(5);
+            return;
+        }
+
+        System.out.println(n);
+        numbers(n + 1);
+    }
 }
