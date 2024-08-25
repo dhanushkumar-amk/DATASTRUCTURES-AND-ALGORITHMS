e76e1565eaf0720af9f6de3dea47edc2e039fde2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 04:37:04 +0530

    Modified on 26-08-2024

--- src/RECURSION/PrintNTo1.java
+++ src/RECURSION/PrintNTo1.java
@@ -1,2 +1,15 @@
-package RECURSION;public class PrintNTo1 {
+package RECURSION;
+
+public class PrintNTo1 {
+    public static void main(String[] args) {
+        int n = 5;
+        print(n);
+    }
+    static  void  print(int n){
+        if(n == 0)
+            return;
+
+        System.out.println(n);
+        print(n - 1);
+    }
 }
