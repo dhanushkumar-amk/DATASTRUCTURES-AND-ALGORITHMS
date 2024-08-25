b4a3907cea5dabf3ffbb14c6d9c38af9e5c11ba2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-25 10:08:44 +0530

    modified

--- src/MATHEMATICALS/EuclideanAlgorithms.java
+++ src/MATHEMATICALS/EuclideanAlgorithms.java
@@ -1,2 +1,15 @@
-package MATHEMATICALS;public class EuclideanAlgorithms {
+package MATHEMATICALS;
+
+public class EuclideanAlgorithms {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        System.out.println(gcd(a,b));
+    }
+    static  int gcd(int a, int b){
+        if(a==0)
+            return b;
+
+        return gcd(b % a, a);
+    }
 }
