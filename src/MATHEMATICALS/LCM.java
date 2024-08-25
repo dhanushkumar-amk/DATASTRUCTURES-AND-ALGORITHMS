5ae366938d0a58f9f835f2ecca7257ab188cb1c4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-25 10:18:59 +0530

    modified

--- src/MATHEMATICALS/LCM.java
+++ src/MATHEMATICALS/LCM.java
@@ -1,2 +1,19 @@
-package MATHEMATICALS;public class LCM {
+package MATHEMATICALS;
+
+public class LCM {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 18;
+        System.out.println(lcm(a,b));
+    }
+    static int gcd(int a, int b){
+        if(a == 0)
+            return b;
+
+        return gcd(b % a,a);
+
+    }
+    static  int lcm(int a, int b){
+        return  a * b / gcd(a,b);
+    }
 }
