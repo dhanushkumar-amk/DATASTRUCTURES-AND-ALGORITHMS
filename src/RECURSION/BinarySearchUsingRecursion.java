9d07ec8e919371201024639458942a507d284ece
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 04:20:05 +0530

    Modified on 26-08-2024

--- src/RECURSION/BinarySearchUsingRecursion.java
+++ src/RECURSION/BinarySearchUsingRecursion.java
@@ -1,2 +1,25 @@
-package RECURSION;public class BinarySearchUsingRecursion {
+package RECURSION;
+
+public class BinarySearchUsingRecursion {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        int target = 30;
+        System.out.println(binarySearch(arr, target, 0, 4));
+    }
+    static  int  binarySearch(int[] arr, int target, int start, int end){
+
+        // base Condition
+        if(start > end)
+            return -1;
+
+        int mid = start + (end - start)/2;
+
+        if(arr[mid] == target)
+            return  mid;
+
+        if(target > arr[mid])
+            return binarySearch(arr, target, mid + 1, end);
+        else
+            return  binarySearch(arr, target, start, mid -1);
+    }
 }
