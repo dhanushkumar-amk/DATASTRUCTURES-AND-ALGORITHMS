2d3e8e1a8d02c5e2875807317fd82035ab167def
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 16:03:17 +0530

    modified

--- src/RECURSION/CountNoOfSteps.java
+++ src/RECURSION/CountNoOfSteps.java
@@ -5,8 +5,21 @@ package RECURSION;
 public class CountNoOfSteps {
     public static void main(String[] args) {
         int num = 14;
+        System.out.println(numberOfSteps(num));
     }
-    public int numberOfSteps(int num){
+    public  static int numberOfSteps(int num){
         return  helper(num, 0);
+    }
+
+    private  static int helper(int num, int steps) {
+
+        // base case
+        if( num == 0)
+            return  steps;
+
+        if(num % 2 == 0)
+            return  helper(num / 2, steps + 1);
+
+        return  helper(num - 1, steps+1);
     }
 }
