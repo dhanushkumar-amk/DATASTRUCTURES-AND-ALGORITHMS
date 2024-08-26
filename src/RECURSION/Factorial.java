13272c5d47a9e26acd679f0b9f12f66d16ff0323
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 10:28:39 +0530

    modified

--- src/RECURSION/Factorial.java
+++ src/RECURSION/Factorial.java
@@ -2,13 +2,13 @@ package RECURSION;
 
 public class Factorial {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 0;
         System.out.println(fact(n));
     }
     static  int fact(int n){
         if( n == 1 ||  n == 0)
             return 1;
 
-        return n + fact(n - 1);
+        return n * fact(n - 1);
     }
 }
