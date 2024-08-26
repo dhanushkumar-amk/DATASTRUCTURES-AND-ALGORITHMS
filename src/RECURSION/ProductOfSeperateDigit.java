28b6e7952301c4e07a862a2be16aee699514fe3b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 10:47:50 +0530

    modified

--- src/RECURSION/ProductOfSeperateDigit.java
+++ src/RECURSION/ProductOfSeperateDigit.java
@@ -6,9 +6,11 @@ public class ProductOfSeperateDigit {
         System.out.println(product(n));
     }
     static  int product(int n){
+        // if there is an n is a last number just return that number
         if( n % 10 == n)
             return n;
 
+        // recursive case
         return product(n / 10) * (n % 10);
     }
 }
