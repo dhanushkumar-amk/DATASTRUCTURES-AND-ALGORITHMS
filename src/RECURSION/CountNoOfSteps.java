483408df0993c0f1e9196f654bcb5c89ed695556
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 15:59:23 +0530

    modified

--- src/RECURSION/CountNoOfSteps.java
+++ src/RECURSION/CountNoOfSteps.java
@@ -1,2 +1,9 @@
-package RECURSION;public class CountNoOfSteps {
+package RECURSION;
+
+// leetcode 1342
+
+public class CountNoOfSteps {
+    public static void main(String[] args) {
+        int num = 14;
+    }
 }
