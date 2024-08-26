979b0146ca08e404be0a1489b25d8bf79690abb7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 10:40:00 +0530

    Modified on 26-08-2024

--- src/RECURSION/SumOfISeprateDigits.java
+++ src/RECURSION/SumOfISeprateDigits.java
@@ -1,2 +1,15 @@
-package RECURSION;public class SumOfISeprateDigits {
+package RECURSION;
+
+public class SumOfISeprateDigits {
+    public static void main(String[] args) {
+        int n = 1342;
+        System.out.println(sum(n));
+    }
+    static  int sum(int n){
+        // base case
+        if( n == 0)
+            return 0;
+
+        return sum(n/10) + (n%10);
+    }
 }
