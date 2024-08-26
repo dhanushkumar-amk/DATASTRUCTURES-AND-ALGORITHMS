bc5e8cd87113eeaac29ef7ad82c53bdb290004b5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 10:42:40 +0530

    modified

--- src/RECURSION/ProductOfSeperateDigit.java
+++ src/RECURSION/ProductOfSeperateDigit.java
@@ -3,6 +3,7 @@ package RECURSION;
 public class ProductOfSeperateDigit {
     public static void main(String[] args) {
         int n = 1111;
+        System.out.println(product(n));
     }
     static  int product(int n){
         if( n == 0)
