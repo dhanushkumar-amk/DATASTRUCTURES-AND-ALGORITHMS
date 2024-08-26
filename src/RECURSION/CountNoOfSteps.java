cc56fc894157631d0cd3dc5a9bfcbd79ff007eca
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 16:01:05 +0530

    modified

--- src/RECURSION/CountNoOfSteps.java
+++ src/RECURSION/CountNoOfSteps.java
@@ -6,4 +6,7 @@ public class CountNoOfSteps {
     public static void main(String[] args) {
         int num = 14;
     }
+    public int numberOfSteps(int num){
+        return  helper(num, 0);
+    }
 }
