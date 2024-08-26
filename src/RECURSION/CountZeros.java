37be3d41d807134d490868bed3438cb2db53aee0
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 15:42:43 +0530

    modified

--- src/RECURSION/CountZeros.java
+++ src/RECURSION/CountZeros.java
@@ -2,6 +2,23 @@ package RECURSION;
 
 public class CountZeros {
     public static void main(String[] args) {
-      int n =   
+      int n = 100000;
+        System.out.println(count(n));
+    }
+    static  int count(int n){
+        return  helper(n,0);
+    }
+
+    private static int helper(int n, int c) {
+
+        if(n == 0)
+            return  c;
+
+        int rem = n % 10;
+
+        if( rem == 0)
+            return helper(n/10, c+1);
+        else
+            return helper(n/10, c);
     }
 }
