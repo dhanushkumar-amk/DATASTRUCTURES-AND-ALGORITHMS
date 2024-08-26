af891d344a204d6fadf6653dc0f57c14a635e0df
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-26 10:31:07 +0530

    modified

--- src/RECURSION/SumOfNNumbers.java
+++ src/RECURSION/SumOfNNumbers.java
@@ -2,12 +2,12 @@ package RECURSION;
 
 public class SumOfNNumbers {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 1;
         System.out.println(sum(n));
     }
     static  int sum(int n){
-        if( n == 1)
-            return 1;
+        if( n == 0)
+            return 0;
 
         return n + sum(n - 1);
     }
