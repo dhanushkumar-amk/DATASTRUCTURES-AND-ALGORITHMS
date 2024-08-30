3bc9bcc86d68c1b9dd45aea584bbb6bfdf5652a8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-30 09:31:22 +0530

    modified

--- src/GRAPH/BFS.java
+++ src/GRAPH/BFS.java
@@ -1,10 +1,61 @@
 package GRAPH;
 
+import java.util.*;
+
 public class BFS {
 
 
+    public ArrayList<Integer> bfsOfGraph(int V,
+                                         ArrayList<ArrayList<Integer>> adj) {
 
-    
+        ArrayList < Integer > bfs = new ArrayList < > ();
+        boolean vis[] = new boolean[V];
+        Queue < Integer > q = new LinkedList < > ();
+
+        q.add(0);
+        vis[0] = true;
+
+        while (!q.isEmpty()) {
+            Integer node = q.poll();
+            bfs.add(node);
+
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+            for (Integer it: adj.get(node)) {
+                if (vis[it] == false) {
+                    vis[it] = true;
+                    q.add(it);
+                }
+            }
+        }
+
+        return bfs;
+    }
+
+    public static void main(String args[]) {
+
+        ArrayList < ArrayList < Integer >> adj = new ArrayList < > ();
+        for (int i = 0; i < 5; i++) {
+            adj.add(new ArrayList < > ());
+        }
+        adj.get(0).add(1);
+        adj.get(1).add(0);
+        adj.get(0).add(4);
+        adj.get(4).add(0);
+        adj.get(1).add(2);
+        adj.get(2).add(1);
+        adj.get(1).add(3);
+        adj.get(3).add(1);
+
+        Solution sl = new Solution();
+        ArrayList < Integer > ans = sl.bfsOfGraph(5, adj);
+        int n = ans.size();
+        for(int i = 0;i<n;i++) {
+            System.out.print(ans.get(i)+" ");
+        }
+    }
+
 
 
 }
