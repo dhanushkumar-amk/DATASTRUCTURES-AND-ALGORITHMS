6a9b45d6e67469004fc876326dc2f16b7eed97d2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-30 18:28:16 +0530

    Modified on 30-08-2024

--- src/RECURSION/ReturnAnarrayList.java
+++ src/RECURSION/ReturnAnarrayList.java
@@ -1,8 +1,22 @@
 package RECURSION;
 
+import java.util.ArrayList;
+
 public class ReturnAnarrayList {
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,56,56,7};
+        int target = 56;
 
+        System.out.println(findAllIndex(arr, target, 0, new ArrayList<>()));
     }
-    
+  static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list){
+        if(index == arr.length)
+            return list;
+
+        if(arr[index] == target )
+            list.add(index);
+
+        return  findAllIndex(arr, target, index + 1 , list);
+  }
+
 }
