6ec0f22945136146e8d4981438205220d1d704fd
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-30 19:20:32 +0530

    modified

--- src/RECURSION/StarPatternusingRecursion.java
+++ src/RECURSION/StarPatternusingRecursion.java
@@ -1,2 +1,20 @@
-package RECURSION;public class StarPatternusingRecursion {
+package RECURSION;
+
+public class StarPatternusingRecursion {
+    public static void main(String[] args) {
+        pattern(4, 0);
+    }
+    static  void pattern(int row, int column){
+        if(row == 0)
+            return;
+
+        if( column < row){
+            System.out.print( " * ");
+            pattern(row, column + 1);
+        }
+        else {
+            System.out.println();
+            pattern(row - 1, 0);
+        }
+    }
 }
