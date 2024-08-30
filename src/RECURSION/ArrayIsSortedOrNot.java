25fb0e45551544aeae104ee01b09456e2eba49bd
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-30 17:31:47 +0530

    Modified on 30-08-2024

--- src/RECURSION/ArrayIsSortedOrNot.java
+++ src/RECURSION/ArrayIsSortedOrNot.java
@@ -1,2 +1,18 @@
-package RECURSION;public class ArrayIsSortedOrNot {
+package RECURSION;
+
+public class ArrayIsSortedOrNot {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,5};
+        System.out.println(ArrayIsSorted(arr, 0));
+    }
+    static  boolean ArrayIsSorted(int[] arr, int index){
+
+        // base case
+        if( index == arr.length -1 )
+            return  true;
+
+        // recursive case
+        return arr[index] < arr[index + 1] && ArrayIsSorted(arr, index + 1);
+
+    }
 }
