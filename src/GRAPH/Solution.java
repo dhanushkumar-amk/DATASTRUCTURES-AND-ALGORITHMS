5c81e9e5703452715bbe3fae787a4a04517d0808
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-30 09:33:34 +0530

    modified

--- src/GRAPH/Solution.java
+++ src/GRAPH/Solution.java
@@ -1,4 +1,36 @@
 package GRAPH;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
+    public ArrayList<Integer> bfsOfGraph(int V,
+                                         ArrayList<ArrayList<Integer>> adj) {
+
+        ArrayList < Integer > bfs = new ArrayList < > ();
+        boolean vis[] = new boolean[V];
+        Queue< Integer > q = new LinkedList< >();
+
+        q.add(0);
+        vis[0] = true;
+
+        while (!q.isEmpty()) {
+            Integer node = q.poll();
+            bfs.add(node);
+
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+            for (Integer it: adj.get(node)) {
+                if (vis[it] == false) {
+                    vis[it] = true;
+                    q.add(it);
+                }
+            }
+        }
+
+        return bfs;
+    }
+
 }
