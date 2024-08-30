76e15c98bc321fa7b1fe19f8f5e3b169993465b2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-30 17:56:32 +0530

    Modified on 30-08-2024

--- src/RECURSION/LinearSearchWithMultipleOccurance.java
+++ src/RECURSION/LinearSearchWithMultipleOccurance.java
@@ -1,2 +1,28 @@
-package RECURSION;public class LinearSearchWithMultipleOccurance {
+package RECURSION;
+
+import java.util.ArrayList;
+
+public class LinearSearchWithMultipleOccurance {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,56,56,7};
+        int target = 56;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 0; i <ans.size()-1 ;i++) {
+            System.out.print(ans.get(i));
+        }
+//        System.out.println(findAllOccurrance(arr, target, 0));
+    }
+    static  ArrayList<Integer> list = new ArrayList<>();
+    static  void findAllOccurrance(int[] arr, int target, int index){
+
+        // base case
+        if( index == arr.length)
+            return ;
+
+        if(arr[index] == target)
+            list.add(index);
+
+        findAllOccurrance(arr, target, index + 1);
+    }
+
 }
