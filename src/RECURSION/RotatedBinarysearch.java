5ea98eab8b4758e28fe42df6eeadd24367b04cc0
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-30 18:49:45 +0530

    Modified on 30-08-2024

--- src/RECURSION/RotatedBinarysearch.java
+++ src/RECURSION/RotatedBinarysearch.java
@@ -2,8 +2,32 @@ package RECURSION;
 
 public class RotatedBinarysearch {
     public static void main(String[] args) {
+int[] arr = {5,6,7,8,1,2,3,4};
+int target = 7;
 
     }
 
-    
+    static  int bSearch(int[] arr, int target,int start, int end){
+        if( start > end )
+            return  -1;
+
+        int mid = start + (end - start) / 2;
+
+        if(arr[mid] == target)
+            return mid;
+
+        // case 1
+        if(arr[start] <= arr[mid]){
+            if(target >= arr[start] & target <= arr[mid])
+               return bSearch(arr, target, start, mid -1);
+            else
+                 return  bSearch(arr, target, mid + 1, end);
+        }
+        else {
+            if (target >= arr[mid] && target <= arr[end])
+                return bSearch(arr, target, mid + 1, end);
+            else
+                return bSearch(arr, target, start, mid -1 );
+        }
+    }
 }
