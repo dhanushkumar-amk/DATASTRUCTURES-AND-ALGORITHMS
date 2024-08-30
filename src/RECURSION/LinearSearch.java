8fa83cfe78d3d7cc94c79a7696b205c1e61db668
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-30 17:48:14 +0530

    Modified on 30-08-2024

--- src/RECURSION/LinearSearch.java
+++ src/RECURSION/LinearSearch.java
@@ -5,7 +5,7 @@ public class LinearSearch {
         int[] arr = {1,3,6,3,45,8};
         int target = 45;
         System.out.println(linerarSearch(arr, target, 0));
-        System.out.println(findIndex(arr, target, 0));
+//        System.out.println(findIndex(arr, target, 0));
     }
     static boolean linerarSearch(int[] arr, int target, int index){
 
@@ -16,14 +16,5 @@ public class LinearSearch {
         // recursive case
         return  arr[index] == target || linerarSearch(arr,target, index + 1);
     }
-    static  int findIndex(int[] arr, int target, int index){
-        if(index == arr.length)
-            return  -1;
 
-        if(arr[index] == target)
-            return  index;
-        else
-            return findIndex(arr, target, index + 1);
-    }
 }
-`````````````````````
--- src/RECURSION/LinerSearchIndex.java
+++ src/RECURSION/LinerSearchIndex.java
@@ -1,2 +1,18 @@
-package RECURSION;public class LinerSearchIndex {
+package RECURSION;
+
+public class LinerSearchIndex {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,56,7};
+        int target = 56;
+        System.out.println(findIndex(arr, target, 0));
+    }
+    static  int findIndex(int[] arr, int target, int index){
+        if(index == arr.length)
+            return  -1;
+
+        if(arr[index] == target)
+            return  index;
+        else
+            return findIndex(arr, target, index + 1);
+    }
 }
