721ca34316f3bb225876de514ba383584df7433d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-08-30 17:36:47 +0530

    modified

--- src/RECURSION/LinearSearch.java
+++ src/RECURSION/LinearSearch.java
@@ -1,2 +1,15 @@
-package RECURSION;public class LinearSearch {
+package RECURSION;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1,3,6,3,45,8};
+        int target = 45;
+        System.out.println(linerarSearch(arr, target, 0));
+    }
+    static boolean linerarSearch(int[] arr, int target, int index){
+        if( index == arr.length)
+            return false;
+
+        return  arr[index] == target || linerarSearch(arr,target, index = 1);
+    }
 }
