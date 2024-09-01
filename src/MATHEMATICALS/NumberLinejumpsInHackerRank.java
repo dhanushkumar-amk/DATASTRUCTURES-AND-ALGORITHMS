e5c02b65f5b9b7625bc5395f477395f867879f5a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-01 10:37:10 +0530

    Modified on 01-09-2024

--- src/MATHEMATICALS/NumberLinejumpsInHackerRank.java
+++ src/MATHEMATICALS/NumberLinejumpsInHackerRank.java
@@ -2,9 +2,28 @@ package MATHEMATICALS;
 
 public class NumberLinejumpsInHackerRank {
     public static void main(String[] args) {
-
+        int kangaroo1 = 2;
+        int distance1 = 1;
+        int kangaroo2 = 5;
+        int distance2 = 3;
+        Kangaroo(kangaroo1, kangaroo2, distance1, distance2);
     }
-    static  String Kangaroo(int x1, int v1, int x2, int v2){
+    public static String Kangaroo(int kangaroo1, int distance1, int kangaroo2, int distance2){
         
+       // base condition
+        if (distance1 < distance2)
+            return "No";
+
+        int newKangaroo1Position = kangaroo1;
+        int newKangaroo2Position = kangaroo2;
+
+        while(newKangaroo1Position < newKangaroo2Position) {
+            newKangaroo1Position += distance1;
+            newKangaroo2Position += distance2;
+
+            if (newKangaroo1Position == newKangaroo2Position)
+                return "yes";
+        }
+        return  "no";
     }
 }
