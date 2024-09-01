df11453f64ebd954b55435b707281903d6517384
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-01 11:51:24 +0530

    Modified on 01-09-2024

--- src/ARRAYS/CompareTripletsInHackerRank.java
+++ src/ARRAYS/CompareTripletsInHackerRank.java
@@ -1,2 +1,27 @@
-package ARRAYS;public class CompareTripletsInHackerRank {
+package ARRAYS;
+
+//import java.util.ArrayList;
+
+import java.util.Arrays;
+
+public class CompareTripletsInHackerRank {
+    public static void main(String[] args) {
+        int[] alice = {5,6,7};
+        int[] bob = {3,6,10};
+        System.out.println(Arrays.toString(Triplets(alice, bob)));
+    }
+    static int[] Triplets(int[] alice, int[] bob){
+
+        int[] result = new int[2];
+        int Alice = 0;
+        int Bob = 0;
+
+        for (int i = 0; i < 3 ; i++) {
+            if (alice[i] > bob[i])
+                result[Alice]++;
+            else if (alice[i] < bob[i])
+                result[Bob]++;
+        }
+    return result;
+    }
 }
