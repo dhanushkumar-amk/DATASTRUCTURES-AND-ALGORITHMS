91a6d77a8eeeceb75e42265e957fe7af3e30f8dd
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-01 12:46:22 +0530

    modified

--- src/MATHEMATICALS/FindDigitInHakerRank.java
+++ src/MATHEMATICALS/FindDigitInHakerRank.java
@@ -3,12 +3,23 @@ package MATHEMATICALS;
 public class FindDigitInHakerRank {
     public static void main(String[] args) {
         int n = 124;
+        System.out.println(digit(n));
     }
 
     static  int digit(int n){
-        int count = 0;
 
-        
+        int temp = n;
+        int count = 0;
+        int lastDigit = 0;
+
+        while (temp > 0){
+            lastDigit = temp % 10;
+
+            if (n % lastDigit == 0)
+                count++;
+
+            temp = temp / 10;
+        }
         return count;
     }
 }
