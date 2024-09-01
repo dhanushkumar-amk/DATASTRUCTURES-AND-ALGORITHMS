6e8feb76fb1bdb53453b106597d2d748887e3650
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-01 10:07:04 +0530

    modified

--- src/MATHEMATICALS/NumberLinejumpsInHackerRank.java
+++ src/MATHEMATICALS/NumberLinejumpsInHackerRank.java
@@ -4,5 +4,7 @@ public class NumberLinejumpsInHackerRank {
     public static void main(String[] args) {
 
     }
-    
+    static  String Kangaroo(int x1, int v1, int x2, int v2){
+        
+    }
 }
