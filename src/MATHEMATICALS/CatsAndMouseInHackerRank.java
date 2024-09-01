c184607e5b999fda2267a68fdf92bdbe3703b6f7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-01 15:50:52 +0530

    modified

--- src/MATHEMATICALS/CatsAndMouseInHackerRank.java
+++ src/MATHEMATICALS/CatsAndMouseInHackerRank.java
@@ -1,2 +1,36 @@
-package MATHEMATICALS;public class CatsAndMouseInHackerRank {
+package MATHEMATICALS;
+
+public class CatsAndMouseInHackerRank {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 4;
+        int z = 6;
+        System.out.println(catsAndMouse(x,y,z));
+    }
+    static String catsAndMouse(int x, int y, int z){
+        int distance1 = 0; // distance between x and y
+        int distance2 = 0; // distance between y and z
+
+
+        // it is alternative for abs build in function
+
+        if (x <= z)
+            distance1 = z - x;
+        else
+            distance1 = x - z;
+
+
+        if (y <= z)
+            distance2 = z - y;
+        else
+            distance2 = y - z;
+
+
+        if (distance1 == distance2)
+            return "Mouse c";
+        else if(distance1 > distance2)
+            return "Cat A";
+        else
+            return "Cat B";
+    }
 }
