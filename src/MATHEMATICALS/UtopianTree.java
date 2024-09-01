1eafb1833e8f12811f7baa9514906a0e63f59180
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-01 16:24:04 +0530

    modified

--- src/MATHEMATICALS/UtopianTree.java
+++ src/MATHEMATICALS/UtopianTree.java
@@ -1,2 +1,23 @@
-package MATHEMATICALS;public class UtopianTree {
+package MATHEMATICALS;
+
+public class UtopianTree
+{
+    public static void main(String[] args) {
+        int n = 5;
+        int Height = 1;
+        System.out.println(treeHeight(n,Height));
+    }
+
+    static  int treeHeight(int n, int Height){
+
+        for (int i = 1; i <=n ; i++) {
+
+            if(i % 2 == 0)
+                Height *= 2;
+            else
+                Height += 1;
+        }
+        return Height;
+    }
+
 }
