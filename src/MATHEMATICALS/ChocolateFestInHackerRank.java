9a6010331fc7846078ece93946e728600dc4732e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-01 09:49:49 +0530

    modified

--- src/MATHEMATICALS/ChocolateFestInHackerRank.java
+++ src/MATHEMATICALS/ChocolateFestInHackerRank.java
@@ -2,6 +2,23 @@ package MATHEMATICALS;
 
 public class ChocolateFestInHackerRank {
     public static void main(String[] args) {
-        
+        int n = 15;
+        int c = 3;
+        int m = 2;
+        System.out.println(chocalateFeast(n,c,m));
+    }
+    static  int chocalateFeast(int n, int c, int m){
+
+        int choco = n/c;
+        int wrap = choco;
+
+        int new_Choco = 0;
+
+        while(wrap >= m){
+            new_Choco = wrap / m;
+            choco += new_Choco;
+            wrap %= m + new_Choco;
+        }
+        return  choco;
     }
 }
