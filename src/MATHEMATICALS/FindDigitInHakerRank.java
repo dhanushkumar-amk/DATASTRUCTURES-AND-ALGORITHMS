3c2a90916a89a673aeeadb6121a67f87fd135a00
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-01 12:43:57 +0530

    Modified on 01-09-2024

--- src/MATHEMATICALS/FindDigitInHakerRank.java
+++ src/MATHEMATICALS/FindDigitInHakerRank.java
@@ -1,2 +1,14 @@
-package MATHEMATICALS;public class FindDigitInHakerRank {
+package MATHEMATICALS;
+
+public class FindDigitInHakerRank {
+    public static void main(String[] args) {
+        int n = 124;
+    }
+
+    static  int digit(int n){
+        int count = 0;
+
+        
+        return count;
+    }
 }
