a6998aa839f7baeed1a36f0144957b6534f1faac
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-01 13:27:24 +0530

    modified

--- src/MATHEMATICALS/ViralAdvertisingInHackerRank.java
+++ src/MATHEMATICALS/ViralAdvertisingInHackerRank.java
@@ -1,2 +1,23 @@
-package MATHEMATICALS;public class ViralAdvertisingInHackerRank {
+package MATHEMATICALS;
+
+public class ViralAdvertisingInHackerRank {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(advertising(n));
+    }
+    static  int advertising(int n){
+
+        int shared = 5;
+        int liked = 0;
+        int totalLiked = 0;
+
+        for (int i = 1; i <=n ; i++) {
+
+            liked = shared / 2;
+            totalLiked += liked;
+            shared = liked * 3;
+
+        }
+        return  totalLiked;
+    }
 }
