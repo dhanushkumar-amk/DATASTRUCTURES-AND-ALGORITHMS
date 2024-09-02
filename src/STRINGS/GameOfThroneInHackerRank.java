88ed1287868a427f8a7fff716e9ab42565834c69
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-02 10:46:51 +0530

    modified

--- src/STRINGS/GameOfThroneInHackerRank.java
+++ src/STRINGS/GameOfThroneInHackerRank.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class GameOfThroneInHackerRank {
     public static void main(String[] args) {
-    String s = "adamm";
+    String s = "adamma";
         System.out.println(got(s));
     }
     static String got(String s){
