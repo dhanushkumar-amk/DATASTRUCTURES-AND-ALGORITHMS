e9cc335f8a3799b8844e75f312ecbd64167cc676
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-02 12:01:53 +0530

    modified

--- src/ARRAYS/IceCreamParlourProblemInHackerRank.java
+++ src/ARRAYS/IceCreamParlourProblemInHackerRank.java
@@ -1,2 +1,26 @@
-package ARRAYS;public class IceCreamParlourProblemInHackerRank {
+package ARRAYS;
+
+import java.util.ArrayList;
+
+public class IceCreamParlourProblemInHackerRank {
+    public static void main(String[] args) {
+        int m = 6;
+        int[] arr = {1,3,4,5,6};
+        ArrayList<Integer> ans = iceCream(m, arr);
+        System.out.println(ans);
+    }
+   static ArrayList<Integer> iceCream(int m, int[] arr){
+
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i <arr.length-1 ; i++) {
+           for (int j = i + 1; j <arr.length; j++) {
+               if (arr[i] + arr[j] == m) {
+                   res.add(i + 1);
+                   res.add(j + 1);
+               }
+               break;
+           }
+       }
+        return res;
+   }
 }
