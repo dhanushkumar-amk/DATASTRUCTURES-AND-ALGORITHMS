dc092d4035324e3f617c6c566e8149074237fbae
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-02 09:38:40 +0530

    modified

--- src/STRINGS/LoveLetterMystreInhackerRank.java
+++ src/STRINGS/LoveLetterMystreInhackerRank.java
@@ -1,2 +1,34 @@
-package STRINGS;public class LoveLetterMystreInhackerRank {
+package STRINGS;
+
+public class LoveLetterMystreInhackerRank {
+    public static void main(String[] args) {
+        String
+    }
+    static  int loveLetter(String s){
+        int count = 0;
+        int char1 = ' ';
+        int char2 = ' ';
+        int temp = 0;
+
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i < j){
+            char1 = s.charAt(i);
+            char2 = s.charAt(j);
+
+            if(char1 > char2)
+                temp = char1 - char2;
+            else
+                temp = char2 - char1;
+
+            // store whatever the value int temp is store in the count
+            count += temp;
+
+            i++;
+            j--;
+        }
+
+        return  count;
+    }
 }
