90c8175af9b3de6a077e0be951c0abb8f88d63fa
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-03 05:25:32 +0530

    Modified on 03-09-2024

--- src/JAVA_LEARNING/OperatorPrecedencyExample1.java
+++ src/JAVA_LEARNING/OperatorPrecedencyExample1.java
@@ -1,2 +1,11 @@
-package JAVA_LEARNING;public class OperatorPrecedencyExample1 {
+package JAVA_LEARNING;
+
+public class OperatorPrecedencyExample1 {
+
+    public static void main(String[] args) {
+        int m = 10;
+        int n = 10;
+        int res = m++ / (++n * n--) / --m;
+        System.out.println(res);
+    }
 }
