0dce6c14adcaa1311ccc11917fd6d634bf2d068b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-02 10:44:02 +0530

    modified

--- src/STRINGS/GameOfThroneInHackerRank.java
+++ src/STRINGS/GameOfThroneInHackerRank.java
@@ -1,9 +1,34 @@
 package STRINGS;
 
+import java.util.Arrays;
+
 public class GameOfThroneInHackerRank {
     public static void main(String[] args) {
     String s = "adamm";
+        System.out.println(got(s));
     }
     static String got(String s){
-    D    }
+        // to convert an string into char array
+        char[] arr = s.toCharArray();
+
+        // Arrays.sort is an static method we ca call by className
+        Arrays.sort(arr);
+
+        int i= 0;
+        int nonPairCount = 0;
+        while(i <arr.length){
+            if (arr[i] == arr[i+1])
+                i = i + 2;
+            else {
+                i++;
+                nonPairCount++;
+            }
+        }
+
+        if (nonPairCount <= 1)
+            return  "Yes";
+        else
+            return "No";
+
+    }
 }
