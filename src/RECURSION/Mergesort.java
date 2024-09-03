0939f1094fed71af64e4b734fdcf2eec235984a6
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-04 04:39:37 +0530

    Modified on 04-09-2024

--- src/RECURSION/Mergesort.java
+++ src/RECURSION/Mergesort.java
@@ -1,2 +1,63 @@
-package RECURSION;public class Mergesort {
+package RECURSION;
+
+import java.util.Arrays;
+
+public class Mergesort {
+
+    public static void main(String[] args) {
+        int[] arr  = {3,2,6,5,8,7};
+        int[] ans = merge(arr);
+        System.out.println(ans);
+    }
+    static  int[] merge(int[] arr){
+
+        // base case
+        if(arr.length == 1)
+            return  arr;
+
+        int mid = arr.length/ 2;
+
+        int[] left = merge(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = merge(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return  mergeMix(left, right);
+    }
+
+    private static int[] mergeMix(int[] left, int[] right) {
+
+        int[] mix = new int[left.length + right.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < left.length && j < right.length){
+            if (left[i] < right[j]){
+                mix[k] = left[i];
+                i++;
+            }
+            else {
+                mix[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+
+        // it may possible if one of an array remains the element
+
+        while(i < left.length){
+            mix[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while(j < right.length){
+            mix[k] = left[j];
+            j++;
+            k++;
+        }
+
+        return  mix;
+    }
 }
