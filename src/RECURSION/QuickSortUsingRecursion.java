64501fd6ffcf1e6e23b49631afdba7cda07df00b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-04 16:50:05 +0530

    Modified on 04-09-2024

--- src/RECURSION/QuickSortUsingRecursion.java
+++ src/RECURSION/QuickSortUsingRecursion.java
@@ -6,7 +6,8 @@ public class QuickSortUsingRecursion {
     public static void main(String[] args) {
     int[] arr = {5,4,3,2,1};
     sort(arr, 0, arr.length);
-        System.out.println(Arrays.toString(arr));
+
+//        System.out.println(Arrays.toString(arr));
     }
     static  void sort(int[] arr, int low, int high){
         // base case
