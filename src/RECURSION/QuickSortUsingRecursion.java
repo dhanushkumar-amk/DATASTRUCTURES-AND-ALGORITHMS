07619c133ebaa0fca26965743fc13977b4be2593
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-04 16:35:54 +0530

    modified

--- src/RECURSION/QuickSortUsingRecursion.java
+++ src/RECURSION/QuickSortUsingRecursion.java
@@ -1,2 +1,40 @@
-package RECURSION;public class QuickSortUsingRecursion {
+package RECURSION;
+
+public class QuickSortUsingRecursion {
+    public static void main(String[] args) {
+
+    }
+    static  void sort(int[] arr, int low, int high){
+        // base case
+        if(low >= high)
+            return;
+
+        int start = low;
+        int end = high;
+
+        // assuming the pivot point as middle element
+        int mid = start + (end -start)/2;
+        int pivot =arr[mid];
+
+        while (start <= end){
+
+            while (arr[start] < pivot){
+                start++;
+            }
+            while (arr[end] > pivot){
+                end--;
+            }
+
+            if (start <= end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+
+        }
+        sort(arr, low, end);
+        sort(arr,start, hoi);
+    }
 }
