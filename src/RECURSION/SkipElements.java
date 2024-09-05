4c0a4c0be16ed10c1405d2bb1fdb255b6ff979ec
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-05 19:09:10 +0530

    modified

--- src/RECURSION/SkipElements.java
+++ src/RECURSION/SkipElements.java
@@ -1,2 +1,24 @@
-package RECURSION;public class SkipElements {
+package RECURSION;
+
+public class SkipElements {
+    public static void main(String[] args) {
+        String p = " ";
+        String up = "bccdaa";
+        skip(p,up);
+    }
+    static  void skip(String p, String up){
+
+        // base case
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        if(ch == 'a')
+            skip(p, up.substring(1));
+        else
+            skip(p + ch, up.substring(1));
+    }
 }
