0cde52916bbfe2901e45edfee535fca839445db8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-05 19:47:25 +0530

    modified

--- src/RECURSION/SubSequence.java
+++ src/RECURSION/SubSequence.java
@@ -1,2 +1,19 @@
-package RECURSION;public class SubSequence {
+package RECURSION;
+
+public class SubSequence {
+    public static void main(String[] args) {
+        sequence(" ", "abc");
+    }
+    static  void sequence(String p, String up){
+        if(up.isEmpty())
+        {
+            System.out.println(p);
+            return;
+        }
+      char ch = up.charAt(0);
+
+        sequence(p + ch, up.substring(1));
+        sequence(p, up.substring(1));
+
+    }
 }
