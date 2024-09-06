400a9bee7333b4edce04e7a173db7017a12ac473
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-06 20:48:51 +0530

    Modified on 06-09-2024

--- src/RECURSION/Josephusproblem.java
+++ src/RECURSION/Josephusproblem.java
@@ -2,7 +2,14 @@ package RECURSION;
 
 public class Josephusproblem {
     public static void main(String[] args) {
-
+    int n = 7;
+    int k = 3;
+        System.out.println(jos(n, k));
     }
-    
+    static  int jos(int n, int k){
+        if( n == 1)
+            return 0;
+
+        return  (jos(n - 1, k) + k) % n;
+    }
 }
