6f9f1454075570ddee55a48dffd9270283ff3219
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-07 15:20:59 +0530

    Modified on 07-09-2024

--- src/RECURSION/PrintAllPermutationsOfAGivenString.java
+++ src/RECURSION/PrintAllPermutationsOfAGivenString.java
@@ -1,2 +1,31 @@
-package RECURSION;public class PrintAllPermutationsOfAGivenString {
+package RECURSION;
+
+import java.util.Scanner;
+
+public class PrintAllPermutationsOfAGivenString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        permutations(s.toCharArray(), 0);
+    }
+    static  void permutations(char[] arr,int fixedIndex){
+
+        // base case
+        if (fixedIndex == arr.length) {
+            System.out.println(arr);
+            return;
+        }
+
+        for (int i = fixedIndex; i < arr.length ; i++) {
+            swap(arr, i, fixedIndex);
+            permutations(arr,fixedIndex+1);
+            swap(arr, i, fixedIndex);
+        }
+    }
+
+    static void swap(char[] arr, int i, int fixedIndex){
+        char temp = arr[i];
+        arr[i] = arr[fixedIndex];
+        arr[fixedIndex] = temp;
+    }
 }
