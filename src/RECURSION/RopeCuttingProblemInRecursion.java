636fde205b52a03de20241ed3a2247877f674a05
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-07 16:10:45 +0530

    Modified on 07-09-2024

--- src/RECURSION/RopeCuttingProblemInRecursion.java
+++ src/RECURSION/RopeCuttingProblemInRecursion.java
@@ -2,7 +2,31 @@ package RECURSION;
 
 public class RopeCuttingProblemInRecursion {
     public static void main(String[] args) {
-
+        int n = 15;
+        int a = 5;
+        int b = 7;
+        int c = 8;
+        System.out.println(maxPieces(n,a,b,c));
     }
+    static  int maxPieces(int n, int a, int b, int c){
 
+        // base case
+        if(n == 0)
+            return 0;  // for successfully cut
+        else if(n < 0)
+            return -1; // for unsuccessfully cut
+
+
+        // recursive call
+        int temp1 = maxPieces(n-a,  a, b, c);
+        int temp2 = maxPieces(n-b,  a, b, c);
+        int temp3 = maxPieces(n-c,  a, b, c);
+
+      int pieces = Math.max(temp3,Math.max(temp1, temp2));
+
+        if(pieces == -1)
+            return -1;
+        else
+            return pieces+1;
+    }
 }
