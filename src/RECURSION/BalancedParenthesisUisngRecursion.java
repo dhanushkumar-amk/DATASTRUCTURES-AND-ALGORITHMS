3e65d4fb8ebc7887f1fe2c06ccca651811b79c62
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-07 11:39:37 +0530

    modified

--- src/RECURSION/BalancedParenthesisUisngRecursion.java
+++ src/RECURSION/BalancedParenthesisUisngRecursion.java
@@ -1,2 +1,26 @@
-package RECURSION;public class BalancedParenthesisUisngRecursion {
+package RECURSION;
+
+public class BalancedParenthesisUisngRecursion {
+    public static void main(String[] args) {
+        int n = 3;
+        char[] arr = new char[n * 2];
+        paren(arr, n, 0,0,0);
+    }
+
+    static  void paren(char[] arr, int n, int i, int opening, int closing){
+
+        // base case
+        if( i == arr.length)
+            System.out.println(arr);
+
+        if(opening < n){
+            arr[i] = '(';
+            paren(arr,n,i+1,opening+1, closing);
+        }
+
+        if(closing < n){
+            arr[i] = ')';
+            paren(arr,n,i+1, opening, closing+1);
+        }
+    }
 }
