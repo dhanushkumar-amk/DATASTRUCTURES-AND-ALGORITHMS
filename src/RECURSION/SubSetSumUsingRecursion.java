21b83103343cd17b37e155af5b9cd4a7572c1541
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-07 21:38:36 +0530

    modified

--- src/RECURSION/SubSetSumUsingRecursion.java
+++ src/RECURSION/SubSetSumUsingRecursion.java
@@ -1,2 +1,25 @@
-package RECURSION;public class SubSetSumUsingRecursion {
+package RECURSION;
+
+public class SubSetSumUsingRecursion {
+    public static void main(String[] args) {
+        int[] arr = {10,20,15,5};
+        int sum = 25;
+        countSubSet(arr,sum, 0);
+
+    }
+    static  int countSubSet(int[] arr, int sum, int index){
+        // base case
+
+        if(sum == 0)
+            return 1;
+
+        if (sum < 0)
+            return 0;
+
+        if (index == arr.length)
+            return 0;
+
+
+        return countSubSet(arr, sum - arr[index], index + 1) + countSubSet(arr, sum, index + 1);
+    }
 }
