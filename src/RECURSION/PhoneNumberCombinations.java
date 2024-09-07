ea1c83a25cc5f21526e9d57916ddcf375900be43
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-07 12:57:09 +0530

    modified

--- src/RECURSION/PhoneNumberCombinations.java
+++ src/RECURSION/PhoneNumberCombinations.java
@@ -2,6 +2,23 @@ package RECURSION;
 
 public class PhoneNumberCombinations {
     public static void main(String[] args) {
+        String s = "";
+        String ans = " ";
+        combination(s,ans );
+    }
 
+    static String[] keypad = {"","","abc", "def", "ghi", "jkl","mno", "pqrs", "tuv","wxyz"};
+
+    static  void combination(String s, String ans){
+        // base case
+        if(s.length()== 0){
+            System.out.println(ans);
+            return;
+        }
+
+        String key= keypad[s.charAt(0) - 48];
+        for (int i = 0; i <key.length() ; i++) {
+            combination(s.substring(1), ans + s.charAt(i));
+        }
     }
 }
