f9d12c7140b123ea4446c5da2bb35c78dfe36801
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-09 21:16:37 +0530

    Modified on 09-09-2024

--- src/RECURSION/TowerOfHanoi.java
+++ src/RECURSION/TowerOfHanoi.java
@@ -1,2 +1,20 @@
-package RECURSION;public class TowerOfHanoi {
+package RECURSION;
+
+public class TowerOfHanoi {
+    public static void main(String[] args) {
+        tower(3,'A','B','C');
+    }
+    static  void tower(int n, char src, char aux, char dest){
+
+        // base case
+        if( n == 1){
+            System.out.println(src + " -> " + dest);
+            return;
+        }
+
+        // recursive calls
+        tower(n-1, src,dest, aux);
+        tower(1,src, aux, dest);
+        tower(n-1, aux, src, dest);
+    }
 }
