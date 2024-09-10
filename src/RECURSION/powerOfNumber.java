c5459c02a0193386a45b9c4081162b1572114aa7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-10 07:19:25 +0530

    Modified on 10-09-2024

--- src/RECURSION/powerOfNumber.java
+++ src/RECURSION/powerOfNumber.java
@@ -1,2 +1,20 @@
-package RECURSION;public class powerOfNumber {
+package RECURSION;
+
+public class powerOfNumber {
+    public static void main(String[] args) {
+        System.out.println(power(8,3));
+    }
+    static  long power(int x, int y){
+
+        if(y == 0)
+            return 1;
+
+        if(y % 2 == 0){
+            long result = power(x, y/2);
+            return result * result;
+        }
+        else
+           return power(x, y-1)*x;
+
+    }
 }
