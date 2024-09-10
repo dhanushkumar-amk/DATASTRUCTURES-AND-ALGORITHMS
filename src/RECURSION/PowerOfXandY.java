e157670913497f31b01e5c7171320cbe75e48189
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-10 11:24:30 +0530

    Modified on 10-09-2024

--- src/RECURSION/PowerOfXandY.java
+++ src/RECURSION/PowerOfXandY.java
@@ -1,2 +1,21 @@
-package RECURSION;public class PowerOfXandY {
+package RECURSION;
+
+
+// in-efficient  way
+public class PowerOfXandY {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 8;
+        System.out.println(power(x,y));
+    }
+    static  long power(int x, int y){
+        // base case
+        if(y == 0)
+            return 1;
+
+        if(y % 2 == 0)
+            return power(x, y/2) * power(x, y/2);
+        else
+            return power(x, y-1) * x;
+    }
 }
