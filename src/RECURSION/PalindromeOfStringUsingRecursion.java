5649d4c180e95fb6ea3de3f322784da3a9d11e17
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-11 20:39:00 +0530

    modified

--- src/RECURSION/PalindromeOfStringUsingRecursion.java
+++ src/RECURSION/PalindromeOfStringUsingRecursion.java
@@ -2,7 +2,23 @@ package RECURSION;
 
 public class PalindromeOfStringUsingRecursion {
     public static void main(String[] args) {
+        String s = "madam";
+        boolean ans = isPalindrome(s,0, s.length()-1);
 
+        if ( ans == true)
+            System.out.println("It is a palindrome");
+        else
+            System.out.println("It is  not a palindrome");
+    }
+    static boolean isPalindrome(String s, int i,int j){
+
+        // base case
+        if (s.charAt(i) != s.charAt(j))
+            return  false;
+        if(j <= i)
+            return true;
+
+        return  isPalindrome(s, i+1, j - 1);
     }
 
 }
