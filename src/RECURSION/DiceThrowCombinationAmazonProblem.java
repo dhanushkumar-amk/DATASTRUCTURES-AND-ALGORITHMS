35774884624bc65f2cde6f8a0286d7d73077d5db
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-11 18:02:23 +0530

    Modified on 11-09-2024

--- src/RECURSION/DiceThrowCombinationAmazonProblem.java
+++ src/RECURSION/DiceThrowCombinationAmazonProblem.java
@@ -1,2 +1,20 @@
-package RECURSION;public class DiceThrowCombinationAmazonProblem {
+package RECURSION;
+
+public class DiceThrowCombinationAmazonProblem {
+    public static void main(String[] args) {
+        diceCombination("", 4);
+    }
+
+    static void diceCombination(String p, int target){
+
+        // base case
+        if(target == 0){
+            System.out.println(p);
+            return;
+        }
+
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            diceCombination(p + i, target - i);
+        }
+    }
 }
