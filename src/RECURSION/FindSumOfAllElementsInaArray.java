ce7d20832bea5b59c22d49ea55d4a054fa5bec4c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-11 19:51:44 +0530

    modified

--- src/RECURSION/FindSumOfAllElementsInaArray.java
+++ src/RECURSION/FindSumOfAllElementsInaArray.java
@@ -2,7 +2,16 @@ package RECURSION;
 
 public class FindSumOfAllElementsInaArray {
     public static void main(String[] args) {
+    int[] arr = {1,2,3,4};
+    }
 
+    static  int sum(int[] arr, int index){
+        // base case
+        if ( index == arr.length)
+            return 0;
+
+
+        return sum(arr, i + 1) +
     }
 
 }
