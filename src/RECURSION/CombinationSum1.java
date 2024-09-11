55799337ff36013b2ce72da2994c5a42e309cf6b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-11 17:42:28 +0530

    modified

--- src/RECURSION/CombinationSum1.java
+++ src/RECURSION/CombinationSum1.java
@@ -1,2 +1,35 @@
-package RECURSION;public class CombinationSum1 {
+package RECURSION;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum1 {
+    public static void main(String[] args) {
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+        System.out.println(combinationSum(candidates,target));
+
+    }
+
+    private static void findCombination(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds ){
+        if (ind == arr.length){
+            if (target == 0)
+                ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        if(arr[ind] <= target){
+            ds.add(arr[ind]);
+            findCombination(ind,arr,target - arr[ind],ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+        findCombination(ind + 1, arr, target,ans, ds);
+
+    }
+
+    public static  List<List<Integer>>combinationSum(int[] candidates,int target){
+        List<List<Integer>> ans = new ArrayList<>();
+        findCombination(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
 }
