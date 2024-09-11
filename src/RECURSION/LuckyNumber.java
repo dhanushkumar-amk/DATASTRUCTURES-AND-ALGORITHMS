68cfec70e4545e44664ac1f2eeec7d0e346a5d48
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-11 19:36:44 +0530

    Modified on 11-09-2024

--- src/RECURSION/LuckyNumber.java
+++ src/RECURSION/LuckyNumber.java
@@ -3,5 +3,22 @@ package RECURSION;
 public class LuckyNumber {
     public static void main(String[] args) {
         int num = 13;
+        int counter = 2;
+        boolean ans = luck(num,counter);
+        if ( ans == true)
+            System.out.println("It is a a lucky number ");
+        else
+            System.out.println("It is a unlucky number");
+    }
+    static boolean luck(int num, int counter){
+
+        // base case
+        if (num < counter)
+            return true;
+
+        if (num % counter == 0)
+            return  false;
+
+        return  luck(num - (num / counter), counter+1);
     }
 }
