3edc37376aa1f00995b8d4947693a289ce8060c8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-11 18:08:20 +0530

    modified

--- src/RECURSION/DiceCombinationReturnAsArrayList.java
+++ src/RECURSION/DiceCombinationReturnAsArrayList.java
@@ -1,2 +1,26 @@
-package RECURSION;public class DiceCombinationReturnAsArrayList {
+package RECURSION;
+
+import java.util.ArrayList;
+
+public class DiceCombinationReturnAsArrayList {
+
+    public static void main(String[] args) {
+        System.out.println(dice("", 4));
+    }
+    static ArrayList<String> dice(String p, int target){
+
+        if ( target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 1; i <=6 && i <= target ; i++) {
+            list.addAll(dice(p + i, target - i));
+        }
+        return list;
+
+    }
 }
