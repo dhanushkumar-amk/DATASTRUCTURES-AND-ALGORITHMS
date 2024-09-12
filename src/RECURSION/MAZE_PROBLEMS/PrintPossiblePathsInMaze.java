6294dcc0d8d946d49e91966e59cc9c14235f4b1f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-12 09:05:01 +0530

    Modified on 12-09-2024

--- src/RECURSION/MAZE_PROBLEMS/PrintPossiblePathsInMaze.java
+++ src/RECURSION/MAZE_PROBLEMS/PrintPossiblePathsInMaze.java
@@ -2,6 +2,23 @@ package RECURSION.MAZE_PROBLEMS;
 
 public class PrintPossiblePathsInMaze {
     public static void main(String[] args) {
-        System.out
+        printPath("",3,3);
+    }
+    static  void printPath(String p, int row, int column){
+
+        // base case
+        if(row == 1 && column == 1){
+            System.out.println(p);
+            return;
+        }
+
+        if (row > 1 ){
+            printPath(p + 'D', row - 1, column);
+        }
+
+        if (column > 1){
+            printPath(p + 'R', row, column - 1);
+        }
+
     }
 }
