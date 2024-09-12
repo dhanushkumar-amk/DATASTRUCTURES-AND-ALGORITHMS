30614ff74a57411b8532ef3949ef7671b82b57ea
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-12 07:47:34 +0530

    Modified on 12-09-2024

--- src/RECURSION/MAZE_PROBLEMS/CountPossiblePathsInMaze.java
+++ src/RECURSION/MAZE_PROBLEMS/CountPossiblePathsInMaze.java
@@ -1,2 +1,21 @@
-package RECURSION.MAZE_PROBLEMS;public class CountPossiblePathsInMaze {
+package RECURSION.MAZE_PROBLEMS;
+
+public class CountPossiblePathsInMaze {
+    public static void main(String[] args) {
+
+        int row = 3;
+        int column = 3;
+        System.out.println(countPath(row, column));
+    }
+    static  int countPath(int row, int column){
+
+        // base case
+        if(row == 1 || column == 1)
+            return 1;
+
+        int left = countPath(row - 1, column);
+        int right = countPath(row, column - 1);
+
+        return right + left;
+    }
 }
