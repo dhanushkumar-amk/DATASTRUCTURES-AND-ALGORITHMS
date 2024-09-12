6d086923920895112db0e8b76fd201f95fc30555
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-12 08:58:29 +0530

    modified

--- src/RECURSION/MAZE_PROBLEMS/PrintPossiblePathsInMaze.java
+++ src/RECURSION/MAZE_PROBLEMS/PrintPossiblePathsInMaze.java
@@ -2,6 +2,6 @@ package RECURSION.MAZE_PROBLEMS;
 
 public class PrintPossiblePathsInMaze {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out
     }
 }
