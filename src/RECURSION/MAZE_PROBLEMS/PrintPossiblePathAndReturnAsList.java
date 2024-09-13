6a15e60016d514a3ef1a03d33670403e2df33c61
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-13 16:08:10 +0530

    modified

--- src/RECURSION/MAZE_PROBLEMS/PrintPossiblePathAndReturnAsList.java
+++ src/RECURSION/MAZE_PROBLEMS/PrintPossiblePathAndReturnAsList.java
@@ -1,2 +1,28 @@
-package RECURSION.MAZE_PROBLEMS;public class PrintPossiblePathAndReturnAsList {
+package RECURSION.MAZE_PROBLEMS;
+
+import java.util.ArrayList;
+
+public class PrintPossiblePathAndReturnAsList {
+    public static void main(String[] args) {
+        System.out.println(path("", 3,3));
+    }
+    static ArrayList<String> path(String p, int row, int column){
+
+        // base case
+        if (row == 1 && column == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (row > 1)
+            list.addAll(path(p + 'D', row -1, column));
+
+        if (column > 1)
+            list.addAll(path( p + 'R', row, column - 1));
+
+        return list;
+    }
 }
