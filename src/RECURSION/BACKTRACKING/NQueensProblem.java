fc2356397d1e54c32204661626670c749ce5be6e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-13 20:38:15 +0530

    Modified on 13-09-2024

--- src/RECURSION/BACKTRACKING/NQueensProblem.java
+++ src/RECURSION/BACKTRACKING/NQueensProblem.java
@@ -1,7 +1,26 @@
 package RECURSION.BACKTRACKING;
 
+
+
 public class NQueensProblem {
     public static void main(String[] args) {
-        
+
+    }
+    static  int queens(boolean[][] board, int row){
+        if (row == board.length){
+            Display(board);
+            return 1;
+        }
+    }
+
+    private static void Display(boolean[][] board) {
+        for (boolean[] row : board){
+            for (boolean element : row){
+                if (element == true)
+                    System.out.println("Q ");
+                else
+                    System.out.println("");
+            }
+        }
     }
 }
