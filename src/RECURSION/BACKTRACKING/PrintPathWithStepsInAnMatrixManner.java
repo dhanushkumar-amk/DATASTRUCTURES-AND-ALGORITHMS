af6a2a8b981d4f50e0c7437db0eec3899f0b8992
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-13 18:47:59 +0530

    modified

--- src/RECURSION/BACKTRACKING/PrintPathWithStepsInAnMatrixManner.java
+++ src/RECURSION/BACKTRACKING/PrintPathWithStepsInAnMatrixManner.java
@@ -1,2 +1,55 @@
-package RECURSION.BACKTRACKING;public class PrintPathWithStepsInAnMatrixManner {
+package RECURSION.BACKTRACKING;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PrintPathWithStepsInAnMatrixManner {
+    public static void main(String[] args) {
+
+    boolean[][] maze =  {
+          {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        };
+
+    int[][] path = new int[maze.length][maze[0].length];
+
+
+        }
+
+        static  void print(String p, boolean[][] maze, int row, int column, int[][] path, int steps){
+
+        if (row == maze.length - 1 && column ==maze[0].length - 1){
+
+            for (int[] i : path)
+                System.out.println(Arrays.toString(i));
+            System.out.println();
+        }
+
+        if (maze[row][column] == false)
+            return;
+        else
+            maze[row][column] = false;
+
+        path[row][column] = steps;
+
+            if (row < maze.length - 1)
+                print(p + 'D', maze,row + 1,column,path,steps + 1);
+
+
+            if (column < maze[0].length - 1)
+                print(p + 'R', maze,row ,column + 1,path,steps + 1);
+
+
+            if (row > 0 )
+                print(p + 'U', maze,row - 1,column,path,steps + 1);
+
+            if (column > 0)
+                print(p + 'L', maze,row ,column + 1,path,steps + 1);
+
+            maze[row][column] = true;
+            path[row][column] = 0;
+
+    }
+
 }
