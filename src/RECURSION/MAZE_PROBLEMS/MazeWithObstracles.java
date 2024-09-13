88b14c04353149dd4e7544806c77bc2ff9d32c06
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-13 16:43:20 +0530

    Modified on 13-09-2024

--- src/RECURSION/MAZE_PROBLEMS/MazeWithObstracles.java
+++ src/RECURSION/MAZE_PROBLEMS/MazeWithObstracles.java
@@ -23,11 +23,11 @@ public class MazeWithObstracles {
             return;
 
 
-        if ( row < maze.length - 1)
-          path( p + 'D', maze, row - 1, column);
+        if ( row < 0)
+          path( p + 'U', maze, row - 1, column);
 
-        if (column < maze[0].length - 1)
-          path(p + 'R', maze, row, column - 1);
+        if (column < 0)
+          path(p + 'L', maze, row, column - 1);
 
 
     }
