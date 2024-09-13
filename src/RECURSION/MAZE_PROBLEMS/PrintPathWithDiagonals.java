8f703cd5841cdab3ce0969afa6fe6497adc50f9d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-13 16:13:54 +0530

    modified

--- src/RECURSION/MAZE_PROBLEMS/PrintPathWithDiagonals.java
+++ src/RECURSION/MAZE_PROBLEMS/PrintPathWithDiagonals.java
@@ -1,2 +1,31 @@
-package RECURSION.MAZE_PROBLEMS;public class PrintPathWithDiagonals {
+package RECURSION.MAZE_PROBLEMS;
+
+import java.util.ArrayList;
+
+public class PrintPathWithDiagonals {
+    public static void main(String[] args) {
+        System.out.println(path("", 3,3));
+    }
+    static ArrayList<String> path(String p, int row, int column){
+
+        // base case
+        if (row == 1 && column == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (row > 1 && column > 1)
+            list.addAll(path(p + 'D', row - 1, column - 1));
+
+        if (row > 1)
+            list.addAll(path(p + 'D', row -1, column));
+
+        if (column > 1)
+            list.addAll(path( p + 'R', row, column - 1));
+
+        return list;
+    }
 }
