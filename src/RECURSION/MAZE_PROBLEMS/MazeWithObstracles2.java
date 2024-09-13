152d4968db9a86bd093730b15982dc54d2e0cd12
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-13 16:49:42 +0530

    modified

--- src/RECURSION/MAZE_PROBLEMS/MazeWithObstracles2.java
+++ src/RECURSION/MAZE_PROBLEMS/MazeWithObstracles2.java
@@ -1,2 +1,7 @@
-package RECURSION.MAZE_PROBLEMS;public class MazeWithObstracles2 {
+package RECURSION.MAZE_PROBLEMS;
+
+public class MazeWithObstracles2 {
+    public static void main(String[] args) {
+
+    }
 }
