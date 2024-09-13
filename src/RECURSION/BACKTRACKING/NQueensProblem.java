d6a89b4b9c95cc64d6e062c98776c91ee9d89474
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-13 20:50:03 +0530

    Modified on 13-09-2024

--- src/RECURSION/BACKTRACKING/NQueensProblem.java
+++ src/RECURSION/BACKTRACKING/NQueensProblem.java
@@ -5,22 +5,66 @@ package RECURSION.BACKTRACKING;
 public class NQueensProblem {
     public static void main(String[] args) {
 
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        queens(board, 0);
     }
     static  int queens(boolean[][] board, int row){
         if (row == board.length){
             Display(board);
             return 1;
         }
+
+        int count = 0;
+        //placing the queen and checking every row and column
+        for (int column = 0; column <board.length; column++) {
+            // place the queen if it is safe
+
+            if (issafe(board, row,column)){
+                board[row][column] = true;
+                count += queens(board, row + 1);
+                board[row][column] = false;
+            }
+
+        }
+        return count;
+
+    }
+
+    private static boolean issafe(boolean[][] board, int row, int column) {
+        // check vertical or up
+        for (int i = 0; i < row ; i++) {
+            if (board[i][column] == true)
+                return false;
+        }
+        // diagonal left
+        int maxLeft = Math.min(row, column);
+        for (int i = 1; i <= maxLeft ; i++) {
+            if (board[row - i][column - i]){
+                return false;
+            }
+        }
+
+        // diagonal right
+        int maxRight = Math.min(row, board.length - column-1 );
+        for (int i = 1; i <= maxRight ; i++) {
+            if (board[row - i][column + i]){
+                return false;
+            }
+        }
+
+        return  true;
     }
 
     private static void Display(boolean[][] board) {
         for (boolean[] row : board){
             for (boolean element : row){
                 if (element == true)
-                    System.out.println("Q ");
+                    System.out.print("Q ");
                 else
-                    System.out.println("");
+                    System.out.print(" ");
             }
+            System.out.println();
         }
     }
 }
