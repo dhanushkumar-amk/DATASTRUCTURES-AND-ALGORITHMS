73a61a34a59c42533bae40d425bf5231b6d2146a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-13 19:03:09 +0530

    Modified on 13-09-2024

--- src/RECURSION/BACKTRACKING/PrintPathWithStepsInAnMatrixManner.java
+++ src/RECURSION/BACKTRACKING/PrintPathWithStepsInAnMatrixManner.java
@@ -14,7 +14,8 @@ public class PrintPathWithStepsInAnMatrixManner {
 
         int[][] path = new int[maze.length][maze[0].length];
 
-        print("", maze,0,0,path,1);
+//        print("", maze,0,0,path,1);
+        print1("", maze,0,0,path,1);
 
         }
 
@@ -56,6 +57,54 @@ public class PrintPathWithStepsInAnMatrixManner {
             //  make it as 0
             path[row][column] = 0;
 
+    }
+
+
+
+    static void print1(String p, boolean[][] maze, int row, int column, int[][] path, int steps) {
+        // Check if the current position is out of bounds or not valid
+        if (row < 0 || row >= maze.length || column < 0 || column >= maze[0].length || !maze[row][column]) {
+            return;
+        }
+
+        // Check if we have reached the destination
+        if (row == maze.length - 1 && column == maze[0].length - 1) {
+            path[row][column] = steps;
+            for (int[] i : path) {
+                System.out.println(Arrays.toString(i));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        // Mark the cell as visited
+        maze[row][column] = false;
+        path[row][column] = steps;
+
+        // Move Down
+        if (row < maze.length - 1) {
+            print1(p + 'D', maze, row + 1, column, path, steps + 1);
+        }
+
+        // Move Right
+        if (column < maze[0].length - 1) {
+            print1(p + 'R', maze, row, column + 1, path, steps + 1);
+        }
+
+        // Move Up
+        if (row > 0) {
+            print1(p + 'U', maze, row - 1, column, path, steps + 1);
+        }
+
+        // Move Left
+        if (column > 0) {
+            print1(p + 'L', maze, row, column - 1, path, steps + 1);
+        }
+
+        // Backtrack: Unmark the cell as visited and reset path
+        maze[row][column] = true;
+        path[row][column] = 0;
     }
 
 }
