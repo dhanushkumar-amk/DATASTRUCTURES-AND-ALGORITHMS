9a2bba01c19be4c056402a8c05bc91fb8e077e29
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 11:11:35 +0530

    Modified on 14-09-2024

--- src/RECURSION/BACKTRACKING/SudokuSolver.java
+++ src/RECURSION/BACKTRACKING/SudokuSolver.java
@@ -1,8 +1,6 @@
 package RECURSION.BACKTRACKING;
 
 public class SudokuSolver {
-    public static void main(String[] args) {
-
 
         /*
         char[][] board = {
@@ -19,110 +17,108 @@ public class SudokuSolver {
 
          */
 
-        int[][]  board = {
+    public static void main(String[] args) {
+        int[][] board = {
                 {5,3,0,0,7,0,0,0,0},
                 {6,0,0,1,9,5,0,0,0},
                 {0,9,8,0,0,0,0,6,0},
                 {8,0,0,0,6,0,0,0,3},
-                {4,0,0,8,9,3,0,0,1},
+                {4,0,0,8,0,3,0,0,1},
                 {7,0,0,0,2,0,0,0,6},
                 {0,6,0,0,0,0,2,8,0},
                 {0,0,0,4,1,9,0,0,5},
-                {0,0,0,0,8,0,0,7,9},
-            };
+                {0,0,0,0,8,0,0,7,9}
+        };
+
         System.out.println(solve(board));
 
-        if (solve(board))
+        if (solve(board)) {
             Display(board);
-
-        else
-            System.out.println("Can't be solve");
+        } else {
+            System.out.println("Can't be solved");
+        }
     }
 
-    public static void solveSudoku(char[][] board) {
-
-
-
-
-    }
-
-    static  boolean solve(int[][] board){
-
-        int n  = board.length;
-        int row = - 1;
+    public static boolean solve(int[][] board) {
+        int n = board.length;
+        int row = -1;
         int column = -1;
 
         boolean emptyLeft = true;
 
-        for (int i = 0; i < n ; i++) {
-            for (int j = 0; j < n ; j++) {
-                if (board[i][j] == 0){
+        // Find the first empty cell
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
                     row = i;
                     column = j;
                     emptyLeft = false;
                     break;
                 }
             }
-            // if you found  some empty element in row then break;
-        if (emptyLeft == false)
-            break;
-        }
-
-        if (emptyLeft == true)
-            return true;
-
-        for (int number = 1; number <= 9 ; number++) {
-            if (isSafe(board, row, column,number)){
-                board[row][column] = number;
-
-                if ( solve(board)) {
-                    return true;
-                }
-                else
-                    board[row][column] = 0;
+            if (!emptyLeft) {
+                break;
             }
         }
-        return  false;
+
+        // If no empty cell is found, the puzzle is solved
+        if (emptyLeft) {
+            return true;
+        }
+
+        // Try placing numbers from 1 to 9 in the empty cell
+        for (int number = 1; number <= 9; number++) {
+            if (isSafe(board, row, column, number)) {
+                board[row][column] = number;
+
+                // Recursively attempt to solve the rest of the board
+                if (solve(board)) {
+                    return true;
+                } else {
+                    // Backtrack
+                    board[row][column] = 0;
+                }
+            }
+        }
+        return false;
     }
 
-    private static  void  Display(int[][] board){
-        for (int[] row : board){
-            for (int num : row){
-                System.out.println(num + "  ");
+    private static void Display(int[][] board) {
+        for (int[] row : board) {
+            for (int num : row) {
+                System.out.print(num + " ");
             }
             System.out.println();
         }
     }
 
-    static  boolean isSafe(int[][] board, int row, int column, int num){
-        // check the row
-        // if there already a number is present in this number we can't put number so return false
-        for (int i = 0; i < board.length ; i++) {
-            if (board[row][i] == num)
-                return  false;
-        }
-
-        // for every column
-        for(int[] nums : board){
-            if (nums[column] == num)
+    static boolean isSafe(int[][] board, int row, int column, int num) {
+        // Check the row
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == num) {
                 return false;
+            }
         }
 
+        // Check the column
+        for (int[] nums : board) {
+            if (nums[column] == num) {
+                return false;
+            }
+        }
 
-        // for checking sub mtrix
-        int sqrt = (int) (Math.sqrt(board.length));
-
-//        sub matrix start column and row
-
+        // Check the 3x3 sub-grid
+        int sqrt = (int) Math.sqrt(board.length);
         int rowStart = row - row % sqrt;
         int colStart = column - column % sqrt;
 
-        for (int r = rowStart; r < rowStart + sqrt ; r++) {
+        for (int r = rowStart; r < rowStart + sqrt; r++) {
             for (int c = colStart; c < colStart + sqrt; c++) {
-                if (board[r][c] = num)
-                    return  ;
+                if (board[r][c] == num) {
+                    return false;
+                }
             }
         }
-        return  true;
+        return true;
     }
 }
