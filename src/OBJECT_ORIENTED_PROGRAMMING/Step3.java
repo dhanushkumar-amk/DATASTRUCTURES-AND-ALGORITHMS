17d60e9eaccaf490d5fdf902400022e578c6c64f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 17:02:55 +0530

    Modified on 14-09-2024

--- src/OBJECT_ORIENTED_PROGRAMMING/Step3.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/Step3.java
@@ -2,6 +2,9 @@ package OBJECT_ORIENTED_PROGRAMMING;
 
 import static OBJECT_ORIENTED_PROGRAMMING.importPackage.message;
 
+// import statement used to import method from one file to another file;
+
+
 public class Step3 {
 
     public static void main(String[] args) {
