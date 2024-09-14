f894862b98535418028a78be9a3b01b6e45c0790
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 10:11:48 +0530

    modified

--- src/RECURSION/BACKTRACKING/SudokuSolver.java
+++ src/RECURSION/BACKTRACKING/SudokuSolver.java
@@ -1,2 +1,8 @@
-package RECURSION.BACKTRACKING;public class SudokuSolver {
+package RECURSION.BACKTRACKING;
+
+public class SudokuSolver {
+    public static void main(String[] args) {
+
+    }
+    
 }
