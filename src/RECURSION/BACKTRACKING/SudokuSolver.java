b66224d96049ba4f78b79aaaa062405f47168a9f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 11:10:11 +0530

    Modified on 14-09-2024

--- src/RECURSION/BACKTRACKING/SudokuSolver.java
+++ src/RECURSION/BACKTRACKING/SudokuSolver.java
@@ -42,12 +42,12 @@ public class SudokuSolver {
     public static void solveSudoku(char[][] board) {
 
 
-        
-        
+
+
     }
-    
+
     static  boolean solve(int[][] board){
-        
+
         int n  = board.length;
         int row = - 1;
         int column = -1;
@@ -93,34 +93,34 @@ public class SudokuSolver {
             System.out.println();
         }
     }
-    
+
     static  boolean isSafe(int[][] board, int row, int column, int num){
         // check the row
-        // if there already a number is present in this number we can't put number so return false 
+        // if there already a number is present in this number we can't put number so return false
         for (int i = 0; i < board.length ; i++) {
             if (board[row][i] == num)
                 return  false;
         }
-        
+
         // for every column
         for(int[] nums : board){
             if (nums[column] == num)
                 return false;
         }
-        
-        
+
+
         // for checking sub mtrix
         int sqrt = (int) (Math.sqrt(board.length));
-        
+
 //        sub matrix start column and row
-        
+
         int rowStart = row - row % sqrt;
         int colStart = column - column % sqrt;
 
         for (int r = rowStart; r < rowStart + sqrt ; r++) {
             for (int c = colStart; c < colStart + sqrt; c++) {
                 if (board[r][c] = num)
-                    return  false;
+                    return  ;
             }
         }
         return  true;
