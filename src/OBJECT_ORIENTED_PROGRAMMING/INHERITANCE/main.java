d840975549bb2c39ec7cc8a769cadcbfda74684c
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 21:22:09 +0530

    modified

--- src/OBJECT_ORIENTED_PROGRAMMING/INHERITANCE/main.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/INHERITANCE/main.java
@@ -3,7 +3,7 @@ package OBJECT_ORIENTED_PROGRAMMING.INHERITANCE;
 public class main {
     public static void main(String[] args) {
 
-        //
+        // default constructor
         BOX box = new BOX();
         System.out.println(box.height + " " +  box.length + " " + box.width);
     }
