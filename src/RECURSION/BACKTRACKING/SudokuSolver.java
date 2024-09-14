a0cda4082f5b28bde078ac77b03191f7fbe130f9
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 11:09:04 +0530

    modified

--- src/RECURSION/BACKTRACKING/SudokuSolver.java
+++ src/RECURSION/BACKTRACKING/SudokuSolver.java
@@ -3,6 +3,8 @@ package RECURSION.BACKTRACKING;
 public class SudokuSolver {
     public static void main(String[] args) {
 
+
+        /*
         char[][] board = {
                 {'5','3','.','.','7','.','.','.','.'},
                 {'6','.','.','1','9','5','.','.','.'},
@@ -14,10 +16,32 @@ public class SudokuSolver {
                 {'.','.','.','4','1','9','.','.','5'},
                 {'.','.','.','.','8','.','.','7','9'},
              };
-        }
+
+         */
+
+        int[][]  board = {
+                {5,3,0,0,7,0,0,0,0},
+                {6,0,0,1,9,5,0,0,0},
+                {0,9,8,0,0,0,0,6,0},
+                {8,0,0,0,6,0,0,0,3},
+                {4,0,0,8,9,3,0,0,1},
+                {7,0,0,0,2,0,0,0,6},
+                {0,6,0,0,0,0,2,8,0},
+                {0,0,0,4,1,9,0,0,5},
+                {0,0,0,0,8,0,0,7,9},
+            };
+        System.out.println(solve(board));
+
+        if (solve(board))
+            Display(board);
+
+        else
+            System.out.println("Can't be solve");
+    }
 
     public static void solveSudoku(char[][] board) {
-        
+
+
         
         
     }
@@ -28,10 +52,45 @@ public class SudokuSolver {
         int row = - 1;
         int column = -1;
 
+        boolean emptyLeft = true;
+
         for (int i = 0; i < n ; i++) {
-            for (int j = 0; j < ; j++) {
-                
+            for (int j = 0; j < n ; j++) {
+                if (board[i][j] == 0){
+                    row = i;
+                    column = j;
+                    emptyLeft = false;
+                    break;
+                }
             }
+            // if you found  some empty element in row then break;
+        if (emptyLeft == false)
+            break;
+        }
+
+        if (emptyLeft == true)
+            return true;
+
+        for (int number = 1; number <= 9 ; number++) {
+            if (isSafe(board, row, column,number)){
+                board[row][column] = number;
+
+                if ( solve(board)) {
+                    return true;
+                }
+                else
+                    board[row][column] = 0;
+            }
+        }
+        return  false;
+    }
+
+    private static  void  Display(int[][] board){
+        for (int[] row : board){
+            for (int num : row){
+                System.out.println(num + "  ");
+            }
+            System.out.println();
         }
     }
     
