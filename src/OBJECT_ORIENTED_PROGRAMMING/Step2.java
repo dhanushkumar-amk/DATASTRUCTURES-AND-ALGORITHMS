2faeaef87c053d126aa7fcf7dfe50d0ca57820b2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 16:30:51 +0530

    modified

--- src/OBJECT_ORIENTED_PROGRAMMING/Step2.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/Step2.java
@@ -50,7 +50,7 @@ public class Step2 {
       */
 
         A obj;
-        for (int i = 0; i < 100000000 ; i++) {
+        for (int i = 0; i < 10 ; i++) {
             obj = new A("random name");
         }
     }
