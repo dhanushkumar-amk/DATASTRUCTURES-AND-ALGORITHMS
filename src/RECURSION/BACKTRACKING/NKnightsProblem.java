ee8c8ef85a999798d517e0af89447ce63034f8c7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 07:53:12 +0530

    modified

--- src/RECURSION/BACKTRACKING/NKnightsProblem.java
+++ src/RECURSION/BACKTRACKING/NKnightsProblem.java
@@ -13,6 +13,10 @@ public class NKnightsProblem {
 
         }
 
+     // if row and column become out of bounds then just return
+     if (row == board.length - 1 && column == board.length)
+         return;
+
      // if one column reaches the end then go to the next row and start with column 0
      if (column == board.length)
          knight(board, row + 1, 0, knightCount);
@@ -24,8 +28,21 @@ public class NKnightsProblem {
          knight(board, row, column + 1, knightCount - 1);
          // then backtrack and check
          board[row][column] = false;
-     }
+      }
+     knight(board, row,column + 1, knightCount);
 
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int column) {
+
+    }
+
+
+    private  static boolean  isValid(boolean[][] board, int row, int column){
+        if (row >= 0 && row < board.length && column >= 0 && column < board.length)
+            return true;
+
+        return false;
     }
 
     private static void Display(boolean[][] board) {
