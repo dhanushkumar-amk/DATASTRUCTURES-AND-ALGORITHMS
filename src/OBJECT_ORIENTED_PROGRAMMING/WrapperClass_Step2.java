898f7078f3117b530b079e93461a8f90e0121f49
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 15:57:19 +0530

    modified

--- src/OBJECT_ORIENTED_PROGRAMMING/WrapperClass_Step2.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/WrapperClass_Step2.java
@@ -18,15 +18,28 @@ public class WrapperClass_Step2 {
 
 
         swap(a, b);
+
+        // here the swap will not be change
+        // because here it is an pass by reference
         System.out.println(a);
         System.out.println(b);
 
-        System.out.println(num);
+        // the value of a and b is not swap
+
+//        System.out.println(num);
     }
 
     static  void swap(Integer a, Integer b){
         Integer temp = a;
         a = b;
         b = temp;
+
+
+        System.out.println(a + " " +  b);
     }
+
+
+
+
+
 }
