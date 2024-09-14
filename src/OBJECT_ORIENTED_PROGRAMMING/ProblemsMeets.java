a9eff88b2bc3ae60ecf8cd022b036c0fd064724e
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 12:40:48 +0530

    modified

--- src/OBJECT_ORIENTED_PROGRAMMING/ProblemsMeets.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/ProblemsMeets.java
@@ -1,2 +1,15 @@
-package OBJECT_ORIENTED_PROGRAMMING;public class ProblemsMeets {
+package OBJECT_ORIENTED_PROGRAMMING;
+
+public class ProblemsMeets {
+    public static void main(String[] args) {
+
+        // store 5 rollno
+        int[] roll = new int[5];
+
+        // store 5 names
+        String[] names = new String[5];
+
+        // data of 5 students that consists of name roll no,marks within an single data structure
+//        here will be used objected oriented programming
+    }
 }
