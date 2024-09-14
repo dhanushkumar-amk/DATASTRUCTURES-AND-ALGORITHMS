192399cfa2285c711e3b313d3482261f0d18c0bf
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 21:19:47 +0530

    modified

--- src/OBJECT_ORIENTED_PROGRAMMING/INHERITANCE/BOX.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/INHERITANCE/BOX.java
@@ -1,2 +1,39 @@
-package OBJECT_ORIENTED_PROGRAMMING.INHERITANCE;public class BOX {
+package OBJECT_ORIENTED_PROGRAMMING.INHERITANCE;
+
+public class BOX {
+    double length;
+    double height;
+    double width;
+
+    BOX(){
+        this.height = -1;
+        this.length = -1;
+        this.width = - 1;
+    }
+
+    // cube
+    //
+    BOX(double side){
+        this.width = side;
+        this.height = side;
+        this.length = side;
+    }
+
+    // three arguments
+     BOX(double length, double height, double width){
+        this.width =  width;
+        this.height = height;
+        this.length = length;
+    }
+
+
+    // copy constructor
+    BOX(BOX old){
+        this.width = old.width;
+        this.length = old.length;
+        this.height  = old.height;
+    }
+
+
+
 }
