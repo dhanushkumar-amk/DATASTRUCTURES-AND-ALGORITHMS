0b5b99dc28be0fb96eaa7db5fe9b7c7d7bf3a2d9
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-14 15:44:47 +0530

    Modified on 14-09-2024

--- src/OBJECT_ORIENTED_PROGRAMMING/WrapperClass_Step2.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/WrapperClass_Step2.java
@@ -1,2 +1,13 @@
-package OBJECT_ORIENTED_PROGRAMMING;public class WrapperClass_Step2 {
+package OBJECT_ORIENTED_PROGRAMMING;
+
+public class WrapperClass_Step2 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // wrapper class
+
+        Integer num = 5;
+
+        System.out.println(num);
+    }
 }
