ebee1a0b5447d76b28f0e211ca8e9bf40f142dec
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-15 20:33:15 +0530

    modified

--- src/OBJECT_ORIENTED_PROGRAMMING/POLYMORPHISM/circle.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/POLYMORPHISM/circle.java
@@ -1,8 +1,15 @@
 package OBJECT_ORIENTED_PROGRAMMING.POLYMORPHISM;
 
 public class circle extends  shapes{
+
+ @Override
     void area(){
         System.out.println("Area r* r * pi :");
+    }
+
+//    @Override   wen the function doesn't not override then it will show error on override
+    void area1(){
+        System.out.println("Override is called anatations");
     }
 }
 
