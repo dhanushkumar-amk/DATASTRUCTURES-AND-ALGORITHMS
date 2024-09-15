3c5f53df300cc13575158369aa4dffcf57054ff3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-15 20:16:37 +0530

    modified

--- src/OBJECT_ORIENTED_PROGRAMMING/POLYMORPHISM/Numbera.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/POLYMORPHISM/Numbera.java
@@ -1,2 +1,21 @@
-package OBJECT_ORIENTED_PROGRAMMING.POLYMORPHISM;public class Numbera {
+package OBJECT_ORIENTED_PROGRAMMING.POLYMORPHISM;
+
+public class Numbera {
+    int sum(int a, int b){
+        return  a + b;
+    }
+        int sum(int a, int b, int c){
+            return  a + b + c;
+        }
+
+    public static void main(String[] args) {
+
+        Numbera num = new Numbera();
+
+        // method overloading
+        num.sum(10,20);
+        num.sum(5,4,6);
+//        num.sum(5,4,3,2);
+    }
+
 }
