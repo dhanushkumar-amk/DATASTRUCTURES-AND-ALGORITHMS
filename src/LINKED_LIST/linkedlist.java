e302901a906962b2018a78966d627bf1faa6f815
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-16 20:34:27 +0530

    modified

--- src/LINKED_LIST/linkedlist.java
+++ src/LINKED_LIST/linkedlist.java
@@ -1,2 +1,30 @@
-package LINKED_LIST;public class linkedlist {
+package LINKED_LIST;
+
+public class linkedlist {
+
+
+    private  Node head;
+    private Node tail;
+
+    private int size;
+
+    public linkedlist(){
+        this.size = 0;
+    }
+    
+
+   private  class Node{
+       private int value;
+       private Node next;
+
+    public  Node(int value){
+        this.value = value;
+     }
+
+     public  Node(int value, Node next){
+        this.value = value;
+        this.next = next;
+     }
+   }
+
 }
--- src/LINKED_LIST/main.java
+++ src/LINKED_LIST/main.java
@@ -1,2 +1,7 @@
-package LINKED_LIST;public class main {
+package LINKED_LIST;
+
+public class main {
+    public static void main(String[] args) {
+        linkedlist list = new linkedlist();
+    }
 }
