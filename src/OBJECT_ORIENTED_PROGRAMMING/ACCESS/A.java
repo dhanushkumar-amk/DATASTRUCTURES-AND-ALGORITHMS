91312a04ec01dd9d5ba2f657edc158a63acfe8cf
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-16 19:43:03 +0530

    Modified on 16-09-2024

--- src/OBJECT_ORIENTED_PROGRAMMING/ACCESS/A.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/ACCESS/A.java
@@ -1,2 +1,15 @@
-package OBJECT_ORIENTED_PROGRAMMING.ACCESS;public class A {
+package OBJECT_ORIENTED_PROGRAMMING.ACCESS;
+
+public class A {
+    int num;
+    String name;
+    int[] arr;
+
+    public A(int num, String name){
+        this.num = num;
+        this.name = name ;
+        this.arr = new  int[34];
+
+
+    }
 }
--- src/OBJECT_ORIENTED_PROGRAMMING/ACCESS/Main.java
+++ src/OBJECT_ORIENTED_PROGRAMMING/ACCESS/Main.java
@@ -1,2 +1,14 @@
-package OBJECT_ORIENTED_PROGRAMMING.ACCESS;public class Main {
+package OBJECT_ORIENTED_PROGRAMMING.ACCESS;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(20,"Dhanushkumar");
+
+        // need to do some thing
+        // 1. access the data member
+        // 2. modify the data member
+
+
+
+    }
 }
