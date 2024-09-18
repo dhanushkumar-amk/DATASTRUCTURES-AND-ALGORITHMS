2e599fb528ec85518c6139db465dda25e51c1a33
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-19 04:54:15 +0530

    Modified on 19-09-2024

--- src/LINKED_LIST/RemoveDuplicatesFomSortedList.java
+++ src/LINKED_LIST/RemoveDuplicatesFomSortedList.java
@@ -1,8 +1,78 @@
 package LINKED_LIST;
 
+import java.util.LinkedList;
+
 public class RemoveDuplicatesFomSortedList {
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public RemoveDuplicatesFomSortedList(){
+        this.size = 0;
+    }
+
     public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        RemoveDuplicatesFomSortedList ll = new RemoveDuplicatesFomSortedList();
+
+        list.add(5);
+        list.add(5);
+        list.add(4);
+        list.add(3);
+        list.add(3);
+
+        ll.display();
+
+        ll.removeDuplicates();
+
 
     }
-    
+
+
+
+
+    public void removeDuplicates(){
+        Node node = head;
+
+        while (node.next != null){
+            if (node.value == node.next.value){
+                node.next = node.next.next;
+                size--;
+            }
+            else
+                node = node.next;
+
+        }
+        tail = node;
+        tail.next = null;
+
+    }
+
+// display function
+    public  void display(){
+        Node node = head;
+
+        while (node != null){
+            System.out.print(node.value + " -> ");
+            node = node.next;
+        }
+        System.out.print("end");
+
+    }
+
+    private  class Node{
+        private int value;
+        private Node next;
+
+        public  Node(int value){
+            this.value = value;
+        }
+
+        public  Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
 }
