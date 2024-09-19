40b595537b8825abac367416c0e4c66b5310d080
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-19 18:46:57 +0530

    Modified on 19-09-2024

--- src/LINKED_LIST/MergeTwoSortedList.java
+++ src/LINKED_LIST/MergeTwoSortedList.java
@@ -2,8 +2,43 @@ package LINKED_LIST;
 
 public class MergeTwoSortedList {
     public static void main(String[] args) {
+         Node head;
+         Node tail;
+
+        int size;
+
+    public MergeTwoSortedList(){
+            this.size = 0;
+    }
+
+
+    // display function
+    public  void display(){
+        Node node = head;
+
+        while (node != null){
+            System.out.print(node.value + " -> ");
+            node = node.next;
+        }
+        System.out.print("end");
 
     }
 
 
+
 }
+
+    private  class Node{
+        private int value;
+        private Node next;
+
+        public  Node(int value){
+            this.value = value;
+        }
+
+        public  Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+
