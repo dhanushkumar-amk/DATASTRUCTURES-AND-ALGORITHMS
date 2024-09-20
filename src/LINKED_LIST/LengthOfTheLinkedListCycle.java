5f3c9d0d694a8e7e37d53d9ea3232038f211aa41
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-20 19:51:35 +0530

    Modified on 20-09-2024

--- src/LINKED_LIST/LengthOfTheLinkedListCycle.java
+++ src/LINKED_LIST/LengthOfTheLinkedListCycle.java
@@ -1,2 +1,34 @@
-package LINKED_LIST;public class LengthOfTheLinkedListCycle {
+package LINKED_LIST;
+
+public class LengthOfTheLinkedListCycle {
+
+    // amazon and flipkart
+
+    LengthOfTheLinkedListCycle next;
+
+    public int LengthCycle(LengthOfTheLinkedListCycle head) {
+        LengthOfTheLinkedListCycle fast = head;
+        LengthOfTheLinkedListCycle slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                // calculate the length
+
+                LengthOfTheLinkedListCycle temp = slow;
+                int length = 0;
+
+                do{
+                    temp = temp.next;
+                    length++;
+                }while (temp != slow);
+
+
+                return length;
+            }
+        }
+        return 0;
+    }
 }
--- src/LINKED_LIST/LinkedLsitCycle.java
+++ src/LINKED_LIST/LinkedLsitCycle.java
@@ -1,2 +1,22 @@
-package LINKED_LIST;public class LinkedLsitCycle {
+package LINKED_LIST;
+
+public class LinkedLsitCycle {
+
+
+LinkedLsitCycle next;
+    // leetcode 141 -problem can't have default solution
+    public boolean hasCycle(LinkedLsitCycle head) {
+        LinkedLsitCycle fast = head;
+        LinkedLsitCycle slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
+
 }
