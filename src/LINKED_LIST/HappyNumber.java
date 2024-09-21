2c7f11c27baa185e6d729bdefa1855dd551ef241
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-21 14:14:03 +0530

    modified

--- src/LINKED_LIST/HappyNumber.java
+++ src/LINKED_LIST/HappyNumber.java
@@ -1,2 +1,44 @@
-package LINKED_LIST;public class HappyNumber {
+package LINKED_LIST;
+
+// leetcode 202
+
+// related to cyclic linkedList
+
+
+public class HappyNumber {
+    public static boolean isHappy(int n){
+        int slow = n;
+        int fast = n;
+
+        do {
+
+            slow = findSquare(slow); // move one steps
+            fast = findSquare(findSquare(fast)); // move two steps
+
+        }while (fast != slow);
+
+        if (slow == 1)
+            return true;
+        else
+            return false;
+
+    }
+
+    private static  int findSquare(int number){
+        int answer = 0;
+
+        while (number > 0){
+            int rem = number % 10;
+            answer += rem * rem;
+            number = number / 10;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 12;
+        System.out.println(isHappy(n));
+    }
+
 }
