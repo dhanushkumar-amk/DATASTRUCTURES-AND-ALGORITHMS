33220adcb8c716b7f6e245a245d7ae99fda62d27
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-23 20:15:34 +0530

    modified

--- src/LINKED_LIST/ReverseALinkedListUsingRecursion.java
+++ src/LINKED_LIST/ReverseALinkedListUsingRecursion.java
@@ -1,18 +1,6 @@
 package LINKED_LIST;
 
 public class ReverseALinkedListUsingRecursion {
-    public static void main(String[] args) {
-
-        ReverseALinkedListUsingRecursion list = new ReverseALinkedListUsingRecursion();
-        list.insertFirst(5);
-        list.insertFirst(4);
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(1);
-
-        list.display();
-
-    }
 
 
 
@@ -24,17 +12,7 @@ public class ReverseALinkedListUsingRecursion {
     public ReverseALinkedListUsingRecursion(){
         this.size = 0;
     }
-    public void insertFirst(int val){
-        Node node = new Node(val);   // create a nwe node;
-        node.next = head;  //  node => next = head
-        head = node; // then this node will be first head
 
-        if (tail == null)
-            tail = head; // if tail is a empty then head is equal to tail
-
-        size += 1;    // else size+=1
-
-    }
 
     // reverse a linkedList
 
@@ -50,17 +28,7 @@ public class ReverseALinkedListUsingRecursion {
         tail.next = null;
     }
 
-    // display function
-    public  void display(){
-        Node node = head;
 
-        while (node != null){
-            System.out.print(node.value + " -> ");
-            node = node.next;
-        }
-        System.out.print("end");
-
-    }
 
     private  class Node{
         private int value;
