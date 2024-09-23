30df40f6f11e972d2f5bcbc7f8cf7b8c073956d2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-23 20:09:12 +0530

    Modified on 23-09-2024

--- src/LINKED_LIST/ReverseALinkedListUsingRecursion.java
+++ src/LINKED_LIST/ReverseALinkedListUsingRecursion.java
@@ -1,2 +1,33 @@
-package LINKED_LIST;public class ReverseALinkedListUsingRecursion {
+package LINKED_LIST;
+
+public class ReverseALinkedListUsingRecursion {
+    public static void main(String[] args) {
+
+    }
+
+    // display function
+    public  void display(){
+        Node node = head;
+
+        while (node != null){
+            System.out.print(node.value + " -> ");
+            node = node.next;
+        }
+        System.out.print("end");
+
+    }
+
+    private  class Node{
+        private int value;
+        private Node next;
+
+        public  Node(int value){
+            this.value = value;
+        }
+
+        public  Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
 }
