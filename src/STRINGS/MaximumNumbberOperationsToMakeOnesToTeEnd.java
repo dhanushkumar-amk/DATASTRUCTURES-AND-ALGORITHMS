1e4eac0c75a9d9ad5d7b0c64c87da55493ae4eb3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-28 07:33:12 +0530

    Modified on 28-09-2024

--- src/STRINGS/MaximumNumbberOperationsToMakeOnesToTeEnd.java
+++ src/STRINGS/MaximumNumbberOperationsToMakeOnesToTeEnd.java
@@ -3,29 +3,14 @@ package STRINGS;
 
 public class MaximumNumbberOperationsToMakeOnesToTeEnd {
     public static void main(String[] args) {
-        String s = "hello";
-reverse(s);
+
     }
 
     public  static int maxOperations(String s){
-return 0;
+        
     }
 
-public  static  void reverse(String s){
-    char i = 0;
-    char j = (char) (s.length() - 1);
 
-    while (i <= j){
-        char temp = i;
-        i = j;
-        j =  temp;
-
-        i++;
-        j--;
-    }
-
-    System.out.println(s);
-    }
 
 }
 
