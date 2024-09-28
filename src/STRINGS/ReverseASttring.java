941533f5ff0ddb5d99bb8c341032a81f3751859a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-28 07:37:35 +0530

    Modified on 28-09-2024

--- src/STRINGS/ReverseASttring.java
+++ src/STRINGS/ReverseASttring.java
@@ -1,2 +1,18 @@
-package STRINGS;public class ReverseASttring {
+package STRINGS;
+
+public class ReverseASttring {
+    public static void main(String[] args) {
+    String s = "hello";
+
+
+        String reverse = "";
+        for (int i = s.length() - 1; i > 0 ; i--) {
+            reverse += i;
+            }
+
+        System.out.println(s);
+
+
+    }
+
 }
