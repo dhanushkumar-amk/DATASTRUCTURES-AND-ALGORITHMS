8769c95a02b489b77b88b1f729e1e5f636366f90
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-28 07:28:40 +0530

    Modified on 28-09-2024

--- src/STRINGS/MaximumNumbberOperationsToMakeOnesToTeEnd.java
+++ src/STRINGS/MaximumNumbberOperationsToMakeOnesToTeEnd.java
@@ -12,8 +12,8 @@ return 0;
     }
 
 public  static  void reverse(String s){
-    char i = s.charAt(0);
-    char j =  s.charAt(s.length() - 1);
+    char i = 0;
+    char j = (char) (s.length() - 1);
 
     while (i < j){
         char temp = i;
