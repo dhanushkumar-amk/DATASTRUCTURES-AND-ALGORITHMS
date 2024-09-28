09815e8b5968c935699c25819c2fb4719f73daa6
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-28 07:46:09 +0530

    Modified on 28-09-2024

--- src/STRINGS/ReverseASttring.java
+++ src/STRINGS/ReverseASttring.java
@@ -2,16 +2,19 @@ package STRINGS;
 
 public class ReverseASttring {
     public static void main(String[] args) {
-        String s = "Hello";
+    String s = "Hello";
+
 
         String reverse = "";
 
         String ans = s.toLowerCase();
 
-        for (int i = ans.length() - 1; i >= 0 ; i--) {
+        for (int i = s.length() - 1; i >= 0 ; i--) {
             reverse = reverse + ans.charAt(i);
-        }
+            System.out.println(ans);
+            }
 
-        System.out.println("Reversed String: " + reverse);
+
     }
+
 }
