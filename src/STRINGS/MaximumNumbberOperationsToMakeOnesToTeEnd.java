e30edec764f4697a012c1220fa6f41c8e9f8af0b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-28 07:14:38 +0530

    Modified on 28-09-2024

--- src/STRINGS/MaximumNumbberOperationsToMakeOnesToTeEnd.java
+++ src/STRINGS/MaximumNumbberOperationsToMakeOnesToTeEnd.java
@@ -1,2 +1,13 @@
-package STRINGS;public class MaximumNumbberOperationsToMakeOnesToTeEnd {
+package STRINGS;
+
+import java.util.Arrays;
+
+public class MaximumNumbberOperationsToMakeOnesToTeEnd {
+    public static void main(String[] args) {
+
+    }
+
+    public  static int maxOperations(String s){
+
+    }
 }
