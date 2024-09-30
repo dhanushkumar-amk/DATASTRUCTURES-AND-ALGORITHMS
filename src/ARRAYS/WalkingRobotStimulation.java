ed869eb2e692bc1a4a4ae8733f4d581e2433804b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-30 20:10:52 +0530

    modified

--- src/ARRAYS/WalkingRobotStimulation.java
+++ src/ARRAYS/WalkingRobotStimulation.java
@@ -1,5 +1,8 @@
 package ARRAYS;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class WalkingRobotStimulation {
     public static void main(String[] args) {
 
@@ -7,6 +10,24 @@ public class WalkingRobotStimulation {
 
     public static int robotSim(int[] commands, int[][] obstacles) {
 
-        
+        int XDirection = 0;
+        int YDirection = 1;
+        int CurrentX = 0;
+        int CurrentY = 0;
+
+        Set<String> set = new HashSet<>();
+        int max = 0;
+        for(int[] obs : obstacles)
+            set.add(obs[0] + ":" + obs[1]);
+
+        for (int command : commands){
+            if (command == -1){
+                if (XDirection == 0 && YDirection == 1){
+                    XDirection = 1;
+                    YDirection =0;
+                }
+            }
+        }
+
     }
 }
