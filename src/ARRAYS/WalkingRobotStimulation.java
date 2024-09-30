ec1a5b25d0895b29a7a717f761030be75918a271
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-30 20:05:28 +0530

    modified

--- src/ARRAYS/WalkingRobotStimulation.java
+++ src/ARRAYS/WalkingRobotStimulation.java
@@ -1,2 +1,12 @@
-package ARRAYS;public class WalkingRobotStimulation {
+package ARRAYS;
+
+public class WalkingRobotStimulation {
+    public static void main(String[] args) {
+
+    }
+
+    public static int robotSim(int[] commands, int[][] obstacles) {
+
+        
+    }
 }
