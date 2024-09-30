0875ca6da7f1b9331e285d57d55258dfb90fce58
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-09-30 19:30:59 +0530

    modified

--- src/ARRAYS/Convert1DArrayInto2DArray.java
+++ src/ARRAYS/Convert1DArrayInto2DArray.java
@@ -3,5 +3,19 @@ package ARRAYS;
 // leetcode 2022
 
 public class Convert1DArrayInto2DArray {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] construct2DArray(int[] original, int m , int n){
+
+        // base condition
+
+        if (m * n != original.length)
+            return  new int[][]{};
+
+        
+
+    }
 
 }
