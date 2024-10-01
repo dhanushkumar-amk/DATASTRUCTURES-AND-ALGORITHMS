5ece31fe04520c072ae876623aff0d73cdd86cf3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-01 20:22:31 +0530

    modified

--- src/STRINGS/RepeatedString.java
+++ src/STRINGS/RepeatedString.java
@@ -1,2 +1,19 @@
-package STRINGS;public class RepeatedString {
+package STRINGS;
+
+public class RepeatedString {
+    public static void main(String[] args) {
+
+    }
+    static  int repeat(String s, int n){
+        int count = 0;
+        for (int i = 0; i <s.length() ; i++) {
+            if (s.charAt(i) == 'a')
+                count++;
+        }
+       count  =  count * (n/s.length());
+
+        for (int i = 0; i < n % s.length() ; i++) {
+            
+        }
+    }
 }
