d05f885ea15df5f4ab9b83652909899f8f7158c6
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 10:04:07 +0530

    modified

--- src/STRINGS/AlternatingCharacters.java
+++ src/STRINGS/AlternatingCharacters.java
@@ -1,2 +1,19 @@
-package STRINGS;public class AlternatingCharacters {
+package STRINGS;
+
+public class AlternatingCharacters {
+    public static void main(String[] args) {
+        String s = "aabaab";
+        System.out.println(delete(s));
+    }
+    static  int delete(String s){
+        int count = 0;
+
+        for (int i = 0; i <s.length() ; i++) {
+            if (s.charAt(i) == s.charAt(i + 1))
+                count++;
+            else
+                continue;
+        }
+        return count;
+    }
 }
