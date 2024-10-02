185cb6e5af9543f7c4bc74db4c0a78ded1044519
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 13:48:37 +0530

    modified

--- src/JAVA_LEARNING/FileHandlinginJava.java
+++ src/JAVA_LEARNING/FileHandlinginJava.java
@@ -1,26 +1,45 @@
 package JAVA_LEARNING;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class FileHandlinginJava {
     public static void main(String[] args) {
 
-        try(InputStreamReader isr = new InputStreamReader(System.in)){
-            System.out.print("Enter the something... : ");
-            int letters = isr.read();
+//        try(InputStreamReader isr = new InputStreamReader(System.in)){
+//            System.out.print("Enter the something... : ");
+//            int letters = isr.read();
+//
+//            while (isr.ready()){
+//                System.out.println((char) letters);
+//                letters = isr.read();
+//            }
+//
+////            isr.close();
+//            System.out.println();
+//
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());;
+//        }
 
-            while (isr.ready()){
+
+        try(FileReader fr = new FileReader("note.txt")){
+            int letters = fr.read();
+
+            while (fr.ready()){
                 System.out.println((char) letters);
-                letters = isr.read();
+                letters = fr.read();
             }
 
-            isr.close();
+//            isr.close();
             System.out.println();
 
         } catch (IOException e) {
             System.out.println(e.getMessage());;
         }
+
+
 
     }
 
