bceeb76a5836f539838c8903911a344fc41be8b4
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 11:26:48 +0530

    modified

--- src/JAVA_LEARNING/BIGVALUES/BigDecimals.java
+++ src/JAVA_LEARNING/BIGVALUES/BigDecimals.java
@@ -3,7 +3,7 @@ package JAVA_LEARNING.BIGVALUES;
 public class BigDecimals {
     public static void main(String[] args) {
     double x = 0.03;
-    double y = 0.02;
+    double y = 0.04;
      double ans = y - x;
 
         System.out.println(ans);
