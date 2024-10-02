b7ce697e34b332e4a9cc09c783b49a8ac88306da
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-03 04:29:49 +0530

    modified

--- src/ARRAYS/NextGreaterElement1.java
+++ src/ARRAYS/NextGreaterElement1.java
@@ -1,2 +1,10 @@
-package ARRAYS;public class NextGreaterElement1 {
+package ARRAYS;
+
+public class NextGreaterElement1 {
+    public static void main(String[] args) {
+
+    }
+    public static  int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+    }
 }
