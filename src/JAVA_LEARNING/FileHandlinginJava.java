9cb95aa9f25e448933506ddc3f1d1aded12cc980
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 14:32:05 +0530

    modified

--- src/JAVA_LEARNING/FileHandlinginJava.java
+++ src/JAVA_LEARNING/FileHandlinginJava.java
@@ -28,7 +28,7 @@ public class FileHandlinginJava {
             int letters = fr.read();
 
             while (fr.ready()){
-                System.out.print( letters);
+                System.out.print((char) letters);
                 letters = fr.read();
             }
 
