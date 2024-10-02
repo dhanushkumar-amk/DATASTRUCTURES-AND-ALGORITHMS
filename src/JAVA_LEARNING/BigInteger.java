0b0d1669380eca1a4a666e46daeeba3fb0063128
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 10:16:13 +0530

    modified

--- src/JAVA_LEARNING/BigInteger.java
+++ src/JAVA_LEARNING/BigInteger.java
@@ -1,2 +1,9 @@
-package JAVA_LEARNING;public class BigInteger {
+package JAVA_LEARNING;
+
+public class BigInteger {
+    public static void main(String[] args) {
+
+        BigInteger a = new BigInteger();
+
+    }
 }
