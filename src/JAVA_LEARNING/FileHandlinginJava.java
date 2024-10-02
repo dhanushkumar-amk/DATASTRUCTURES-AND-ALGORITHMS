d8831fa501b4360f96628ce99060eac085dda077
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 13:45:22 +0530

    modified

--- src/JAVA_LEARNING/FileHandlinginJava.java
+++ src/JAVA_LEARNING/FileHandlinginJava.java
@@ -1,7 +1,28 @@
 package JAVA_LEARNING;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class FileHandlinginJava {
     public static void main(String[] args) {
 
+        try(InputStreamReader isr = new InputStreamReader(System.in)){
+            System.out.println("Enter the something...");
+            int letters = isr.read();
+
+            while (isr.ready()){
+                System.out.println((char) letters);
+                letters = isr.read();
+            }
+
+            isr.close();
+            System.out.println();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());;
+        }
+
     }
+
+
 }
