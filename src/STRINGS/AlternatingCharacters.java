0a4fdb689bd8997e1fa47b69168f087b65d3b534
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 10:04:19 +0530

    modified

--- src/STRINGS/AlternatingCharacters.java
+++ src/STRINGS/AlternatingCharacters.java
@@ -8,7 +8,7 @@ public class AlternatingCharacters {
     static  int delete(String s){
         int count = 0;
 
-        for (int i = 0; i <s.length() ; i++) {
+        for (int i = 0; i <s.length()-1 ; i++) {
             if (s.charAt(i) == s.charAt(i + 1))
                 count++;
             else
