f409c5fbb95ea862090e1accfcae6e49585cd819
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 10:31:54 +0530

    modified

--- src/JAVA_LEARNING/BigIntegers.java
+++ src/JAVA_LEARNING/BigIntegers.java
@@ -1,10 +1,26 @@
 package JAVA_LEARNING;
 
-public class BigInteger {
+import  java.math.BigInteger;
+public class BigIntegers {
     public static void main(String[] args) {
 
         int a = 30;
-        
+        int b = 20;
+
+        // we can also have the integer value that convert into the bigInteger
+        BigInteger A = BigInteger.valueOf(33);
+        BigInteger B = BigInteger.valueOf(337565);
+
+
+        // we can also store the string values
+        BigInteger C = new BigInteger("64746556874675645678654854767");
+
+        // we can also store the constants
+
+        BigInteger D = BigInteger.ONE;
+        BigInteger E = BigInteger.TWO;
+        BigInteger F = BigInteger.TEN;
+
 
     }
 }
