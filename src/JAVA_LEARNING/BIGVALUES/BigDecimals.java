2a8b7dfa3fecf21bcede79fa9f48e8f2557596b7
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 11:26:24 +0530

    modified

--- src/JAVA_LEARNING/BIGVALUES/BigDecimals.java
+++ src/JAVA_LEARNING/BIGVALUES/BigDecimals.java
@@ -1,2 +1,12 @@
-package JAVA_LEARNING.BIGVALUES;public class BigDecimals {
+package JAVA_LEARNING.BIGVALUES;
+
+public class BigDecimals {
+    public static void main(String[] args) {
+    double x = 0.03;
+    double y = 0.02;
+     double ans = y - x;
+
+        System.out.println(ans);
+    }
+
 }
--- src/JAVA_LEARNING/BIGVALUES/BigIntegers.java
+++ src/JAVA_LEARNING/BIGVALUES/BigIntegers.java
@@ -1,4 +1,4 @@
-package JAVA_LEARNING;
+package JAVA_LEARNING.BIGVALUES;
 
 import  java.math.BigInteger;
 public class BigIntegers {
@@ -44,7 +44,8 @@ public class BigIntegers {
         System.out.println(fact(100));
         // 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000
 
-        System.out.println(fact1(8));
+        System.out.println(fact1(100));
+        // 0  - because the integer range will exceeded
     }
 
 
