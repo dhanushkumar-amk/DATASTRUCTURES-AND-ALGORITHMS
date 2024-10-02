00d589a11d2472704240fa29db10f7bd64a93f73
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 12:08:12 +0530

    modified

--- src/JAVA_LEARNING/BIGVALUES/StringBuffers.java
+++ src/JAVA_LEARNING/BIGVALUES/StringBuffers.java
@@ -1,5 +1,20 @@
 package JAVA_LEARNING.BIGVALUES;
 
 public class StringBuffers {
-    
+    public static void main(String[] args) {
+
+        // constructor 1
+        StringBuffer sb1 = new StringBuffer();
+
+        // constructor 2
+        StringBuffer sb2 = new StringBuffer("Hello world");
+
+        // constructor 3
+        StringBuffer sb3 = new StringBuffer(30);  // to define size
+
+        sb1.append("Welcome to world");
+
+        System.out.println(sb1);
+    }
+
 }
