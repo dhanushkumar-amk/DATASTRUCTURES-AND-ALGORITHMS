3c8dd0d3e6cf00265c32349b8119ea9398ad8571
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-03 04:48:44 +0530

    modified

--- src/ARRAYS/NextGreaterElement1.java
+++ src/ARRAYS/NextGreaterElement1.java
@@ -1,10 +1,15 @@
 package ARRAYS;
 
+import java.util.Stack;
+
 public class NextGreaterElement1 {
     public static void main(String[] args) {
 
     }
     public static  int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        return new int[];
+
+        Stack<Integer> helperStack = new Stack<>();
+
+
     }
 }
