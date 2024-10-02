2f78b3ae6a1601e582d8f53598d3b5fb25c82bb9
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-02 11:40:18 +0530

    modified

--- src/JAVA_LEARNING/BIGVALUES/BigDecimals.java
+++ src/JAVA_LEARNING/BIGVALUES/BigDecimals.java
@@ -4,14 +4,14 @@ import java.math.BigDecimal;
 
 public class BigDecimals {
     public static void main(String[] args) {
-    double x = 0.03;
+     double x = 0.03;
     double y = 0.04;
      double ans = y - x;
      System.out.println(ans);  // 0.010000000000000002
 
 
-        BigDecimal A = new BigDecimal("0.00000000000000003");
-        BigDecimal B = new BigDecimal("0.00000000000000004");
+        BigDecimal A = new BigDecimal("0.03");
+        BigDecimal B = new BigDecimal("0.04");
         BigDecimal ans1 = B.subtract(A);
 
         System.out.println(ans1);   // 0.01
@@ -26,5 +26,4 @@ public class BigDecimals {
 //        System.out.println(A.sqrt();
         System.out.println(A.pow(5));
     }
-
 }
