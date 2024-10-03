cd58d95c3e63a0bb5c3fff09c63fca3b8de2a67d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-03 18:36:39 +0530

    modified

--- src/ARRAYS/NextGreaterElement1.java
+++ src/ARRAYS/NextGreaterElement1.java
@@ -1,51 +1,47 @@
 package ARRAYS;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Stack;
 
 public class NextGreaterElement1 {
     public static void main(String[] args) {
-    int [] nums1 = {4,1,2};
-    int [] nums2 = {1,3,4,2};
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
 
-        System.out.println(Arrays.toString(nextGreaterElement(nums1,nums2)));
-
+        System.out.println(Arrays.toString(nextGreaterElement(nums1, nums2)));
     }
-    public static  int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> helperStack = new Stack<>();
+        HashMap<Integer, Integer> nextGreaterMap = new HashMap<>();
+        int[] result = new int[nums1.length];
 
-        int[] nextGreaterElement = new int[nums2.length];
-
-        for (int i = nums2.length - 1; i >=0 ; i--) {
+        // Traverse nums2 to fill the nextGreaterMap with next greater elements
+        for (int i = nums2.length - 1; i >= 0; i--) {
             int element = nums2[i];
 
-            if (helperStack.isEmpty()){
-                helperStack.push(element);
-                nextGreaterElement[i] =-1;
-                continue;
-            }
-
-            if (helperStack.peek() > element){
-                nextGreaterElement[i] = helperStack.peek();
-                continue;
-            }
-
-            // remove all  elements in stack smaller than element
-
-            while (helperStack.peek() < element && !helperStack.isEmpty()){
+            // Pop elements from stack while they are smaller than the current element
+            while (!helperStack.isEmpty() && helperStack.peek() <= element) {
                 helperStack.pop();
             }
 
-            if (helperStack.isEmpty())
-                nextGreaterElement[i] =-1;
-            else
-                nextGreaterElement[i] = helperStack.peek();
+            // If the stack is empty, there is no greater element
+            if (helperStack.isEmpty()) {
+                nextGreaterMap.put(element, -1);
+            } else {
+                nextGreaterMap.put(element, helperStack.peek());
+            }
 
+            // Push the current element onto the stack
             helperStack.push(element);
-
         }
-        return nextGreaterElement;
 
+        // Use the map to find next greater elements for nums1
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreaterMap.get(nums1[i]);
+        }
+
+        return result;
     }
 }
