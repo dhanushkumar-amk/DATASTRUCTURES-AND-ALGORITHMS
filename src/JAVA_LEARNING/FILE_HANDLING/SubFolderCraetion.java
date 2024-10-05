e0e84ab04175e29f8a7497e4bd124c0d336d6c38
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-05 18:41:25 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/SubFolderCraetion.java
+++ src/JAVA_LEARNING/FILE_HANDLING/SubFolderCraetion.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class SubFolderCraetion {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\FolderDemo\\ folder1\\folder2");
+        File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\SubFolderDemo\\ folder1\\folder2");
 
         // check if the folder is already exists or not
         boolean present = file.exists();
