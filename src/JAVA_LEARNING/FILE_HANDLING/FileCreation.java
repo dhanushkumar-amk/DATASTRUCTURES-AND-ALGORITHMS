959bc4c77699d02371019c6fcfbc649caa5ec877
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-05 18:47:52 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/FileCreation.java
+++ src/JAVA_LEARNING/FILE_HANDLING/FileCreation.java
@@ -1,2 +1,21 @@
-package JAVA_LEARNING.FILE_HANDLING;public class FileCreation {
+package JAVA_LEARNING.FILE_HANDLING;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileCreation {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\file1.txt");
+
+        boolean present = file.exists();
+
+        if (!present){
+            try{
+                boolean created = file.createNewFile();
+                System.out.println("File is Created : " + created );
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
