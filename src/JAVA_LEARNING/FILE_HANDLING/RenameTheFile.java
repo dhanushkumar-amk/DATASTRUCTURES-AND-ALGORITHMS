97f7c61c82298f6827c2dea586399ce4304ee280
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-05 18:59:31 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/RenameTheFile.java
+++ src/JAVA_LEARNING/FILE_HANDLING/RenameTheFile.java
@@ -1,2 +1,30 @@
-package JAVA_LEARNING.FILE_HANDLING;public class RenameTheFile {
+package JAVA_LEARNING.FILE_HANDLING;
+
+import java.io.File;
+import java.io.IOException;
+
+public class RenameTheFile {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\file1.txt");
+
+        boolean present = file.exists();
+
+
+        // file creation
+        if (!present){
+            try {
+                file.createNewFile();
+            }catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+            // then create a new file
+
+            if (present){
+              File RenameFile = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\file1.txt");
+              boolean created = RenameFile.renameTo(RenameFile);
+            }
+            
+        }
+    }
 }
