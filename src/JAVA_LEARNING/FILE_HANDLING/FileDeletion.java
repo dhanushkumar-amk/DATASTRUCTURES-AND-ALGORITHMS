8ad6a1325c397704b6201961ca87c01452ebed24
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-05 18:51:10 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/FileDeletion.java
+++ src/JAVA_LEARNING/FILE_HANDLING/FileDeletion.java
@@ -1,2 +1,11 @@
-package JAVA_LEARNING.FILE_HANDLING;public class FileDeletion {
+package JAVA_LEARNING.FILE_HANDLING;
+
+import java.io.File;
+
+public class FileDeletion {
+    public static void main(String[] args) {
+        File file =new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\file1.txt");
+
+        
+    }
 }
