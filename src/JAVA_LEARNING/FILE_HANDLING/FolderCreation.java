5bc2e4e337d8a962d9d6ba99ce99dcd8010f9316
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-05 18:39:16 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/FolderCreation.java
+++ src/JAVA_LEARNING/FILE_HANDLING/FolderCreation.java
@@ -4,14 +4,14 @@ import java.io.File;
 
 public class FolderCreation {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\FolderDemo");
+        File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\FolderDemo\\hello\\world");
 
             // check if the folder is already exists or not
         boolean present = file.exists();
 //        System.out.println("Folder present : " + present);
 
         if (present == false){
-            file.mkdir();
+            file.mkdirs();
         }
     }
 }
