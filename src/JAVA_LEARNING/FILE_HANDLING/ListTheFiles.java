dd97498a6186b5cf11a54600e467e8f4772b38f1
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-05 19:56:00 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/ListTheFiles.java
+++ src/JAVA_LEARNING/FILE_HANDLING/ListTheFiles.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class ListTheFiles {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\ARRAYS");
+        File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src");
 
 //       String[] listOfFiles =  file.list();
 
