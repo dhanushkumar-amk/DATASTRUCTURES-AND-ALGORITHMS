fcd885ef066c052c52b7f83f8487fb1e2592686f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-05 19:48:33 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/ListTheFiles.java
+++ src/JAVA_LEARNING/FILE_HANDLING/ListTheFiles.java
@@ -6,11 +6,17 @@ public class ListTheFiles {
     public static void main(String[] args) {
         File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src");
 
-       String[] listOfFiles =  file.list();
+//       String[] listOfFiles =  file.list();
 
-        for (int i = 0; i <listOfFiles.length ; i++) {
-            System.out.println(listOfFiles[i]);
-        }
+//        for (int i = 0; i <listOfFiles.length ; i++)
+//            System.out.println(listOfFiles[i]);
+//
+
+        File[] fileFolderList = file.listFiles();
+
+        for(File f : fileFolderList)
+            if (f.isFile())
+                System.out.println(f);
 
     }
 }
