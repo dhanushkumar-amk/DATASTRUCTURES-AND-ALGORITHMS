fc8d8fd030f691074489014a4648efc4fecf9259
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-05 20:17:45 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/ListTheFiles.java
+++ src/JAVA_LEARNING/FILE_HANDLING/ListTheFiles.java
@@ -33,7 +33,7 @@ public class ListTheFiles {
                 String fileName = f.getName();
                 int lastDot = fileName.lastIndexOf(".");
                 String extensionOfFileName = fileName.substring(lastDot + 1);
-//            System.out.println(extensionOfFileName);
+            System.out.println(extensionOfFileName);
 
 
             }
