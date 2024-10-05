fefb69a8df1e5530f2afc7bd6fcc87059ffa80c0
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-05 19:20:52 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/printFileName.java
+++ src/JAVA_LEARNING/FILE_HANDLING/printFileName.java
@@ -1,2 +1,12 @@
-package JAVA_LEARNING.FILE_HANDLING;public class printFileName {
+package JAVA_LEARNING.FILE_HANDLING;
+
+import java.io.File;
+
+public class printFileName {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\file1.txt");
+
+
+        System.out.println(file.getName());
+    }
 }
