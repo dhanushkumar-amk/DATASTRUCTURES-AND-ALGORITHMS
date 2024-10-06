e61a3ca402865d4a8d0541e683b9e4fb5f486ce8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-06 09:38:33 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/BufferReaderWriter.java
+++ src/JAVA_LEARNING/FILE_HANDLING/BufferReaderWriter.java
@@ -1,19 +1,16 @@
 package JAVA_LEARNING.FILE_HANDLING;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 
 public class BufferReaderWriter {
     public static void main(String[] args) {
         File file = new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\hello.txt");
 
-        FileReader fWriter;
+        FileWriter fWriter;
         try {
-            fWriter = new FileReader(file);
+            fWriter = new FileWriter(file);
             BufferedWriter bWriter = new BufferedWriter(fWriter);
-        } catch (FileNotFoundException e) {
+        }  catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
