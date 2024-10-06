2539e9d471e89e29a6125083d677182d1e45bb61
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-06 09:08:22 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/FileReaderAndWriter.java
+++ src/JAVA_LEARNING/FILE_HANDLING/FileReaderAndWriter.java
@@ -1,6 +1,7 @@
 package JAVA_LEARNING.FILE_HANDLING;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -11,10 +12,18 @@ public class FileReaderAndWriter {
         try{
         file.createNewFile();
 
-            FileWriter writer = new FileWriter(file);
-            writer.write("hello world \n welcom eto the world");// there is a one disadvantage iis we use manually escape sequence
+            FileWriter writer = new FileWriter(file, true);
+            writer.write("hello world \n welcome to the world");// there is a one disadvantage iis we use manually escape sequence
             writer.flush();   // to push the content into the file
             writer.close();
+
+
+            FileReader fileReader = new FileReader(file);
+            int output = fileReader.read();  // it will return -1 whenever it completes the reading the file
+            while (output != -1) {
+                System.out.println(output);
+                output = fileReader.read();
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
