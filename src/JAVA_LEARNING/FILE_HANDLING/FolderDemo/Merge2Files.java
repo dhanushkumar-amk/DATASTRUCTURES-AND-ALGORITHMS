b12fed3704332da84c9459f5294e626cfcd8f5d2
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-06 10:04:34 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/FolderDemo/Merge2Files.java
+++ src/JAVA_LEARNING/FILE_HANDLING/FolderDemo/Merge2Files.java
@@ -1,2 +1,36 @@
-package JAVA_LEARNING.FILE_HANDLING.FolderDemo;public class Merge2Files {
+package JAVA_LEARNING.FILE_HANDLING.FolderDemo;
+
+import java.io.*;
+
+public class Merge2Files {
+    public static void main(String[] args) throws IOException {
+
+
+        FileReader Freader1 = new FileReader("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\FolderDemo\\text1.txt");
+        FileReader Freader2 = new FileReader("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\FolderDemo\\text2.txt");
+
+        BufferedReader Breader1 = new BufferedReader(Freader1);
+        BufferedReader Breader2 = new BufferedReader(Freader2);
+
+        // merge two files using printWriter
+        PrintWriter pWriter = new PrintWriter("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\FolderDemo\\text3.txt");
+
+        String line1 = Breader1.readLine();
+        String line2 = Breader2.readLine();
+
+        while (line1 != null || line2 != null){
+            if (line1 != null) {
+                pWriter.println(line1);
+                line2 =
+            }
+            if (line2 != null) {
+                pWriter.println(line2);
+            }
+
+        }
+
+
+
+
+    }
 }
