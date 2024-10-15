b45e25c2166e497d2b8d381d052fae5e22bcf876
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-15 19:13:02 +0530

    modified

--- src/PATTERNS/StairsCase.java
+++ src/PATTERNS/StairsCase.java
@@ -5,8 +5,22 @@ public class StairsCase {
 
         int n = 5;
 
+        int spaces = n-1;
+        int hash = 1;
+
         for (int i = 0; i <n ; i++) {
-            
+
+            // print spaces
+            for (int j = 0; j < spaces; j++) {
+                System.out.println(" ");
+            }
+
+            for (int j = 0; j <hash ; j++) {
+                System.out.print(" # ");
+            }
+
+            spaces--;
+            hash++;
         }
 
     }
