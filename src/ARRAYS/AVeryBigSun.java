0579353f1667fc699928e406affb1e75d9605769
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-15 18:37:22 +0530

    modified

--- src/ARRAYS/AVeryBigSun.java
+++ src/ARRAYS/AVeryBigSun.java
@@ -1,2 +1,17 @@
-package ARRAYS;public class AVeryBigSun {
+package ARRAYS;
+
+public class AVeryBigSun {
+    public static void main(String[] args) {
+        long[] arr = {1000001,1000001,1000002,1000003 1000005};
+        System.out.println(sum(arr));
+    }
+
+    static long sum(long[] arr){
+        long sum = 0;
+
+        for (int i = 0; i < arr.length ; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
 }
