bf8e08f3fa08f61dbdc555ee6b108531a365f2f1
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-15 20:01:10 +0530

    modified

--- src/MATHEMATICALS/GradingStudent.java
+++ src/MATHEMATICALS/GradingStudent.java
@@ -2,10 +2,27 @@ package MATHEMATICALS;
 
 public class GradingStudent {
     public static void main(String[] args) {
-        int[] grades = {};
+        int[] grades = {20,44,67,56,56};
+        gradeStu(grades);
     }
 
-    public  static  int grade(int[] grade){
-        
+    public  static  void gradeStu(int[] grade){
+
+        for (int i = 0; i < grade.length ; i++) {
+
+            if (grade[i]  < 38)
+                continue;
+            else{
+                int multiple = (grade[i]/5 + 1) * 5;
+                if (multiple - grade[i] < 3)
+                    grade[i] = multiple;
+            }
+        }
+
+
+        for (int i = 0; i < grade.length ; i++) {
+            System.out.println(grade[i] + " ");
+        }
+
     }
 }
