2644577a7998d8aa7caea3ef29eed4888d7cbf25
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-15 19:07:02 +0530

    modified

--- src/PATTERNS/StairsCase.java
+++ src/PATTERNS/StairsCase.java
@@ -1,2 +1,13 @@
-package PATTERNS;public class StairsCase {
+package PATTERNS;
+
+public class StairsCase {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 0; i <n ; i++) {
+            
+        }
+
+    }
 }
