3f522611571f704015b6a5cd974176d99f9a604b
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-15 18:48:13 +0530

    modified

--- src/MATRIX/DiagonalDifference.java
+++ src/MATRIX/DiagonalDifference.java
@@ -1,2 +1,19 @@
-package MATRIX;public class DiagonalDifference {
+package MATRIX;
+
+public class DiagonalDifference {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+        };
+
+    }
+
+    public static int difference(int[][] matrix){
+        
+    }
+
+
 }
