390fb35e0c698cceefc2fc04f92f98995fd09e42
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-10-15 18:56:52 +0530

    modified

--- src/MATRIX/DiagonalDifference.java
+++ src/MATRIX/DiagonalDifference.java
@@ -8,11 +8,27 @@ public class DiagonalDifference {
                 {4,5,6},
                 {7,8,9},
         };
+        System.out.println(difference(matrix));
 
     }
 
     public static int difference(int[][] matrix){
-        
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int n = 3;
+
+        for (int i = 0; i <n ; i++) {
+            leftSum += matrix[i][i];
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            rightSum = matrix[i][n-1- i];
+        }
+
+        return  Math.abs(leftSum - rightSum);
+
     }
 
 
