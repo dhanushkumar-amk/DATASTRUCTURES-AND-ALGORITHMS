3ac73c04a484ce54cbee0cc51482890f3df4abc8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-11-05 10:56:33 +0530

    modified

--- src/JAVA_LEARNING/FILE_HANDLING/FileDeletion.java
+++ src/JAVA_LEARNING/FILE_HANDLING/FileDeletion.java
@@ -1,12 +1,18 @@
 package JAVA_LEARNING.FILE_HANDLING;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class FileDeletion {
-    public static void main(String[] args) {
-        File file =new File("C:\\Users\\hp5cd\\OneDrive\\Documents\\DATASTRUCTURES-AND-ALGORITHMS\\src\\JAVA_LEARNING\\FILE_HANDLING\\file1.txt");
 
-        file.delete();
 
+    public static class StudentdetailsValidation {
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the name :");
+            name = sc.next();
+            
+        }
     }
 }
